2894f1c42f18e9643d3fc9e185da0db55d0ee052
Peter Bengtsson <mail@peterbe.com>
2019-06-06 15:56:05 -0400

    travis

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import App from "./App";
+
+it("renders without crashing", () => {
+  const app = (
+    <BrowserRouter>
+      {/* <App document={documentData} /> */}
+      <App />
+    </BrowserRouter>
+  );
+  const div = document.createElement("div");
+  ReactDOM.render(app, div);
   ReactDOM.unmountComponentAtNode(div);
 });
