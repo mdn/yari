923ec9b33f125c2346028b7ce18a67d428ee97bd
Peter Bengtsson <mail@peterbe.com>
2019-07-17 09:48:42 -0400

    tidying up

--- bin/yarn-audit-all.sh
+++ bin/yarn-audit-all.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-# echo "DIFF:"
-# git diff --name-only origin/master
+echo "DIFF file names:"
+git diff --name-only origin/master
+echo ""
 
 function didyarnchange() {
     regex="(${1}/package\.json|${1}/yarn\.lock)"
@@ -12,52 +13,29 @@ function didyarnchange() {
 }
 
 if didyarnchange client; then
-    echo "Client changed!"
+    echo "client packages changed"
     cd client
     yarn audit
     cd -
 else
-    echo "Client did NOT change"
+    echo "client did NOT change - no yarn audit"
 fi
 
 if didyarnchange cli; then
-    echo "Cli changed!"
+    echo "cli packages changed"
     cd cli
     yarn audit
     cd -
 else
-    echo "Cli did NOT change"
+    echo "cli did NOT change - no yarn audit"
 fi
 
 if didyarnchange server; then
-    echo "Server changed!"
+    echo "server packages changed"
     # Only care about the `>= high` warnings because this
     # workspace is only used for local development.
     yarn audit --level high
     cd -
-
 else
-    echo "server did NOT change"
+    echo "server did NOT change - no yarn audit"
 fi
-
-# if [ $SERVER_CHANGED ]; then
-#   echo "Server changed!"
-# else
-#   echo "Server did NOT change"
-# fi
-
-
-
-# cd client
-# yarn audit
-# cd -
-
-# cd cli
-# yarn audit
-# cd -
-
-# cd server
-# # Only care about the `>= high` warnings because this
-# # workspace is only used for local development.
-# yarn audit --level high
-# cd -
