4fb00c71619faaea62154e200a7287c5455bba3d
Joe D'Arcangelo <joedarc@gmail.com>
2019-09-20 14:05:18 -0400

    Initial Jest tests for Search Component
    Addressed review comments

---
+++ client/src/setupTests.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { cleanup } from "@testing-library/react";
+
+// localStorage mock for tests
+const mockLocalStorage = () => {
+  let store = {};
+  return {
+    getItem: function(key) {
+      return store[key] || null;
+    },
+    removeItem: function(key) {
+      delete store[key];
+    },
+    setItem: function(key, value) {
+      store[key] = value.toString();
+    },
+    clear: function() {
+      store = {};
+    }
+  };
+};
+
+global.localStorage = mockLocalStorage();
+
+beforeEach(() => {
+  // Replacing the native history.pushState with a wrapper to sniff calls to
+  // window.history.pushState
+  const nativeHistoryPushState = window.history.pushState;
+  window.history.pushState = function(state, title, url) {
+    nativeHistoryPushState.apply(this, arguments);
+    var event = new CustomEvent("pushState", { detail: { state, title, url } });
+    window.dispatchEvent(event);
+  };
+});
+
+afterEach(() => {
+  // Unmounts React trees that were mounted with @testing-library/react's render.
+  cleanup();
+});
