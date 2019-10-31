0d0cb0282537295a5c6e699090b2c059be2bc9fc
Peter Bengtsson <mail@peterbe.com>
2019-10-31 14:42:43 -0400

    exit earlier

--- scripts/ci-yarn-lockfile-check.sh
+++ scripts/ci-yarn-lockfile-check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # This file exists because as of yarn@1.12.3, --frozen-lockfile is completely
 # broken when combined with Yarn workspaces. See https://github.com/yarnpkg/yarn/issues/6291
 # Taken from https://github.com/yarnpkg/yarn/issues/4098#issuecomment-492729443
@@ -10,7 +12,7 @@ EXIT_CODE=$?
 CKSUM_AFTER=$(cksum yarn.lock)
 
 if [[ $CKSUM_BEFORE != $CKSUM_AFTER ]]; then
-  echo "yarn_frozen_lockfile.sh: yarn.lock was modified unexpectedly - terminating"
+  echo "yarn.lock was modified unexpectedly - terminating"
   exit 1
 fi
 
