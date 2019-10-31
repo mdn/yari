a9e1c0c2972196bd2d05100c6b7192bb20860b6b
Gregor Weber <gweber@mozilla.com>
2019-10-31 19:52:44 +0100

    better cats

--- scripts/ci-yarn-lockfile-check.sh
+++ scripts/ci-yarn-lockfile-check.sh
@@ -4,12 +4,12 @@
 # broken when combined with Yarn workspaces. See https://github.com/yarnpkg/yarn/issues/6291
 # Taken from https://github.com/yarnpkg/yarn/issues/4098#issuecomment-492729443
 
-cat yarn.lock
 CKSUM_BEFORE=$(cksum yarn.lock)
 yarn install
 EXIT_CODE=$?
 CKSUM_AFTER=$(cksum yarn.lock)
-cat yarn.lock
+cat CKSUM_BEFORE
+cat CKSUM_AFTER
 
 if [[ $CKSUM_BEFORE != $CKSUM_AFTER ]]; then
   echo "yarn_frozen_lockfile.sh: yarn.lock was modified unexpectedly - terminating"
