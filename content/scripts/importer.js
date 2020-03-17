97b44e95c6ee36aabc01c72469df98603aeb2f90
rjohnson <rjohnson@mozilla.com>
2020-03-17 16:48:20 -0700

    adjust the importer
    * add tags
    * use "translation_of" rather than "translationof"
    * remove use of "rendered_html"
    * remove query for translations

--- content/scripts/importer.js
+++ content/scripts/importer.js
@@ -130,35 +130,6 @@ async function queryContributors(query, options) {
   return { contributors, usernames };
 }
 
-async function queryTranslationRelationships(query, options) {
-  const { constraintsSQL, queryArgs } = getSQLConstraints(
-    {
-      alias: "d"
-    },
-    options
-  );
-  const sql = `
-    SELECT d.id, d.parent_id, d.slug, d.locale
-    FROM wiki_document d
-    ${constraintsSQL} AND d.parent_id IS NOT NULL
-    ORDER BY d.locale
-  `;
-
-  console.log("Going to fetch ALL parents");
-  const results = await query(sql, queryArgs);
-  const translations = {};
-  for (const row of results) {
-    if (!(row.parent_id in translations)) {
-      translations[row.parent_id] = [];
-    }
-    translations[row.parent_id].push({
-      slug: row.slug,
-      locale: row.locale
-    });
-  }
-  return translations;
-}
-
 async function queryDocumentCount(query, constraintsSQL, queryArgs) {
   const localesSQL = `
     SELECT w.locale, COUNT(*) AS count
@@ -213,7 +184,6 @@ async function queryDocuments(pool, options) {
       w.locale,
       w.is_redirect,
       w.html,
-      w.rendered_html,
       w.modified,
       p.id AS parent_id,
       p.slug AS parent_slug,
@@ -232,6 +202,35 @@ async function queryDocuments(pool, options) {
       // node MySQL uses custom streams which are not iterable. Piping it through a native stream fixes that
       .pipe(new stream.PassThrough({ objectMode: true }))
   };
+}
+
+async function queryDocumentTags(query, options) {
+  const { constraintsSQL, queryArgs } = getSQLConstraints(
+    {
+      alias: "w"
+    },
+    options
+  );
+  const sql = `
+    SELECT
+      w.id,
+      t.name
+    FROM wiki_document w
+    INNER JOIN wiki_taggeddocument wt ON wt.content_object_id = w.id
+    INNER JOIN wiki_documenttag t ON t.id = wt.tag_id
+    ${constraintsSQL}
+  `;
+
+  console.log("Going to fetch ALL document tags");
+  const results = await query(sql, queryArgs);
+  const tags = {};
+  for (const row of results) {
+    if (!(row.id in tags)) {
+      tags[row.id] = [];
+    }
+    tags[row.id].push(row.name);
+  }
+  return tags;
 }
 
 async function withTimer(label, fn) {
@@ -348,7 +347,7 @@ async function processDocument(
   doc,
   { archiveRoot, root, startClean },
   isArchive = false,
-  { usernames, contributors, translations }
+  { usernames, contributors, tags }
 ) {
   const { slug, locale, title } = doc;
   const localeFolder = path.join(
@@ -364,10 +363,7 @@ async function processDocument(
   await fs.promises.mkdir(folder, { recursive: true });
   const htmlFile = path.join(folder, "index.html");
 
-  // XXX As of right now, we don't have a KS shim that converts "raw Kuma HTML"
-  // to rendered HTML. So we'll cheat by copying the `rendered_html`.
-  // await fs.promises.writeFile(htmlFile, doc.html);
-  await fs.promises.writeFile(htmlFile, `${doc.rendered_html}`);
+  await fs.promises.writeFile(htmlFile, doc.html);
 
   const wikiHistoryFile = path.join(folder, "wikihistory.json");
   const metaFile = path.join(folder, "index.yaml");
@@ -381,7 +377,7 @@ async function processDocument(
   }
   if (doc.parent_slug) {
     assert(doc.parent_locale === "en-US");
-    meta.translationof = doc.parent_slug;
+    meta.translation_of = doc.parent_slug;
   }
 
   const wikiHistory = {
@@ -389,10 +385,10 @@ async function processDocument(
     _generated: new Date().toISOString()
   };
 
-  // const otherTranslations = translations[doc.id] || [];
-  // if (otherTranslations.length) {
-  //   meta.other_translations = otherTranslations;
-  // }
+  const docTags = tags[doc.id] || [];
+  if (docTags.length) {
+    meta.tags = docTags.sort();
+  }
   await fs.promises.writeFile(metaFile, yaml.safeDump(meta));
 
   const docContributors = (contributors[doc.id] || []).map(
@@ -405,15 +401,6 @@ async function processDocument(
     wikiHistoryFile,
     JSON.stringify(wikiHistory, null, 2)
   );
-
-  // XXX At the moment, we're pretending we have the KS shim, and that means
-  // we'll have access to the raw (full of macros) string which'll be
-  // useful to infer certain things such as how the {{Compat(...)}}
-  // macro is used. But for now, we'll inject it into the metadata:
-  if (!isArchive) {
-    const rawFile = path.join(folder, "raw.html");
-    await fs.promises.writeFile(rawFile, doc.html);
-  }
 }
 
 async function saveAllRedirects(redirects, root) {
@@ -484,12 +471,12 @@ module.exports = async function runImporter(options) {
   );
 
   const query = promisify(pool.query).bind(pool);
-  const [{ usernames, contributors }, translations] = await Promise.all([
+  const [{ usernames, contributors }, tags] = await Promise.all([
     withTimer("Time to fetch all contributors", () =>
       queryContributors(query, options)
     ),
-    withTimer("Time to fetch all translation relationships", () =>
-      queryTranslationRelationships(query, options)
+    withTimer("Time to fetch all document tags", () =>
+      queryDocumentTags(query, options)
     )
   ]);
 
@@ -556,7 +543,7 @@ module.exports = async function runImporter(options) {
         await processDocument(row, options, isArchive, {
           usernames,
           contributors,
-          translations
+          tags
         });
       }
     })()
