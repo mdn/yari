49e5c393740250056e744bf328d680561137385a
Peter Bengtsson <mail@peterbe.com>
2020-05-15 07:55:53 -0400

    refactor

--- content/scripts/build.js
+++ content/scripts/build.js
@@ -535,7 +535,10 @@ class Builder {
         .map((m) => Array.from(m.values()).reduce((a, b) => a + b, 0))
         .reduce((a, b) => a + b);
       if (!countTodo) {
-        throw new Error("No folders found to process!");
+        this.logger.warn(
+          chalk.red("No folders found to process. Did you filter too much?")
+        );
+        return;
       }
       self.initProgressbar(countTodo);
     }
