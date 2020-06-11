b5be2f51bfd6b819ad4ae3ac71c7a9ceee2ed78b
Gregor Weber <gweber@mozilla.com>
2020-06-11 14:33:33 +0200

    testing timeout change

--- testing/tests/headless.test.js
+++ testing/tests/headless.test.js
@@ -5,17 +5,16 @@ function testURL(pathname = "/") {
 }
 
 const TIMEOUT = 5000;
+describe("Basic viewing of functional pages", () => {
+  it("open the temporary home page", async () => {
+    await page.goto(testURL("/"));
+    await expect(page).toMatch("MDN Web Docs");
+    await expect(page).toMatchElement("title", { text: /MDN Web Docs/ });
+  });
 
-describe(
-  "Basic viewing of functional pages",
-  () => {
-    it("open the temporary home page", async () => {
-      await page.goto(testURL("/"));
-      await expect(page).toMatch("MDN Web Docs");
-      await expect(page).toMatchElement("title", { text: /MDN Web Docs/ });
-    });
-
-    it("search and find Foo page", async () => {
+  it(
+    "search and find Foo page",
+    async () => {
       await page.goto(testURL("/"));
       await expect(page).toFill('form input[type="search"]', "fo");
       await expect(page).toMatch("<foo>: A test tag");
@@ -23,12 +22,12 @@ describe(
       // Should have been redirected too...
       expect(page.url()).toBe(testURL("/en-US/docs/Web/Foo"));
       await expect(page).toMatchElement("h1", { text: "<foo>: A test tag" });
-    });
+    },
+    TIMEOUT
+  );
 
-    it("open the /en-US/docs/Web/Foo page", async () => {
-      await page.goto(testURL("/en-US/docs/Web/Foo"));
-      await expect(page).toMatch("<foo>: A test tag");
-    });
-  },
-  TIMEOUT
-);
+  it("open the /en-US/docs/Web/Foo page", async () => {
+    await page.goto(testURL("/en-US/docs/Web/Foo"));
+    await expect(page).toMatch("<foo>: A test tag");
+  });
+});
