1a4c873a15f5ee84e6daadcd0482e3a26017ebc5
Gregor Weber <gweber@mozilla.com>
2020-06-11 14:46:38 +0200

    change page timeout per test

--- testing/tests/headless.test.js
+++ testing/tests/headless.test.js
@@ -12,19 +12,16 @@ describe("Basic viewing of functional pages", () => {
     await expect(page).toMatchElement("title", { text: /MDN Web Docs/ });
   });
 
-  it(
-    "search and find Foo page",
-    async () => {
-      await page.goto(testURL("/"));
-      await expect(page).toFill('form input[type="search"]', "fo");
-      await expect(page).toMatch("<foo>: A test tag");
-      await expect(page).toClick("div.highlit");
-      // Should have been redirected too...
-      expect(page.url()).toBe(testURL("/en-US/docs/Web/Foo"));
-      await expect(page).toMatchElement("h1", { text: "<foo>: A test tag" });
-    },
-    TIMEOUT
-  );
+  it("search and find Foo page", async () => {
+    page.setDefaultTimeout(TIMEOUT);
+    await page.goto(testURL("/"));
+    await expect(page).toFill('form input[type="search"]', "fo");
+    await expect(page).toMatch("<foo>: A test tag");
+    await expect(page).toClick("div.highlit");
+    // Should have been redirected too...
+    expect(page.url()).toBe(testURL("/en-US/docs/Web/Foo"));
+    await expect(page).toMatchElement("h1", { text: "<foo>: A test tag" });
+  });
 
   it("open the /en-US/docs/Web/Foo page", async () => {
     await page.goto(testURL("/en-US/docs/Web/Foo"));
