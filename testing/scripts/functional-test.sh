8ab2ff9f8aa3d9528e8d31bbbe3a1ef94c5fa813
Peter Bengtsson <mail@peterbe.com>
2020-06-18 16:35:15 -0400

    better testing

--- testing/scripts/functional-test.sh
+++ testing/scripts/functional-test.sh
@@ -11,6 +11,10 @@ yarn workspace testing run test $@
 
 # Pretend you wanna fix some fixable flaws.
 # First in dry-run mode.
-node content build -f web/bar --fix-flaws --fix-flaws-dry-run --fix-flaws-verbose
+node content build -f web/fixable_flaws --fix-flaws --fix-flaws-dry-run --fix-flaws-verbose
 git status
-git diff testing/content/files/en-us/web/bar/index.html
+git diff testing/content/files/en-us/web/fixable_flaws/index.html
+# Now actually, do it
+node content build -f web/fixable_flaws --fix-flaws
+git status
+git diff testing/content/files/en-us/web/fixable_flaws/index.html
