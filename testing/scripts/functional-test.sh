ba205ca3ad4aafefa5db0ddfee3997e370c07c76
Peter Bengtsson <mail@peterbe.com>
2020-06-18 16:11:50 -0400

    functional test

--- testing/scripts/functional-test.sh
+++ testing/scripts/functional-test.sh
@@ -8,3 +8,9 @@ yarn run prebuild
 node content build
 
 yarn workspace testing run test $@
+
+# Pretend you wanna fix some fixable flaws.
+# First in dry-run mode.
+node content build -f web/bar --fix-flaws --fix-flaws-dry-run --fix-flaws-verbose
+git status
+git diff testing/content/files/en-us/web/bar/index.html
