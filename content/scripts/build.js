640bc5da553d2060e49ba28c38514335db7677c3
Gregor Weber <gweber@mozilla.com>
2020-06-18 17:51:47 +0200

    filter out circular redirects

--- content/scripts/build.js
+++ content/scripts/build.js
@@ -1923,11 +1923,13 @@ class Builder {
             return [from, redirect ? to.replace(redirect[0], redirect[1]) : to];
           }),
         ...changedURLs,
-      ].sort((a, b) => {
-        if (a[0] < b[0]) return -1;
-        if (a[0] > b[0]) return 1;
-        return 0;
-      })
+      ]
+        .filter(([oldURL, newURL]) => oldURL !== newURL)
+        .sort((a, b) => {
+          if (a[0] < b[0]) return -1;
+          if (a[0] > b[0]) return 1;
+          return 0;
+        })
     );
   }
 
