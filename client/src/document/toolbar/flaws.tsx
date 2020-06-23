e7f32201d79ce1720c2bc724a76ac6ded714899e
Peter Bengtsson <mail@peterbe.com>
2020-06-23 17:08:36 -0400

    remove commenting

--- client/src/document/toolbar/flaws.tsx
+++ client/src/document/toolbar/flaws.tsx
@@ -136,12 +136,6 @@ function BrokenLinks({ doc, links }: { doc: Doc; links: Link[] }) {
   // Now, when you've filtered the list of
   // all `document.querySelectorAll("a[href]")` that matches 'foo'
   // you know *which* one to pick.
-  console.log("PASSED IN:");
-  console.log("LINKS:", links);
-  console.log("DOC.flaws:", doc.flaws);
-
-
-
   const indexes = {};
   links.forEach((link, i) => {
     if (!(link.href in indexes)) {
