0392030c3cba5c137e84148d8a443db1c883c13b
Peter Bengtsson <mail@peterbe.com>
2020-07-07 16:43:16 -0400

    run 'node content build --fix-flaws' from jest

--- testing/scripts/functional-test.sh
+++ testing/scripts/functional-test.sh
@@ -8,11 +8,3 @@ yarn run prebuild
 node content build
 
 yarn workspace testing run test $@
-
-# Pretend you wanna fix some fixable flaws.
-# First in dry-run mode.
-node content build --quiet -f web/fixable_flaws --fix-flaws --fix-flaws-dry-run --fix-flaws-verbose
-# Now actually, do it
-node content build --quiet -f web/fixable_flaws --fix-flaws
-git status | grep fixable_flaws/index.html # will fail if not in the output
-git diff testing/content/files/en-us/web/fixable_flaws/index.html
--- testing/tests/index.test.js
+++ testing/tests/index.test.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const path = require("path");
+const { execSync } = require("child_process");
 
 const cheerio = require("cheerio");
+const simpleGit = require("simple-git");
 
 const buildRoot = path.join("..", "client", "build");
 
@@ -261,4 +263,101 @@ test("check built flaws for /en-us/learn/css/css_layout/introduction/grid page",
   // Let's make sure there are only 3 "macros" flaws.
   const { doc } = JSON.parse(fs.readFileSync(jsonFile));
   expect(doc.flaws.macros.length).toBe(3);
+});
+
+describe("fixing flaws", () => {
+  const basePattern = path.join("testing", "content", "files");
+  const pattern = path.join("web", "fixable_flaws");
+
+  // If we don't specify this, doing things like `await git.diffSummary()`
+  // will report files as they appear relative to the `.git/` root which
+  // might be different from the current `process.cwd()` which makes it
+  // impossible to look at file paths in a predictable way.
+  const baseDir = path.resolve("..");
+
+  async function getChangedFiles() {
+    const git = simpleGit();
+    const diff = await git.diffSummary();
+    return diff.files
+      .filter((f) => f.file.includes(basePattern) && f.file.includes(pattern))
+      .map((f) => f.file);
+  }
+
+  beforeAll(async () => {
+    // We assume we can test changes to the local git repo. If there were
+    // already existing fixes to the fixtures we intend to actually change,
+    // then tests are a no-go.
+    // We basically want to check that none of the files we intend to mess
+    // with, from jest, are already messed with.
+    const files = await getChangedFiles();
+    if (files.length) {
+      // This is draconian but necessary.
+      // See https://github.com/facebook/jest/issues/2713
+      // which is closed but it't still not a feature for our version of
+      // jest to have it so that it stops running the tests if you throw
+      // an error in here.
+      console.error(
+        `Can't test these things when ${files} already has its own changes`
+      );
+      // Basically, if the files that we care to test changing, were
+      // already changed (manually) then we can't run our tests and we
+      // want to bail hard.
+      process.exit(1);
+    }
+  });
+
+  // Aka. clean up by checking out any unstaged file changes made by the tests
+  afterAll(async () => {
+    // Undo any changed files of the interesting pattern
+    const git = simpleGit({
+      baseDir,
+    });
+    const diff = await git.diffSummary();
+    const files = diff.files
+      .map((f) => f.file)
+      .filter((f) => f.includes(basePattern) && f.includes(pattern));
+    if (files.length) {
+      try {
+        await git.checkout(files);
+      } catch (err) {
+        // Otherwise any error here would be swallowed
+        console.error(err);
+        throw err;
+      }
+    }
+  });
+
+  // Got to test dry-run mode and non-dry-run mode in serial otherwise
+  // test my run those two things in parallel and it's not thread-safe
+  // to change files on disk.
+  // This is why this test does so much.
+  test("build with --fix-flaws", async () => {
+    const command = `node content build --fix-flaws -f ${pattern}`;
+    const dryrunCommand = command + " --fix-flaws-dry-run";
+    const dryrunStdout = execSync(dryrunCommand, {
+      cwd: baseDir,
+      windowsHide: true,
+    }).toString();
+
+    const dryRunNotices = dryrunStdout
+      .split("\n")
+      .filter((line) => /Would have modified/.test(line));
+    expect(dryRunNotices.length).toBe(1);
+    expect(dryRunNotices[0]).toContain(pattern);
+
+    const dryrunFiles = await getChangedFiles();
+    expect(dryrunFiles.length).toBe(0);
+
+    // Now, let's do it without dry-run
+    const stdout = execSync(command + " --fix-flaws-verbose", {
+      cwd: baseDir,
+      windowsHide: true,
+    }).toString();
+    expect(stdout).toContain(pattern);
+
+    const files = await getChangedFiles();
+    expect(files.length).toBe(1);
+    // We could be fancy and open `path.join(baseDir, files[0])` to
+    // inspect the actual changes it made but it's probably overkill for now.
+  });
 });
