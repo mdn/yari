5f90d0ad688558bc0a9853abd674ea1a372b9d92
Peter Bengtsson <mail@peterbe.com>
2020-07-21 16:27:29 -0400

    refactorings

--- content/scripts/build.js
+++ content/scripts/build.js
@@ -1876,18 +1876,18 @@ class Builder {
       });
     }
 
-    function getPathFromAbsoluteURL(query, key, absURL) {
+    function getPathFromAbsoluteURL(absURL) {
       // Because the compat data is mutated out of mdn-browser-compat-data,
       // through our `packageBCD` function, it's very possible that
       // the `doc[i].type === 'browser_compatibility` has already been
       // processed.
       if (!absURL.includes("://")) {
-        return [absURL, null];
+        return absURL;
       }
       const u = url.parse(absURL);
       if (u.hostname !== "developer.mozilla.org") {
         // If URL is from a different host, return without modifying it
-        return [absURL, null];
+        return absURL;
       }
       // Return the pathname without docs directory, the base path for the
       // `Document` component the BCD table is within is /:locale/docs/ so it is
@@ -1899,34 +1899,9 @@ class Builder {
       if (slug.startsWith("/docs/")) {
         // Important! For now, to make this a slug we can understand we
         // have to have a locale and we pick `en-US` as the default.
-        slug = `/en-US${slug}`;
-      } else {
-        throw new Error(`not implemented! ${slug}`);
+        return `/en-US${slug}`;
       }
-      if (checkLinks) {
-        const slugNormalized = slug.toLowerCase();
-        console.log({ slugNormalized });
-        if (!allTitles.has(slugNormalized)) {
-          // Before we call this a broken link, we need to check if it redirects.
-          if (allRedirects.has(slugNormalized)) {
-            // Just because it's a redirect doesn't mean it ends up
-            // on a page we have.
-            // For example, there might be a redirect but where it
-            // goes to is not in this.allTitles.
-            // This can happen if it's a "fundamental redirect" for example.
-            const finalDocument = allTitles.get(
-              allRedirects.get(slugNormalized)
-            );
-            const suggestion = finalDocument ? finalDocument.mdn_url : null;
-            addBadBCDLink(query, key, slug, suggestion);
-            return [slug, suggestion];
-          } else {
-            addBadBCDLink(query, key, slug);
-            return [null, null];
-          }
-        }
-      }
-      return [slug, null];
+      throw new Error(`not implemented! ${slug}`);
     }
 
     doc.body
@@ -1941,24 +1916,38 @@ class Builder {
             block = block.__compat;
           }
           if (block.mdn_url) {
-            const [slug, suggestion] = getPathFromAbsoluteURL(
-              section.value.query,
-              key,
-              block.mdn_url
-            );
-            if (suggestion) {
-              block.mdn_url = suggestion;
-            } else if (!slug) {
-              // If it turns out that the `slug` is null, it means an
-              // `block.mdn_url` was set but it's actually wrong and won't
-              // ever work as a link because it'll just 404 immediately.
-              // So, extend the block by putting an extra boolean on the
-              // block so we can remember to not make a link to it
-              // in the BCD table renderer.
-              block.bad_slug = true;
-            } else {
-              // It could be turned into a relative link (aka. slug)
-              block.mdn_url = slug;
+            const slug = getPathFromAbsoluteURL(block.mdn_url);
+            block.mdn_url = slug;
+            if (checkLinks && slug.startsWith("/")) {
+              // Now we need to scrutinize if that's a slug we can fully
+              // recognize. (But only if it's a relative link)
+              const slugNormalized = slug.toLowerCase();
+              if (!allTitles.has(slugNormalized)) {
+                // That means trouble!
+                // But can it be salvaged with a redirect?
+                if (allRedirects.has(slugNormalized)) {
+                  // Just because it's a redirect doesn't mean it ends up
+                  // on a page we have.
+                  // For example, there might be a redirect but where it
+                  // goes to is not in this.allTitles.
+                  // This can happen if it's a "fundamental redirect" for example.
+                  const finalDocument = allTitles.get(
+                    allRedirects.get(slugNormalized)
+                  );
+                  const suggestion = finalDocument
+                    ? finalDocument.mdn_url
+                    : null;
+                  addBadBCDLink(query, key, slug, suggestion);
+                  block.mdn_url = suggestion;
+                } else {
+                  addBadBCDLink(section.value.query, key, slug);
+                  // If the slug is bad, and can't be salvaged with a redirect,
+                  // we need to pass that information on so that it can be
+                  // leveraged in the UI that displays the BCD table.
+                  // That way, it knows to not attempt to make a link out of it.
+                  block.bad_slug = true;
+                }
+              }
             }
           }
         });
