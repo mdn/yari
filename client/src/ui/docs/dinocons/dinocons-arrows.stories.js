54037b63ab1cd00ac1e77fc0bee15a988a16b98c
Schalk Neethling <sneethling@mozilla.com>
2020-08-14 10:13:29 +0200

    Latest @14-08-2020

--- client/src/ui/docs/dinocons/dinocons-arrows.stories.js
+++ client/src/ui/docs/dinocons/dinocons-arrows.stories.js
@@ -1,43 +1,13 @@
 import React from "react";
 
-import { ReactComponent as Arrow } from "@mdn/dinocons/arrows/arrow.svg";
-import { ReactComponent as Chevron } from "@mdn/dinocons/arrows/chevron.svg";
-import { ReactComponent as Triangle } from "@mdn/dinocons/arrows/triangle.svg";
+import { ReactComponent as ArrowIcon } from "@mdn/dinocons/arrows/arrow.svg";
+import { ReactComponent as ChevronIcon } from "@mdn/dinocons/arrows/chevron.svg";
+import { ReactComponent as TriangleIcon } from "@mdn/dinocons/arrows/triangle.svg";
 
 export default {
   title: "Atoms/Dinocons/Arrows",
 };
 
-const wrapperStyle = {
-  display: "flex",
-  flexWrap: "wrap",
-  gap: "20px",
-};
-
-const iconContainer = {
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  margin: "24px",
-  padding: "5px",
-  border: "1px solid #212121",
-  width: "100px",
-  height: "100px",
-};
-
-export const arrows = () => (
-  <>
-    <h2>Arrows</h2>
-    <div style={wrapperStyle}>
-      <div style={iconContainer}>
-        <Arrow />
-      </div>
-      <div style={iconContainer}>
-        <Chevron />
-      </div>
-      <div style={iconContainer}>
-        <Triangle />
-      </div>
-    </div>
-  </>
-);
+export const Arrow = () => <ArrowIcon />;
+export const Chevron = () => <ChevronIcon />;
+export const Triangle = () => <TriangleIcon />;
