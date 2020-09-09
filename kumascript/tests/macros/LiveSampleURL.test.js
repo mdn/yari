646697ade38d7da7efb7a1793f7f5b5523c515db
Ryan Johnson <rjohnson@mozilla.com>
2020-09-09 11:53:58 -0700

    make env.url relative and use instead of env.path (#1223)
    * make env.url relative and use instead of env.path
    
    * feedbacked
    
    * Update kumascript/index.js
    
    Co-authored-by: Peter Bengtsson <peterbe@mozilla.com>
    
    Co-authored-by: Peter Bengtsson <peterbe@mozilla.com>

--- kumascript/tests/macros/LiveSampleURL.test.js
+++ kumascript/tests/macros/LiveSampleURL.test.js
@@ -11,8 +11,7 @@ describeMacro("LiveSampleURL", function () {
     macro.ctx.env.live_samples = {
       base_url: "https://mdn.mozillademos.org",
     };
-    macro.ctx.env.url =
-      "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/p";
+    macro.ctx.env.url = "/en-US/docs/Web/HTML/Element/p";
     return assert.eventually.equal(
       macro.call("Example"),
       "https://mdn.mozillademos.org/en-US/docs/Web/HTML/Element/p/_samples_/Example"
@@ -23,11 +22,11 @@ describeMacro("LiveSampleURL", function () {
       base_url: "https://mdn.mozillademos.org",
     };
     macro.ctx.env.url =
-      "https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/How_to_build_custom_form_widgets";
+      "/en-US/docs/Learn/HTML/Forms/How_to_build_custom_form_widgets";
     return assert.eventually.equal(
       macro.call(
         "No_JS",
-        "https://developer.mozilla.org/en-US/docs/HTML/Forms/How_to_build_custom_form_widgets/Example_2"
+        "/en-US/docs/HTML/Forms/How_to_build_custom_form_widgets/Example_2"
       ),
       "https://mdn.mozillademos.org/en-US/docs/HTML/Forms/How_to_build_custom_form_widgets/Example_2/_samples_/No_JS"
     );
@@ -38,13 +37,9 @@ describeMacro("LiveSampleURL", function () {
     };
     macro.ctx.info.hasPage = jest.fn((path) => false);
     macro.ctx.info.getDescription = jest.fn((url) => url.toLowerCase());
-    macro.ctx.env.path = "/en-US/docs/Learn/HTML";
-    macro.ctx.env.url = `https://developer.mozilla.org${macro.ctx.env.path}`;
+    macro.ctx.env.url = "/en-US/docs/Learn/HTML";
     await expect(
-      macro.call(
-        "No_JS",
-        "https://developer.mozilla.org/en-US/docs/does/not/exist"
-      )
+      macro.call("No_JS", "/en-US/docs/does/not/exist")
     ).rejects.toThrow(
       "/en-us/docs/learn/html references /en-us/docs/does/not/exist, which does not exist"
     );
@@ -53,8 +48,7 @@ describeMacro("LiveSampleURL", function () {
     macro.ctx.env.live_samples = {
       base_url: "https://files-stage.mdn.mozit.cloud",
     };
-    macro.ctx.env.url =
-      "https://developer.allizom.org/en-US/docs/Web/CSS/background-color";
+    macro.ctx.env.url = "/en-US/docs/Web/CSS/background-color";
     return assert.eventually.equal(
       macro.call("Examples"),
       "https://files-stage.mdn.mozit.cloud/en-US/docs/Web/CSS/background-color/_samples_/Examples"
@@ -62,7 +56,7 @@ describeMacro("LiveSampleURL", function () {
   });
   itMacro("Development default settings", function (macro) {
     macro.ctx.env.live_samples = { base_url: "http://localhost:8000" };
-    macro.ctx.env.url = "http://localhost:8000/en-US/docs/Web/HTML/Element/p";
+    macro.ctx.env.url = "/en-US/docs/Web/HTML/Element/p";
     return assert.eventually.equal(
       macro.call("Example"),
       "http://localhost:8000/en-US/docs/Web/HTML/Element/p/_samples_/Example"
@@ -72,8 +66,7 @@ describeMacro("LiveSampleURL", function () {
     macro.ctx.env.live_samples = {
       base_url: "https://mdn.mozillademos.org",
     };
-    macro.ctx.env.url =
-      "https://developer.mozilla.org/zh-CN/docs/Web/CSS/flex-direction";
+    macro.ctx.env.url = "/zh-CN/docs/Web/CSS/flex-direction";
     return assert.eventually.equal(
       macro.call("例子"),
       "https://mdn.mozillademos.org/zh-CN/docs/Web/CSS/flex-direction/_samples_/%E4%BE%8B%E5%AD%90"
@@ -81,7 +74,7 @@ describeMacro("LiveSampleURL", function () {
   });
   itMacro("Development demo settings", function (macro) {
     macro.ctx.env.live_samples = { base_url: "http://demos:8000" };
-    macro.ctx.env.url = "http://localhost:8000/en-US/docs/Web/HTML/Element/p";
+    macro.ctx.env.url = "/en-US/docs/Web/HTML/Element/p";
     return assert.eventually.equal(
       macro.call("Example"),
       "http://demos:8000/en-US/docs/Web/HTML/Element/p/_samples_/Example"
