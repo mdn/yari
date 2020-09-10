d4b77f4a85244ab552f353c72a39da41f29b6c21
Peter Bengtsson <mail@peterbe.com>
2020-09-10 16:32:52 -0400

    make redirects and popularities loading lazy (#1234)
    * make redirects and popularities loading lazy
    
    * use a Map for all popularities

--- content/popularities.js
+++ content/popularities.js
@@ -3,6 +3,20 @@ const path = require("path");
 
 const { CONTENT_ROOT } = require("./constants");
 
-module.exports = JSON.parse(
-  fs.readFileSync(path.join(CONTENT_ROOT, "popularities.json"), "utf-8")
-);
+// Module-level cache
+const popularities = new Map();
+
+function getPopularities() {
+  if (!popularities.size) {
+    Object.entries(
+      JSON.parse(
+        fs.readFileSync(path.join(CONTENT_ROOT, "popularities.json"), "utf-8")
+      )
+    ).forEach(([url, value]) => {
+      popularities.set(url, value);
+    });
+  }
+  return popularities;
+}
+
+module.exports = getPopularities;
