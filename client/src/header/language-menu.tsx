11012ec63eb60520caab1e825cadfe2878a279cb
Peter Bengtsson <mail@peterbe.com>
2020-09-29 10:40:16 -0400

    simplified language selector
    Part of #1320

--- client/src/header/language-menu.tsx
+++ client/src/header/language-menu.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import LANGUAGES from "../languages.json";
-import Dropdown from "./dropdown";
 import { Translation } from "../document/types";
 
 export default function LanguageMenu({
@@ -12,6 +11,8 @@ export default function LanguageMenu({
   locale: string;
   translations: Translation[];
 }) {
+  const navigate = useNavigate();
+
   // For the menu label, we want to use the name of the document language.
   // We need a special case for English because English documents can
   // appear in pages for other locales, and in that case we need a
@@ -19,30 +20,40 @@ export default function LanguageMenu({
   // locale of the page and the locale of the document should match
   // and we can just use the document language string without translation.
   const verbose = LANGUAGES[locale];
-  const chooseLanguageString = `Current language is ${
-    (verbose && verbose.English) || locale
-  }. Choose your preferred language.`;
 
   return (
-    <Dropdown
-      id="header-language-menu"
-      componentClassName="language-menu"
-      label={(verbose && verbose.English) || locale}
-      right={true}
-      ariaOwns="language-menu"
-      ariaLabel={chooseLanguageString}
-    >
-      {translations.map((t) => {
-        const verbose = LANGUAGES[t.locale];
-        const url = `/${t.locale}/docs/${t.slug}`;
-        return (
-          <li key={url} lang={t.locale} role="menuitem">
-            <Link to={url} title={verbose ? verbose.English : t.locale}>
-              <bdi>{verbose ? verbose.native : t.locale}</bdi>
-            </Link>
-          </li>
-        );
-      })}
-    </Dropdown>
+    <form>
+      <select
+        name="language"
+        defaultValue={locale}
+        onChange={(event) => {
+          const { value: url } = event.target;
+          // If the selection was the existing document, do nothing
+          if (url !== locale) {
+            // Redirect
+            navigate(url);
+          }
+        }}
+      >
+        {/*
+          This option is alway there and always first.
+          The reason it doesn't have the `disabled` attribute is because it
+          might not render when viewing the select un-opened and instead what
+          you see is the second option.
+          The onChange callback is a protection for doing nothing if the
+          already current locale is chosen.
+         */}
+        <option value={locale}>{verbose ? verbose.native : locale}</option>
+        {translations.map((t) => {
+          const verbose = LANGUAGES[t.locale];
+          const url = `/${t.locale}/docs/${t.slug}`;
+          return (
+            <option key={url} value={url}>
+              {verbose ? verbose.native : t.locale}
+            </option>
+          );
+        })}
+      </select>
+    </form>
   );
 }
