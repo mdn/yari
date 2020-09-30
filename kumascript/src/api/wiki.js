efc601a5242eb92187029fabbfabe3404dafe56b
Peter Bengtsson <mail@peterbe.com>
2020-09-29 20:28:53 -0400

    undo change

--- kumascript/src/api/wiki.js
+++ kumascript/src/api/wiki.js
@@ -128,6 +128,7 @@ module.exports = {
     if (path.substr(-1, 1) === "/") {
       path = path.slice(0, -1);
     }
+
     var pages = this.page.subpages(path, depth, self);
 
     if (reverse == 0) {
