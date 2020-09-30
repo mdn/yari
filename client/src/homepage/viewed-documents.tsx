6b22f8aa3dde799a243937b430df2e231aa033e2
Peter Bengtsson <mail@peterbe.com>
2020-09-30 16:31:47 -0400

    recently viewed documents on the home page

---
+++ client/src/homepage/viewed-documents.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { usePageVisibility } from "./hooks";
+
+type Entry = {
+  url: string;
+  title: string;
+  timestamp: number;
+};
+
+// Simpler and cheaper than a proper library
+function friendlyDateDisplay(date: Date): string {
+  const today = new Date();
+  const dateString = date.toDateString();
+  const secondsDiff = (today.getTime() - date.getTime()) / 1000;
+  if (secondsDiff < 60) {
+    return "seconds ago";
+  }
+  if (secondsDiff < 60 * 15) {
+    return "minutes ago";
+  }
+  if (today.toDateString() === dateString) {
+    return "today";
+  }
+  const yesterday = new Date(today.getTime() - 1000 * 3600 * 24);
+  if (yesterday.toDateString() === dateString) {
+    return "yesterday";
+  }
+  return dateString;
+}
+
+export default function ViewedDocuments() {
+  const isVisible = usePageVisibility();
+  const [entries, setEntries] = useState<Entry[] | null>(null);
+
+  useEffect(() => {
+    if (isVisible) {
+      const localStorageKey = "viewed-documents";
+      const previousVisits = JSON.parse(
+        localStorage.getItem(localStorageKey) || "[]"
+      );
+      const newEntries: Entry[] = [];
+      // console.log(previousVisits);
+      for (const visit of previousVisits) {
+        newEntries.push({
+          url: visit.url,
+          title: visit.title,
+          timestamp: visit.timestamp,
+        });
+      }
+
+      if (newEntries.length) {
+        setEntries(newEntries);
+      }
+    }
+  }, [isVisible]);
+
+  return (
+    <div>
+      <h3>Recently viewed documents</h3>
+      {!entries ? (
+        <Banner>Loading recently viewed documents</Banner>
+      ) : entries.length ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Document</th>
+              <th>When</th>
+            </tr>
+          </thead>
+          <tbody>
+            {entries.map((entry) => {
+              return (
+                <tr key={entry.url}>
+                  <td>
+                    <Link to={entry.url}>{entry.title}</Link>
+                  </td>
+                  <td>{friendlyDateDisplay(new Date(entry.timestamp))}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        <Banner>No recently viewed documents at the moment</Banner>
+      )}
+    </div>
+  );
+}
+
+function Banner({ children }) {
+  return <p style={{ margin: 30, fontStyle: "italic" }}>{children}</p>;
+}
