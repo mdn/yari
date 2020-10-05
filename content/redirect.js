ca8b238391f15ca6748f40a3dc25ddc0f1f62fec
Peter Bengtsson <mail@peterbe.com>
2020-10-05 17:48:05 -0400

    CLI tool to check redirects (#1358)
    * cli tool to check redirects
    
    * prototype finished
    
    * remove dot
    
    * remove deadcode

--- content/redirect.js
+++ content/redirect.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const { CONTENT_ROOT } = require("./constants");
+const { CONTENT_ROOT, VALID_LOCALES } = require("./constants");
 const { buildURL } = require("./utils");
 
 function add(locale, oldSlug, newSlug) {
@@ -24,28 +24,71 @@ function add(locale, oldSlug, newSlug) {
 // The module level cache
 const redirects = new Map();
 
-const resolve = (url) => {
-  if (!redirects.size) {
+function load(files = null, verbose = false) {
+  if (!files) {
     const localeFolders = fs
       .readdirSync(CONTENT_ROOT)
       .map((n) => path.join(CONTENT_ROOT, n))
       .filter((filepath) => fs.statSync(filepath).isDirectory());
 
-    for (const folder of localeFolders) {
-      const redirectsFilePath = path.join(folder, "_redirects.txt");
-      if (!fs.existsSync(redirectsFilePath)) {
-        continue;
-      }
+    files = localeFolders
+      .map((folder) => path.join(folder, "_redirects.txt"))
+      .filter((filePath) => fs.existsSync(filePath));
+  }
 
-      const redirectPairs = fs
-        .readFileSync(redirectsFilePath, "utf-8")
-        .split("\n")
-        .slice(1, -1)
-        .map((line) => line.trim().split(/\s+/));
-      for (const [from, to] of redirectPairs) {
-        redirects.set(from.toLowerCase(), to);
-      }
+  function throwError(message, lineNumber, line) {
+    throw new Error(`Invalid line: ${message} (line ${lineNumber}) '${line}'`);
+  }
+
+  const validLocales = new Set([...VALID_LOCALES.values()]);
+
+  for (const redirectsFilePath of files) {
+    if (verbose) {
+      console.log(`Checking ${redirectsFilePath}`);
     }
+    const content = fs.readFileSync(redirectsFilePath, "utf-8");
+    if (!content.endsWith("\n")) {
+      throw new Error(
+        `${redirectsFilePath} must have a trailing newline character.`
+      );
+    }
+    const pairs = new Map();
+    // Parse and collect all and throw errors on bad lines
+    content.split("\n").forEach((line, i) => {
+      if (!line.trim() || line.startsWith("#")) return;
+      const split = line.trim().split(/\s+/);
+      if (split.length !== 2) {
+        throwError("Not two strings split by whitespace", i + 1, line);
+      }
+      const [from, to] = split;
+      if (!from.startsWith("/")) {
+        throwError("From-URL must start with a /", i + 1, line);
+      }
+      if (!from.includes("/docs/")) {
+        throwError("From-URL must contain '/docs/'", i + 1, line);
+      }
+      if (!validLocales.has(from.split("/")[1])) {
+        throwError(
+          `The locale prefix is not valid or wrong case '${
+            from.split("/")[1]
+          }'.`,
+          i + 1,
+          line
+        );
+      }
+      pairs.set(from.toLowerCase(), to);
+    });
+    // Now that all have been collected, transfer them to the `redirects` map
+    // but also do invariance checking.
+    for (const [from, to] of pairs) {
+      redirects.set(from.toLowerCase(), to);
+    }
+  }
+}
+
+const resolve = (url) => {
+  if (!redirects.size) {
+    load();
   }
   return redirects.get(url.toLowerCase()) || url;
 };
@@ -64,4 +107,5 @@ module.exports = {
   add,
   resolve,
   write,
+  load,
 };
---
+++ redirects/cli.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+const cli = require("caporal");
+const chalk = require("chalk");
+
+const { add, resolve, load } = require("../content/redirect");
+
+cli
+  .version("0.0.0")
+  .command("validate", "Check the _redirects.txt file(s)")
+  .action((args, options, logger) => {
+    try {
+      load(null, true);
+      logger.info(chalk.green("🍾 All is well in the world of redirects 🥂"));
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  })
+
+  .command("test", "Test a URL (pathname) to see if it redirects")
+  .argument("[urls...]")
+  .action((args, options, logger) => {
+    try {
+      for (const url of args.urls) {
+        const resolved = resolve(url);
+        if (resolved === url) {
+          logger.info(chalk.yellow(`${url.padEnd(50)} Not a redirecting URL`));
+        } else {
+          logger.info(chalk.green(`${url.padEnd(50)} -> ${resolved}`));
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  })
+
+  .command("add", "Add a new redirect")
+  .option("--locale", "Which locale (defaults to 'en-US')")
+  .argument("[from, to]")
+  .action((args, options) => {
+    throw new Error("not implemented yet");
+  });
+
+cli.parse(process.argv);
