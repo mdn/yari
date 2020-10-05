5f5c37685d06a3c2bd4e3784b5938736db655af9
Ryan Johnson <rjohnson@mozilla.com>
2020-10-05 14:43:37 -0700

    better s3 keys and lambda function (#1373)
    * better s3 keys and lambda function
    
    * feedbacked

--- deployer/aws-lambda/content-origin-request/index.js
+++ deployer/aws-lambda/content-origin-request/index.js
@@ -19,26 +19,66 @@ function slugToFolder(slug) {
     .join("/");
 }
 
+function redirect(
+  location,
+  { permanent = false, cacheControlSeconds = 0 } = {}
+) {
+  /*
+   * Create and return a redirect response.
+   */
+  let status, statusDescription, cacheControlValue;
+  if (permanent) {
+    status = 301;
+    statusDescription = "Moved Permanently";
+  } else {
+    status = 302;
+    statusDescription = "Found";
+  }
+  if (cacheControlSeconds) {
+    cacheControlValue = `max-age=${cacheControlSeconds},public`;
+  } else {
+    cacheControlValue = "no-store";
+  }
+  return {
+    status,
+    statusDescription,
+    headers: {
+      location: [
+        {
+          key: "Location",
+          value: location,
+        },
+      ],
+      "cache-control": [
+        {
+          key: "Cache-Control",
+          value: cacheControlValue,
+        },
+      ],
+    },
+  };
+}
+
 exports.handler = async (event, context) => {
   /*
    * Modify the request before it's passed to the S3 origin.
    */
   const request = event.Records[0].cf.request;
   const host = request.headers.host[0].value.toLowerCase();
+  // A document URL with a trailing slash should redirect
+  // to the same URL without the trailing slash.
+  if (
+    request.uri.endsWith("/") &&
+    request.uri.toLowerCase().includes("/docs/")
+  ) {
+    return redirect(request.uri.slice(0, -1), {
+      permanent: true,
+      cacheControlSeconds: 3600 * 24 * 30,
+    });
+  }
   // Rewrite the URI to match the keys in S3.
   // NOTE: The incoming URI should remain URI-encoded.
-  let newURI = slugToFolder(request.uri);
-  if (
-    newURI.includes("/docs/") &&
-    !newURI.endsWith("/index.json") &&
-    !newURI.endsWith("/contributors.txt")
-  ) {
-    if (!newURI.endsWith("/")) {
-      newURI += "/";
-    }
-    newURI += "index.html";
-  }
-  request.uri = newURI;
+  request.uri = slugToFolder(request.uri);
   // Rewrite the HOST header to match the S3 bucket website domain.
   // This is required only because we're using S3 as a website, which
   // we need in order to do redirects from S3. NOTE: The origin is
