2dbbe04d9aca3505657a46b44a012c9e0581989c
Florian Merz <me@fiji-flo.de>
2020-10-12 13:08:50 +0200

    fix tests

--- server/index.js
+++ server/index.js
@@ -32,11 +32,16 @@ const { staticMiddlewares } = require("./middlewares");
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  // If we have a fundamental redirect mimic out Lambda@Edge and redirect.
+  const { url: fundamentalRedirectUrl, status } = resolveFundamental(req.url);
+  if (fundamentalRedirectUrl && status) {
+    return res.redirect(status, fundamentalRedirectUrl);
+  }
+  return next();
+});
 app.use(staticMiddlewares);
 
-app.use(express.static(STATIC_ROOT));
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
@@ -151,12 +156,6 @@ app.get("/*", async (req, res) => {
     } catch (e) {
       return res.status(404).send(e.toString());
     }
-  }
-
-  // If we have a fundamental redirect mimic out Lambda@Edge and redirect.
-  const { url: fundamentalRedirectUrl, status } = resolveFundamental(req.url);
-  if (fundamentalRedirectUrl && status) {
-    return res.redirect(status, fundamentalRedirectUrl);
   }
 
   if (!req.url.includes("/docs/")) {
