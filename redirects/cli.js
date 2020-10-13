46dbb54583d745d66882c3392284b0004052d095
Peter Bengtsson <mail@peterbe.com>
2020-10-13 16:24:25 -0400

    progress

--- redirects/cli.js
+++ redirects/cli.js
@@ -2,8 +2,13 @@
 const program = require("@caporal/core").default;
 const chalk = require("chalk");
 
-const { add, resolve, load } = require("../content/redirect");
-const { VALID_LOCALES, Document } = require("../content");
+const {
+  add,
+  resolve,
+  load,
+  validateFromURL,
+  validateToURL,
+} = require("../content/redirect");
 
 program
   .version("0.0.0")
@@ -39,73 +44,21 @@ program
   .command("add", "Add a new redirect")
   .argument("<from>", "From-URL", {
     validator: (value) => {
-      const locale = value.split("/")[1];
-      if (!locale || value.split("/")[2] !== "docs") {
-        throw new Error("The from-URL is expected to be /$locale/docs/");
-      }
-      const validValues = [...VALID_LOCALES.values()];
-      if (!validValues.includes(locale)) {
-        throw new Error(`'${locale}' not in ${validValues}`);
-      }
-      const document = Document.findByURL(value);
-      if (document) {
-        throw new Error(
-          `From-URL resolves to a file on disk (${document.fileInfo.path})`
-        );
-      }
-      const resolved = resolve(value);
-      if (resolved !== value) {
-        throw new Error(
-          `${value} is already matched as a redirect (to: '${resolved}')`
-        );
-      }
+      validateFromURL(value);
       return value;
     },
   })
   .argument("<to>", "To-URL", {
     validator: (value) => {
-      // If it's not external, it has to go to a valid document
-      if (value.includes("://")) {
-        // If this throws, conveniently the validator will do its job.
-        const url = new URL(value);
-        if (url.protocol !== "https:") {
-          throw new Error("We only redirect to https://");
-        }
-      } else {
-        // Check that it's a valid document URL
-        const locale = value.split("/")[1];
-        if (!locale || value.split("/")[2] !== "docs") {
-          throw new Error("The from-URL is expected to be /$locale/docs/");
-        }
-        const validValues = [...VALID_LOCALES.values()];
-        if (!validValues.includes(locale)) {
-          throw new Error(`'${locale}' not in ${validValues}`);
-        }
-
-        // Can't point to something that redirects to something
-        const resolved = resolve(value);
-        if (resolved !== value) {
-          throw new Error(
-            `${value} is already matched as a redirect (to: '${resolved}')`
-          );
-        }
-        // XXX Commented out because how else are going to be able
-        // redirect to archived documents.
-        // // It has to match a document
-        // const document = Document.findByURL(value);
-        // if (!document) {
-        //   throw new Error(
-        //     `To-URL has to resolve to a file on disk (${document.fileInfo.path})`
-        //   );
-        // }
-      }
+      validateToURL(value);
       return value;
     },
   })
-  .action(({ args }) => {
+  .action(({ args, logger }) => {
     const { from, to } = args;
-
-    console.warn(chalk.yellow(`not implemented yet ('${from}' → '${to}')`));
+    const locale = from.split("/")[1];
+    add(locale, from, to);
+    logger.info(chalk.green(`Saved '${from}' → '${to}'`));
   });
 
 program.run();
