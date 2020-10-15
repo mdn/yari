56d86eade5649be70fa5c118531ee98861ec070e
Peter Bengtsson <mail@peterbe.com>
2020-10-15 10:14:54 -0400

    the Ever Macro Used table

--- client/src/traits/index.tsx
+++ client/src/traits/index.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { useTable, usePagination } from "react-table";
-import type types from "react-table";
-// import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+// import { useTable, usePagination } from "react-table";
+// import type types from "react-table";
+import { Link } from "react-router-dom";
 import useSWR from "swr";
 
 import "./index.scss";
@@ -14,6 +14,15 @@ interface Document {
   flaws: {
     [key: string]: string[];
   };
+  normalizedMacrosCount: {
+    [key: string]: number;
+  };
+}
+
+interface MacroInfo {
+  sourceName: string;
+  normalizedName: string;
+  totalCount: number;
 }
 
 type Metadata = {
@@ -24,6 +33,7 @@ type Metadata = {
 interface Data {
   documents: Document[];
   metadata: Metadata;
+  allMacros: MacroInfo[];
 }
 
 export default function AllTraits() {
@@ -72,7 +82,7 @@ export default function AllTraits() {
       {data && (
         <p>Data loaded ({data.metadata.count.toLocaleString()} documents)</p>
       )}
-      {data && <DisplayData documents={data.documents} />}
+      {data && <DisplayData data={data} />}
       {error && (
         <p>
           Error loading data: <code>{error.toString()}</code>
@@ -146,124 +156,279 @@ function Loading({ startLoadingTime }: { startLoadingTime: Date }) {
   );
 }
 
-function DisplayData({ documents }: { documents: Document[] }) {
-  const columns: types.Column<Document>[] = useMemo(
-    () => [
-      {
-        Header: "Title",
-        accessor: "title",
-      },
-      {
-        Header: "URL",
-        accessor: "mdn_url",
-      },
-    ],
-    []
-  );
-  const tableInstance = useTable({ columns, data: documents }, usePagination);
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    prepareRow,
-    page,
-
-    // Pagination related
-    canPreviousPage,
-    // canNextPage,
-    // pageOptions,
-    // pageCount,
-    // gotoPage,
-    // nextPage,
-    // previousPage,
-    // setPageSize,
-    // state: { pageIndex, pageSize },
-  } = tableInstance;
-
-  console.log(tableInstance);
-
-  const pageIndex = 0;
-  const pageOptions = [];
-  // const canPreviousPage = false;
-  const canNextPage = true;
-
+function DisplayData({ data }: { data: Data }) {
+  const macros = data.allMacros;
+  const documents = data.documents;
   return (
-    <div className="show-data">
-      <table {...getTableProps()}>
-        <thead>
-          {
-            // Loop over the header rows
-            headerGroups.map((headerGroup) => (
-              // Apply the header row props
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {
-                  // Loop over the headers in each row
-                  headerGroup.headers.map((column) => (
-                    // Apply the header cell props
-                    <th {...column.getHeaderProps()}>
-                      {
-                        // Render the header
-                        column.render("Header")
-                      }
-                    </th>
-                  ))
-                }
-              </tr>
-            ))
-          }
-        </thead>
-
-        {/* Apply the table body props */}
-        <tbody {...getTableBodyProps()}>
-          {
-            // Loop over the table rows
-            page.map((row) => {
-              // Prepare the row for display
-              prepareRow(row);
-              return (
-                // Apply the row props
-                <tr {...row.getRowProps()}>
-                  {
-                    // Loop over the rows cells
-                    row.cells.map((cell) => {
-                      // Apply the cell props
-                      return (
-                        <td {...cell.getCellProps()}>
-                          {
-                            // Render the cell contents
-                            cell.render("Cell")
-                          }
-                        </td>
-                      );
-                    })
-                  }
-                </tr>
-              );
-            })
-          }
-        </tbody>
-      </table>
-      <div className="pagination">
-        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-          {"<<"}
-        </button>{" "}
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          {"<"}
-        </button>{" "}
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          {">"}
-        </button>{" "}
-        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-          {">>"}
-        </button>{" "}
-        <span>
-          Page{" "}
-          <strong>
-            {pageIndex + 1} of {pageOptions.length}
-          </strong>{" "}
-        </span>
-      </div>
+    <div>
+      <AllMacroUses macros={macros} documents={documents} />
     </div>
   );
 }
+
+function AllMacroUses({
+  macros,
+  documents,
+}: {
+  macros: MacroInfo[];
+  documents: Document[];
+}) {
+  const [sortMacros, setSortMacros] = useState("alphabetically");
+  const [filterMacros, setFilterMacros] = useState("");
+  const [hideNeverused, setHideNeverused] = useState(false);
+
+  const displayMacros = macros
+    .sort((a, b) => {
+      if (sortMacros === "popularity") {
+        return b.totalCount - a.totalCount;
+      } else if (sortMacros === "popularityReverse") {
+        return a.totalCount - b.totalCount;
+      }
+      let reverse = sortMacros === "alphabeticallyReverse" ? -1 : 1;
+      return reverse * a.normalizedName.localeCompare(b.normalizedName);
+    })
+    .filter((m) => {
+      const search = filterMacros.trim().toLowerCase();
+      if (hideNeverused && !m.totalCount) return false;
+      return (
+        !(search && search.length > 1) || m.normalizedName.includes(search)
+      );
+    });
+
+  const [filterDocuments, setFilterDocuments] = useState("");
+  const displayDocuments = documents
+    .filter((d) => {
+      const search = filterDocuments.trim().toLowerCase();
+      return (
+        !(search && search.length > 2) ||
+        d.mdn_url.toLowerCase().includes(search)
+      );
+    })
+    .slice(0, 25);
+
+  return (
+    <div className="all-macros-used">
+      <h4>Every Macro Used</h4>
+      <small>
+        This is only the <i>immediate</i> uses of macros. A macro might call
+        another macro inside itself, and never mentioned in a document.
+      </small>
+      <table>
+        <thead>
+          <tr>
+            <th className="filter">
+              <div className="filter-macros">
+                <input
+                  type="search"
+                  placeholder="Filter macros"
+                  value={filterMacros}
+                  onChange={(event) => {
+                    setFilterMacros(event.target.value);
+                  }}
+                />
+                <br />
+                <label htmlFor="id_sort_macros">Sort by:</label>
+                <select
+                  id="id_sort_macros"
+                  value={sortMacros}
+                  onChange={(event) => {
+                    const { value } = event.target;
+                    setSortMacros(value);
+                  }}
+                >
+                  <option value="alphabetically">A-Z</option>
+                  <option value="alphabeticallyReverse">Z-A</option>
+                  <option value="popularity">Frequent</option>
+                  <option value="popularityReverse">Rare</option>
+                </select>
+                <br />
+                <label htmlFor="id_hide_neverused">Hide never-used</label>
+                <input
+                  type="checkbox"
+                  checked={hideNeverused}
+                  onChange={(event) => {
+                    setHideNeverused(event.target.checked);
+                  }}
+                />
+              </div>
+
+              <div className="filter-documents">
+                <input
+                  type="search"
+                  placeholder="Filter documents"
+                  value={filterDocuments}
+                  onChange={(event) => {
+                    setFilterDocuments(event.target.value);
+                  }}
+                />
+              </div>
+            </th>
+            {displayMacros.map((macro) => {
+              return (
+                <th
+                  className="macro"
+                  key={macro.normalizedName}
+                  title={`Total count across ALL documents: ${macro.totalCount.toLocaleString()}`}
+                >
+                  <span className="name">{macro.sourceName}</span>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {displayDocuments.map((document) => {
+            return (
+              <tr key={document.mdn_url}>
+                <td className="mdn_url">
+                  <Link to={document.mdn_url} title={`"${document.title}"`}>
+                    {document.mdn_url.replace("/en-US/docs", "…")}
+                  </Link>
+                </td>
+                {displayMacros.map((macro) => {
+                  const count =
+                    document.normalizedMacrosCount[macro.normalizedName] || 0;
+                  return (
+                    <td
+                      className="count"
+                      key={macro.normalizedName}
+                      title={macro.sourceName}
+                    >
+                      {count ? (
+                        <b>{count}</b>
+                      ) : (
+                        <span className="zero">{count}</span>
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+// function DisplayData({ documents }: { documents: Document[] }) {
+//   const columns: types.Column<Document>[] = useMemo(
+//     () => [
+//       {
+//         Header: "Title",
+//         accessor: "title",
+//       },
+//       {
+//         Header: "URL",
+//         accessor: "mdn_url",
+//       },
+//     ],
+//     []
+//   );
+//   const tableInstance = useTable({ columns, data: documents }, usePagination);
+
+//   const {
+//     getTableProps,
+//     getTableBodyProps,
+//     headerGroups,
+//     prepareRow,
+//     page,
+
+//     // Pagination related
+//     canPreviousPage,
+//     // canNextPage,
+//     // pageOptions,
+//     // pageCount,
+//     // gotoPage,
+//     // nextPage,
+//     // previousPage,
+//     // setPageSize,
+//     // state: { pageIndex, pageSize },
+//   } = tableInstance;
+
+//   console.log(tableInstance);
+
+//   const pageIndex = 0;
+//   const pageOptions = [];
+//   // const canPreviousPage = false;
+//   const canNextPage = true;
+
+//   return (
+//     <div className="show-data">
+//       <table {...getTableProps()}>
+//         <thead>
+//           {
+//             // Loop over the header rows
+//             headerGroups.map((headerGroup) => (
+//               // Apply the header row props
+//               <tr {...headerGroup.getHeaderGroupProps()}>
+//                 {
+//                   // Loop over the headers in each row
+//                   headerGroup.headers.map((column) => (
+//                     // Apply the header cell props
+//                     <th {...column.getHeaderProps()}>
+//                       {
+//                         // Render the header
+//                         column.render("Header")
+//                       }
+//                     </th>
+//                   ))
+//                 }
+//               </tr>
+//             ))
+//           }
+//         </thead>
+
+//         {/* Apply the table body props */}
+//         <tbody {...getTableBodyProps()}>
+//           {
+//             // Loop over the table rows
+//             page.map((row) => {
+//               // Prepare the row for display
+//               prepareRow(row);
+//               return (
+//                 // Apply the row props
+//                 <tr {...row.getRowProps()}>
+//                   {
+//                     // Loop over the rows cells
+//                     row.cells.map((cell) => {
+//                       // Apply the cell props
+//                       return (
+//                         <td {...cell.getCellProps()}>
+//                           {
+//                             // Render the cell contents
+//                             cell.render("Cell")
+//                           }
+//                         </td>
+//                       );
+//                     })
+//                   }
+//                 </tr>
+//               );
+//             })
+//           }
+//         </tbody>
+//       </table>
+//       <div className="pagination">
+//         <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+//           {"<<"}
+//         </button>{" "}
+//         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+//           {"<"}
+//         </button>{" "}
+//         <button onClick={() => nextPage()} disabled={!canNextPage}>
+//           {">"}
+//         </button>{" "}
+//         <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+//           {">>"}
+//         </button>{" "}
+//         <span>
+//           Page{" "}
+//           <strong>
+//             {pageIndex + 1} of {pageOptions.length}
+//           </strong>{" "}
+//         </span>
+//       </div>
+//     </div>
+//   );
+// }
--- server/traits.js
+++ server/traits.js
@@ -1,7 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
 
 const { Document } = require("../content");
 const { analyzeDocument } = require("../build");
+const { normalizeMacroName } = require("../kumascript/src/render.js");
+
+const MACROS_ROOT = path.join(__dirname, "..", "kumascript", "macros");
+console.assert(fs.existsSync(MACROS_ROOT), `${MACROS_ROOT} does not exist`);
 
 const router = express();
 
@@ -16,18 +23,47 @@ router.get("/", async (req, res) => {
     //   : await analyzeDocuments();
     // _cachedAnalyzedDocuments = documents;
     const documents = await analyzeDocuments();
+    const allMacros = getAllMacros(documents);
     const t1 = new Date();
     metadata.tookSeconds = (t1 - t0) / 1000;
     metadata.count = documents.length;
     res.json({
       documents,
       metadata,
+      allMacros,
     });
   } catch (error) {
     console.error(error);
     res.status(500).send(error.toString());
   }
 });
+
+function getAllMacros(documents) {
+  const counts = new Map();
+  documents.forEach((document) => {
+    Object.entries(document.normalizedMacrosCount).forEach(([name, count]) => {
+      counts.set(name, (counts.get(name) || 0) + count);
+    });
+  });
+
+  const root = MACROS_ROOT;
+  const files = fs.readdirSync(root);
+  const allMacros = [];
+  for (const name of files) {
+    const filepath = path.join(root, name);
+    if (name.endsWith(".ejs") && !fs.statSync(filepath).isDirectory()) {
+      const sourceName = name.replace(/\.ejs$/, "");
+      const normalizedName = normalizeMacroName(sourceName);
+      totalCount = counts.get(normalizedName) || 0;
+      allMacros.push({
+        sourceName,
+        normalizedName,
+        totalCount,
+      });
+    }
+  }
+  return allMacros;
+}
 
 async function analyzeDocuments() {
   const documents = Document.findAll();
