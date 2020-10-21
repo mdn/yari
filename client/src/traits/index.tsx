cb0021d3edd49b9d92e93e9f19415c7d994cac81
Peter Bengtsson <mail@peterbe.com>
2020-10-21 09:56:45 -0400

    still not sure about react-table

--- client/src/traits/index.tsx
+++ client/src/traits/index.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-// import { useTable, usePagination } from "react-table";
-// import type types from "react-table";
 import { Link } from "react-router-dom";
 import useSWR from "swr";
+
+import { DisplayDataTable } from "./data-table";
 
 import "./index.scss";
 
@@ -157,11 +157,10 @@ function Loading({ startLoadingTime }: { startLoadingTime: Date }) {
 }
 
 function DisplayData({ data }: { data: Data }) {
-  const macros = data.allMacros;
-  const documents = data.documents;
   return (
     <div>
-      <AllMacroUses macros={macros} documents={documents} />
+      <AllMacroUses macros={data.allMacros} documents={data.documents} />
+      {/* <DisplayDataTable documents={data.documents} /> */}
     </div>
   );
 }
@@ -310,125 +309,3 @@ function AllMacroUses({
     </div>
   );
 }
-
-// function DisplayData({ documents }: { documents: Document[] }) {
-//   const columns: types.Column<Document>[] = useMemo(
-//     () => [
-//       {
-//         Header: "Title",
-//         accessor: "title",
-//       },
-//       {
-//         Header: "URL",
-//         accessor: "mdn_url",
-//       },
-//     ],
-//     []
-//   );
-//   const tableInstance = useTable({ columns, data: documents }, usePagination);
-
-//   const {
-//     getTableProps,
-//     getTableBodyProps,
-//     headerGroups,
-//     prepareRow,
-//     page,
-
-//     // Pagination related
-//     canPreviousPage,
-//     // canNextPage,
-//     // pageOptions,
-//     // pageCount,
-//     // gotoPage,
-//     // nextPage,
-//     // previousPage,
-//     // setPageSize,
-//     // state: { pageIndex, pageSize },
-//   } = tableInstance;
-
-//   console.log(tableInstance);
-
-//   const pageIndex = 0;
-//   const pageOptions = [];
-//   // const canPreviousPage = false;
-//   const canNextPage = true;
-
-//   return (
-//     <div className="show-data">
-//       <table {...getTableProps()}>
-//         <thead>
-//           {
-//             // Loop over the header rows
-//             headerGroups.map((headerGroup) => (
-//               // Apply the header row props
-//               <tr {...headerGroup.getHeaderGroupProps()}>
-//                 {
-//                   // Loop over the headers in each row
-//                   headerGroup.headers.map((column) => (
-//                     // Apply the header cell props
-//                     <th {...column.getHeaderProps()}>
-//                       {
-//                         // Render the header
-//                         column.render("Header")
-//                       }
-//                     </th>
-//                   ))
-//                 }
-//               </tr>
-//             ))
-//           }
-//         </thead>
-
-//         {/* Apply the table body props */}
-//         <tbody {...getTableBodyProps()}>
-//           {
-//             // Loop over the table rows
-//             page.map((row) => {
-//               // Prepare the row for display
-//               prepareRow(row);
-//               return (
-//                 // Apply the row props
-//                 <tr {...row.getRowProps()}>
-//                   {
-//                     // Loop over the rows cells
-//                     row.cells.map((cell) => {
-//                       // Apply the cell props
-//                       return (
-//                         <td {...cell.getCellProps()}>
-//                           {
-//                             // Render the cell contents
-//                             cell.render("Cell")
-//                           }
-//                         </td>
-//                       );
-//                     })
-//                   }
-//                 </tr>
-//               );
-//             })
-//           }
-//         </tbody>
-//       </table>
-//       <div className="pagination">
-//         <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-//           {"<<"}
-//         </button>{" "}
-//         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-//           {"<"}
-//         </button>{" "}
-//         <button onClick={() => nextPage()} disabled={!canNextPage}>
-//           {">"}
-//         </button>{" "}
-//         <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-//           {">>"}
-//         </button>{" "}
-//         <span>
-//           Page{" "}
-//           <strong>
-//             {pageIndex + 1} of {pageOptions.length}
-//           </strong>{" "}
-//         </span>
-//       </div>
-//     </div>
-//   );
-// }
