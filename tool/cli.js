d3467338ff9b93a523498ed7b33a6a9c70ca70c2
Florian Merz <me@fiji-flo.de>
2020-10-27 16:27:37 +0100

    tool to add redirects (#1510)
    * Tool to add redirects
    
    Part of #1418
    
    * progress
    
    * conslidate move/remove
    
    * add document move / delete to cli
    
    * tooling: default to en-US
    
    * use git to move files around
    
    * work on document edit / create
    
    * fix github action
    
    * default to yes for cli tool
    
    * fix non git content root
    
    * add more commands to cli
    
    * add description to commands
    
    * Error handling when moving slugs + verbose level.
    
    * fix redirect add
    
    * rename import
    
    * move to dash separated commands
    
    * Update tool/cli.js
    
    Co-authored-by: Peter Bengtsson <peterbe@mozilla.com>
    
    * remove redirects from scripts (replaced with tool)
    
    * fix yarn
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    Co-authored-by: Peter Bengtsson <peterbe@mozilla.com>

---
+++ tool/cli.js
@@ -0,0 +1,222 @@
+#!/usr/bin/env node
+const program = require("@caporal/core").default;
+const chalk = require("chalk");
+const prompts = require("prompts");
+const openEditor = require("open-editor");
+const open = require("open");
+const fs = require("fs");
+const path = require("path");
+
+const { DEFAULT_LOCALE, VALID_LOCALES } = require("@yari-internal/constants");
+const { Redirect, Document, buildURL } = require("../content");
+
+const PORT = parseInt(process.env.SERVER_PORT || "5000");
+
+function tryOrExit(f) {
+  return async ({ options = {}, ...args }) => {
+    try {
+      await f({ options, ...args });
+    } catch (error) {
+      console.error(chalk.red(error.message));
+      if (options.verbose) {
+        console.error(chalk.red(error.stack));
+      }
+      process.exit(1);
+    }
+  };
+}
+
+program
+  .version("0.0.0")
+  .disableGlobalOption("--silent")
+  .command("redirect-validate", "Check the _redirects.txt file(s)")
+  .action(
+    tryOrExit(({ logger }) => {
+      Redirect.load(null, true);
+      logger.info(chalk.green("🍾 All is well in the world of redirects 🥂"));
+    })
+  )
+
+  .command("redirect-test", "Test a URL (pathname) to see if it redirects")
+  .argument("[urls...]", "URLs to test")
+  .action(
+    tryOrExit(({ args, logger }) => {
+      for (const url of args.urls) {
+        const resolved = Redirect.resolve(url);
+        if (resolved === url) {
+          logger.info(chalk.yellow(`${url.padEnd(50)} Not a redirecting URL`));
+        } else {
+          logger.info(chalk.green(`${url.padEnd(50)} -> ${resolved}`));
+        }
+      }
+    })
+  )
+
+  .command("redirect-add", "Add a new redirect")
+  .argument("<from>", "From-URL", {
+    validator: (value) => {
+      Redirect.validateFromURL(value);
+      return value;
+    },
+  })
+  .argument("<to>", "To-URL", {
+    validator: (value) => {
+      Redirect.validateToURL(value);
+      return value;
+    },
+  })
+  .action(
+    tryOrExit(({ args, logger }) => {
+      const { from, to } = args;
+      const locale = from.split("/")[1];
+      Redirect.add(locale, [[from, to]]);
+      logger.info(chalk.green(`Saved '${from}' → '${to}'`));
+    })
+  )
+
+  .command("content-delete", "Delete content")
+  .argument("<slug>", "Slug")
+  .argument("[locale]", "Locale", {
+    default: DEFAULT_LOCALE,
+    validator: [...VALID_LOCALES.values()],
+  })
+  .option("-r, --recursive", "Delete content recursively", { default: false })
+  .option("--redirect <redirect>", "Redirect document to <redirect>")
+  .option("-y, --yes", "Assume yes", { default: false })
+  .action(
+    tryOrExit(async ({ args, options }) => {
+      const { slug, locale } = args;
+      const { recursive, redirect, yes } = options;
+      const changes = Document.remove(slug, locale, {
+        recursive,
+        redirect,
+        dry: true,
+      });
+      console.log(chalk.green(`Will remove ${changes.length} documents:`));
+      console.log(chalk.red(changes.join("\n")));
+      if (redirect) {
+        console.log(chalk.green(`redirecting to: ${redirect}`));
+      }
+      const { run } = yes
+        ? true
+        : await prompts({
+            type: "confirm",
+            message: "Proceed?",
+            name: "run",
+            initial: true,
+          });
+      if (run) {
+        const removed = Document.remove(slug, locale, { recursive, redirect });
+        console.log(chalk.green(`Moved ${removed.length} documents.`));
+      }
+    })
+  )
+
+  .command("content-move", "Move content to a new slug")
+  .argument("<oldSlug>", "Old slug")
+  .argument("<newSlug>", "New slug")
+  .argument("[locale]", "Locale", {
+    default: DEFAULT_LOCALE,
+    validator: [...VALID_LOCALES.values()],
+  })
+  .option("-y, --yes", "Assume yes", { default: false })
+  .action(
+    tryOrExit(async ({ args, options }) => {
+      const { oldSlug, newSlug, locale } = args;
+      const { yes } = options;
+      const changes = Document.move(oldSlug, newSlug, locale, {
+        dry: true,
+      });
+      console.log(
+        chalk.green(
+          `Will move ${changes.length} documents from ${oldSlug} to ${newSlug} for ${locale}`
+        )
+      );
+      console.log(
+        changes
+          .map(([from, to]) => `${chalk.red(from)} → ${chalk.green(to)}`)
+          .join("\n")
+      );
+      const { run } = yes
+        ? true
+        : await prompts({
+            type: "confirm",
+            message: "Proceed?",
+            name: "run",
+            initial: true,
+          });
+      if (run) {
+        const moved = Document.move(oldSlug, newSlug, locale);
+        console.log(chalk.green(`Moved ${moved.length} documents.`));
+      }
+    })
+  )
+
+  .command("content-edit", "Spawn your EDITOR for an existing slug")
+  .argument("<slug>", "Slug of the document in question")
+  .argument("[locale]", "Locale", {
+    default: DEFAULT_LOCALE,
+    validator: [...VALID_LOCALES.values()],
+  })
+  .action(
+    tryOrExit(({ args }) => {
+      const { slug, locale } = args;
+      if (!Document.exists(slug, locale)) {
+        throw new Error(`${slug} does not exists for ${locale}`);
+      }
+      const filePath = Document.fileForSlug(slug, locale);
+      openEditor([filePath]);
+    })
+  )
+
+  .command("content-create", "Spawn your Editor for a new slug")
+  .argument("<slug>", "Slug of the document in question")
+  .argument("[locale]", "Locale", {
+    default: DEFAULT_LOCALE,
+    validator: [...VALID_LOCALES.values()],
+  })
+  .action(
+    tryOrExit(({ args }) => {
+      const { slug, locale } = args;
+      const parentSlug = Document.parentSlug(slug);
+      if (!Document.exists(parentSlug, locale)) {
+        throw new Error(`Parent ${parentSlug} does not exists for ${locale}`);
+      }
+      if (Document.exists(slug, locale)) {
+        throw new Error(`${slug} already exists for ${locale}`);
+      }
+      const filePath = Document.fileForSlug(slug, locale);
+      fs.mkdirSync(path.basename(filePath), { recursive: true });
+      openEditor([filePath]);
+    })
+  )
+
+  .command("content-validate", "Validate a document")
+  .argument("<slug>", "Slug of the document in question")
+  .argument("[locale]", "Locale", {
+    default: DEFAULT_LOCALE,
+    validator: [...VALID_LOCALES.values()],
+  })
+  .action(
+    tryOrExit(async ({ args }) => {
+      const { slug, locale } = args;
+      Document.validate(slug, locale);
+      console.log(chalk.green("✓ All seems fine"));
+    })
+  )
+
+  .command("content-preview", "Open a preview of a slug")
+  .argument("<slug>", "Slug of the document in question")
+  .argument("[locale]", "Locale", {
+    default: DEFAULT_LOCALE,
+    validator: [...VALID_LOCALES.values()],
+  })
+  .action(
+    tryOrExit(async ({ args }) => {
+      const { slug, locale } = args;
+      const url = `http://localhost:${PORT}${buildURL(locale, slug)}`;
+      await open(url);
+    })
+  );
+
+program.run();
