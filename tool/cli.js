3d1684c1a46d7e54260b88dfec283674defc0715
Ryan Johnson <escattone@gmail.com>
2020-11-05 14:22:45 -0800

    fix another typo in tool/cli.js

--- tool/cli.js
+++ tool/cli.js
@@ -206,7 +206,7 @@ program
       let okay = true;
       const document = Document.findByURL(buildURL(locale, slug));
       if (!document) {
-        throw new Error(`Slug ${slug} does not exists for ${locale}`);
+        throw new Error(`Slug ${slug} does not exist for ${locale}`);
       }
       const { doc } = await buildDocument(document);
 
