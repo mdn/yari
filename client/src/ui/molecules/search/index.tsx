7331c09ecfdbff3ca9e6826ffd13366eb756df2c
Peter Bengtsson <mail@peterbe.com>
2020-11-12 17:27:58 -0500

    disable auto-complete search in non-CRUD mode

--- client/src/ui/molecules/search/index.tsx
+++ client/src/ui/molecules/search/index.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 
-import { SearchNavigateWidget } from "../../../search";
+import { SearchNavigateWidget, BasicSearchWidget } from "../../../search";
+import { CRUD_MODE } from "../../../constants";
 
 import "./index.scss";
 
 export function Search(props) {
   return (
     <div className="header-search">
-      <SearchNavigateWidget {...props} />
+      {/* See the code comment next to the <BasicSearchWidget> component */}
+      {CRUD_MODE ? <SearchNavigateWidget {...props} /> : <BasicSearchWidget />}
     </div>
   );
 }
