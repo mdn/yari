8c0a640c3918b6e97c22d38d6ecfac4ae3094af1
Peter Bengtsson <mail@peterbe.com>
2020-11-13 11:22:21 -0500

    Importer replaces an empty string with an empty string
    Fixes #1736

--- import/import.js
+++ import/import.js
@@ -1115,7 +1115,6 @@ const _DELETE_STRINGS = [
   new RegExp(
     '<p class="hidden">The source for this interactive example is stored in a GitHub repository. If you\'d like to contribute to the interactive examples project, please clone <a href="https://github.com/mdn/interactive-examples">https://github.com/mdn/interactive-examples</a> and send us a pull request.</p>'
   ),
-  ``,
 ];
 function getCleanedKumaHTML(html) {
   // You can't parse the Kuma HTML because it's not actually HTML. It's
