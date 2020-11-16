10f11765de01e409268cc9b8abda03cb78a02967
Peter Bengtsson <mail@peterbe.com>
2020-11-16 11:07:52 -0500

    add a useful comment

--- client/src/ui/atoms/main-content/index.tsx
+++ client/src/ui/atoms/main-content/index.tsx
@@ -7,6 +7,8 @@ interface Props {
 export function MainContentContainer(props: Props) {
   return (
     <main
+      // This exists for the benefit of a11y navigation which
+      // uses anchor links to focus in on the content.
       id="content"
       className={`main-content ${
         props.extraClassName ? props.extraClassName : ""
