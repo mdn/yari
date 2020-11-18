0919d707d1a9c8ab2219a650048ffe323f30732a
Peter Bengtsson <mail@peterbe.com>
2020-11-18 14:59:31 -0500

    English fallback on 404
    Fixes #1340

--- client/src/page-not-found/index.tsx
+++ client/src/page-not-found/index.tsx
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
+import useSWR from "swr";
 
+import LANGUAGES_RAW from "../languages.json";
+import { Doc } from "../document/types";
 import { PageContentContainer } from "../ui/atoms/page-content";
 import "./index.scss";
+
+const LANGUAGES = new Map(
+  Object.entries(LANGUAGES_RAW).map(([locale, data]) => {
+    return [locale.toLowerCase(), data];
+  })
+);
+console.log(LANGUAGES);
 
 // NOTE! To hack on this component, you have to use a trick to even get to this
 // unless you use the Express server on localhost:5000.
@@ -42,10 +52,81 @@ export function PageNotFound() {
             Sorry, the page <code>{url}</code> could not be found.
           </p>
         )}
+
+        {url && <FallbackLink url={url} />}
+
         <p>
           <a href="/">Go back to the home page</a>
         </p>
       </PageContentContainer>
     </div>
   );
+}
+
+function FallbackLink({ url }: { url: string }) {
+  const { locale } = useParams();
+  const location = useLocation();
+  // useSWR();
+  // const fallbackCheckURL = `/en-US/` + location
+  const [fallbackCheckURL, setFallbackCheckURL] = useState<null | string>(null);
+
+  const { error, data: document } = useSWR<null | Doc>(
+    fallbackCheckURL,
+    async (url) => {
+      const response = await fetch(url);
+      if (response.ok) {
+        const { doc } = await response.json();
+        return doc;
+      }
+      return null;
+    },
+    { revalidateOnFocus: false }
+  );
+  useEffect(() => {
+    if (url && locale.toLowerCase() !== "en-us") {
+      // What if we attempt to see if it would be something there in English?
+      console.log("KNOW", { locale, url, location });
+      // We'll use the `index.json` version of the URL
+      let enUSURL = url.replace(`/${locale}/`, "/en-US/");
+      // But of the benefit of local development, devs can use `/_404/`
+      // instead of `/docs/` to simulate getting to the Page not found page.
+      // So remove that when constructing the English index.json URL.
+      enUSURL = enUSURL.replace("/_404/", "/docs/");
+
+      if (!enUSURL.endsWith("/")) {
+        enUSURL += "/";
+      }
+      enUSURL += "index.json";
+      setFallbackCheckURL(enUSURL);
+    }
+  }, [url, locale, location]);
+
+  console.log({ error, document });
+
+  if (document) {
+    console.log(LANGUAGES);
+
+    console.log({ locale });
+
+    return (
+      <div className="fallback-document">
+        <p>
+          <b>Good news!</b> The page you requested doesn't exist in{" "}
+          <b>{LANGUAGES.get(locale.toLowerCase())?.English}</b> but it exists in{" "}
+          <b>English</b>
+        </p>
+        <p>
+          <Link to={document.mdn_url}>
+            <b>{document.title}</b>
+          </Link>
+          <br />
+          <Link to={document.mdn_url}>
+            <small>{document.mdn_url}</small>
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+  return null;
 }
