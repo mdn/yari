cd19f9057c3422d0c1536dcee5f884d9168dddcc
Peter Bengtsson <mail@peterbe.com>
2020-12-05 04:15:24 -0500

    disable auto-complete search in non-CRUD mode (#1726)
    Co-authored-by: Schalk Neethling <sneethling@mozilla.com>

---
+++ client/src/ui/molecules/search/lazy-search-widget.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { SearchNavigateWidget } from "../../../search";
+
+export default function LazySearchNavigateWidget(props) {
+  return <SearchNavigateWidget {...props} />;
+}
