519ed3bc89cec4c69e4c3409f4d582b88a237227
Peter Bengtsson <mail@peterbe.com>
2020-12-09 18:35:21 -0500

    oops fix

--- build/resolve-bcd.js
+++ build/resolve-bcd.js
@@ -4,7 +4,7 @@ const bcd = require("@mdn/browser-compat-data");
 
 function packageBCD(query) {
   let data = query.split(".").reduce((prev, curr) => {
-    return prev && Object.prototype.hasOwnProperty.call(prev, "curr")
+    return prev && Object.prototype.hasOwnProperty.call(prev, curr)
       ? prev[curr]
       : undefined;
   }, bcd);
