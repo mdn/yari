dbf0576538502ca9d3317ead230e99a160138eb5
Schalk Neethling <sneethling@mozilla.com>
2020-12-11 17:00:19 +0200

    rewrite in-page-callout (#1794)
    * rewrite in-page-callout
    
    In support of https://github.com/mdn/yari/pull/1793, rewrite instances of in-page-callout with just callout. We also no longer have special cases so, we also remove the webdev class.
    
    fix #1606
    
    * add rewriteInPageCallout function to build/index.js
    
    * change based on review feedback

--- import/import.js
+++ import/import.js
@@ -1127,6 +1127,12 @@ function getCleanedRenderedHTML(html) {
     mutations++;
   });
 
+  $("div.in-page-callout").each((i, element) => {
+    $(element).removeClass("in-page-callout webdev");
+    $(element).addClass("callout");
+    mutations++;
+  });
+
   $("div.bc-data[id]").each((i, element) => {
     const $element = $(element);
     $element.empty();
