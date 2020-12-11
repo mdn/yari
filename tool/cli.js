5edc70e2a8f25e249ad9dad6252cf3fa1669ecb3
Peter Bengtsson <mail@peterbe.com>
2020-12-11 16:39:03 -0500

    Language drop down lists some languages twice (#2070)
    * Language drop down lists some languages twice
    
    Fixes #2034
    
    * uniqifyTranslationsOf function
    
    * fix for when there are NO translations at all
    
    * fix eslint problem
    
    * starting a reporting tool
    
    * fix the eslint problem

--- tool/cli.js
+++ tool/cli.js
@@ -9,7 +9,13 @@ const openEditor = require("open-editor");
 const open = require("open");
 
 const { DEFAULT_LOCALE, VALID_LOCALES } = require("../libs/constants");
-const { CONTENT_ROOT, Redirect, Document, buildURL } = require("../content");
+const {
+  CONTENT_ROOT,
+  CONTENT_TRANSLATED_ROOT,
+  Redirect,
+  Document,
+  buildURL,
+} = require("../content");
 const { buildDocument, gatherGitHistory } = require("../build");
 
 const PORT = parseInt(process.env.SERVER_PORT || "5000");
@@ -351,6 +357,67 @@ program
       if (run) {
         buildDocument(document, { fixFlaws: true, fixFlawsVerbose: true });
       }
+    })
+  )
+
+  .command("redundant-translations", "Find redundant translations")
+  .action(
+    tryOrExit(async () => {
+      if (!CONTENT_TRANSLATED_ROOT) {
+        throw new Error("CONTENT_TRANSLATED_ROOT not set");
+      }
+      if (!fs.existsSync(CONTENT_TRANSLATED_ROOT)) {
+        throw new Error(`${CONTENT_TRANSLATED_ROOT} does not exist`);
+      }
+      const documents = Document.findAll();
+      if (!documents.count) {
+        throw new Error("No documents to analyze");
+      }
+      // Build up a map of translations by their `translation_of`
+      const map = new Map();
+      for (const document of documents.iter()) {
+        if (!document.isTranslated) continue;
+        const { translation_of, locale } = document.metadata;
+        if (!map.has(translation_of)) {
+          map.set(translation_of, new Map());
+        }
+        if (!map.get(translation_of).has(locale)) {
+          map.get(translation_of).set(locale, []);
+        }
+        map
+          .get(translation_of)
+          .get(locale)
+          .push(
+            Object.assign(
+              { filePath: document.fileInfo.path },
+              document.metadata
+            )
+          );
+      }
+      // Now, let's investigate those with more than 1
+      let sumENUS = 0;
+      let sumTotal = 0;
+      for (const [translation_of, localeMap] of map) {
+        for (const [, metadatas] of localeMap) {
+          if (metadatas.length > 1) {
+            // console.log(translation_of, locale, metadatas);
+            sumENUS++;
+            sumTotal += metadatas.length;
+            console.log(
+              `https://developer.allizom.org/en-US/docs/${translation_of}`
+            );
+            for (const metadata of metadatas) {
+              console.log(metadata);
+            }
+          }
+        }
+      }
+      console.warn(
+        `${sumENUS} en-US documents have multiple translations with the same locale`
+      );
+      console.log(
+        `In total, ${sumTotal} translations that share the same translation_of`
+      );
     })
   );
 
