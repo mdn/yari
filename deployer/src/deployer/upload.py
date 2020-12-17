a6e527bed098a6d9bff19dce89cead18dad18af5
Peter Bengtsson <mail@peterbe.com>
2020-12-17 11:21:56 -0500

    deployer sets wrong URL on external URL redirects
    Fixes #350

--- deployer/src/deployer/upload.py
+++ deployer/src/deployer/upload.py
@@ -274,7 +274,7 @@ class BucketManager:
     def get_redirect_keys(self, from_url, to_url):
         return (
             f"{self.key_prefix}{from_url.strip('/').lower()}",
-            f"/{to_url.strip('/')}",
+            f"/{to_url.strip('/')}" if to_url.startswith("/") else to_url,
         )
 
     def get_bucket_objects(self):
