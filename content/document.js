fd16a2a715dea5a9a3c9cad541e30f53bbb17169
Peter Bengtsson <mail@peterbe.com>
2021-01-06 09:07:31 -0500

    debugging

--- content/document.js
+++ content/document.js
@@ -167,7 +167,7 @@ const read = memoize((folder) => {
   let root = null;
   for (const possibleRoot of ROOTS) {
     const possibleFilePath = path.join(possibleRoot, getHTMLPath(folder));
-    console.log({ possiblePath });
+    console.log({ possibleFilePath });
     if (fs.existsSync(possibleFilePath)) {
       root = possibleRoot;
       filePath = possibleFilePath;
