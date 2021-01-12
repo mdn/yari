17d990ef77326dcc7e34b69459a8d61075e9272a
Mattia Pontonio <44380480+mattiapontonio@users.noreply.github.com>
2021-01-12 12:03:57 +0100

    JTS to TOC
    https://en.m.wikipedia.org/wiki/Table_of_contents

--- client/src/document/organisms/toc/index.tsx
+++ client/src/document/organisms/toc/index.tsx
@@ -31,7 +31,7 @@ export function TOC({ toc }: { toc: Toc[] }) {
       {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
       <section className="document-toc" onClick={sendTOCClicks}>
         <header>
-          <h2>Jump to section</h2>
+          <h2>Table of contents</h2>
           <button
             type="button"
             className="ghost toc-trigger-mobile"
@@ -41,7 +41,7 @@ export function TOC({ toc }: { toc: Toc[] }) {
             aria-controls="toc-entries"
             aria-expanded={showTOC}
           >
-            Jump to section
+            Table of contents
           </button>
         </header>
         <ul id="toc-entries" className={showTOC ? "show-toc" : undefined}>
