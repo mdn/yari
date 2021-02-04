7d6b6f6b407872f5dadbead56a8b158383232e18
rjohnson <rjohnson@mozilla.com>
2021-02-03 17:01:22 -0800

    update yarn.lock for content-origin-request

--- deployer/aws-lambda/content-origin-response/index.js
+++ deployer/aws-lambda/content-origin-response/index.js
@@ -1,13 +1,26 @@
 exports.handler = async (event) => {
   /*
-   * Modify the response before it's cached by CloudFront.
+   * This Lambda@Edge function is designed to handle origin-response
+   * events, so for example we can modify the response before it's
+   * cached by CloudFront. More specifically, when serving content
+   * from S3, only a small set of common headers, like Cache-Control
+   * and Content-Type, can be be associated and served with the content.
+   * The other headers are added to the response here.
    */
+  const request = event.Records[0].cf.request;
   const response = event.Records[0].cf.response;
-  response.headers["x-frame-options"] = [
-    { key: "X-Frame-Options", value: "DENY" },
-  ];
+
+  // The live-sample pages should never respond with an X-Frame-Options
+  // header, because they're explicitly created for rendering within an
+  // iframe on a different origin.
+  if (!request.uri.toLowerCase().includes("/_samples_/")) {
+    response.headers["x-frame-options"] = [
+      { key: "X-Frame-Options", value: "DENY" },
+    ];
+  }
   response.headers["strict-transport-security"] = [
     { key: "Strict-Transport-Security", value: "max-age=63072000" },
   ];
+
   return response;
 };
