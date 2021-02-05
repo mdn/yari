9aaf8b3a3fd0216e485bce23e85291c5ab00420c
Nick Schonning <nschonni@gmail.com>
2021-02-05 00:00:39 -0500

    fix: ESLint no-nested-ternary

--- build/utils.js
+++ build/utils.js
@@ -3,7 +3,13 @@ function humanFileSize(size) {
   let i = Math.floor(Math.log(size) / Math.log(1024));
   let num = size / Math.pow(1024, i);
   let round = Math.round(num);
-  num = round < 10 ? num.toFixed(2) : round < 100 ? num.toFixed(1) : round;
+  if (round < 10) {
+    num = num.toFixed(2);
+  } else if (round < 100) {
+    num = num.toFixed(1);
+  } else {
+    num = round;
+  }
   return `${num} ${"KMGTPEZY"[i - 1]}B`;
 }
 
