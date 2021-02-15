0ce961de903f2852d7540fb170ef829b2eab13f1
Florian Merz <me@fiji-flo.de>
2021-02-15 13:56:08 +0100

    build en-us too

--- para.sh
+++ para.sh
@@ -1,1 +1,1 @@
-parallel ./build.sh ::: ar bg bn ca de el es fa fi fr he hi-in hu id it ja kab ko ms my nl pl pt-br pt-pt ru sv-se th tr uk vi zh-cn zh-tw
+parallel ./build.sh ::: ar bg bn ca de el en-us es fa fi fr he hi-in hu id it ja kab ko ms my nl pl pt-br pt-pt ru sv-se th tr uk vi zh-cn zh-tw
