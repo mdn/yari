62ec395cc0ad07c68c81ab1a2b228d912084b524
Ryan Johnson <rjohnson@mozilla.com>
2021-02-25 16:01:50 -0800

    support home and search pages in S3 (#2922)
    * support home and search pages in S3
    
    * feedbacked
    
    * ensure redirect includes query string
    
    * more redirect cases and tests
    
    * feedback and fixes
    
    * Update deployer/aws-lambda/content-origin-request/index.js
    
    Co-authored-by: Peter Bengtsson <peterbe@mozilla.com>
    
    Co-authored-by: Peter Bengtsson <peterbe@mozilla.com>

--- deployer/aws-lambda/content-origin-request/index.js
+++ deployer/aws-lambda/content-origin-request/index.js
@@ -6,6 +6,30 @@ const {
   encodePath,
   slugToFolder,
 } = require("@yari-internal/slug-utils");
+const { VALID_LOCALES } = require("@yari-internal/constants");
+
+const THIRTY_DAYS = 3600 * 24 * 30;
+const NEEDS_LOCALE = /^\/(?:docs|search|settings|signin|signup)(?:$|\/)/;
+// Note that the keys of "VALID_LOCALES" are lowercase locales.
+const LOCALE_URI_WITHOUT_TRAILING_SLASH = new Set(
+  [...VALID_LOCALES.keys()].map((locale) => `/${locale}`)
+);
+const LOCALE_URI_WITH_TRAILING_SLASH = new Set(
+  [...VALID_LOCALES.keys()].map((locale) => `/${locale}/`)
+);
+// TODO: The code that uses LEGACY_URI_NEEDING_TRAILING_SLASH should be
+//       temporary. For example, when we have moved to the Yari-built
+//       account settings page, we should add fundamental redirects
+//       for "/{locale}/account/?" and "/account/?" that redirect to
+//       "/{locale}/settings" and "/settings" respectively. The other
+//       cases can be either redirected or deleted eventually as well.
+//       The goal is to eventually remove the code that uses
+//       LEGACY_URI_NEEDING_TRAILING_SLASH.
+const LEGACY_URI_NEEDING_TRAILING_SLASH = new RegExp(
+  `^(?:${[...LOCALE_URI_WITHOUT_TRAILING_SLASH].join(
+    "|"
+  )})?/(?:account|contribute|maintenance-mode|payments)/?$`
+);
 
 const CONTENT_DEVELOPMENT_DOMAIN = ".content.dev.mdn.mozit.cloud";
 
@@ -50,22 +74,28 @@ exports.handler = async (event) => {
    * Modify the request before it's passed to the S3 origin.
    */
   const request = event.Records[0].cf.request;
+  const requestURILowerCase = request.uri.toLowerCase();
   const host = request.headers.host[0].value.toLowerCase();
+  const qs = request.querystring ? `?${request.querystring}` : "";
 
   const { url, status } = resolveFundamental(request.uri);
   if (url) {
+    // TODO: Do we want to add the query string to the redirect?
+    //       If we decide we do, then we probably need to change
+    //       the caching policy on the "*/docs/* behavior" to
+    //       cache based on the query strings as well.
     return redirect(url, {
       status,
-      cacheControlSeconds: 3600 * 24 * 30,
+      cacheControlSeconds: THIRTY_DAYS,
     });
   }
 
-  // Starting with /docs/ or empty path (/) should redirect to a locale.
-  // Also trim a trailing slash to avoid a double redirect.
+  // Do we need to insert the locale? If we do, trim a trailing slash
+  // to avoid a double redirect, except when requesting the home page.
   if (
-    request.uri.startsWith("/docs/") ||
+    request.uri === "" ||
     request.uri === "/" ||
-    request.uri === ""
+    NEEDS_LOCALE.test(requestURILowerCase)
   ) {
     const path = request.uri.endsWith("/")
       ? request.uri.slice(0, -1)
@@ -73,20 +103,59 @@ exports.handler = async (event) => {
     const locale = getLocale(request);
     // The only time we actually want a trailing slash is when the URL is just
     // the locale. E.g. `/en-US/` (not `/en-US`)
-    return redirect(`/${locale}${path || "/"}`);
+    return redirect(`/${locale}${path || "/"}` + qs);
   }
 
-  // A document URL with a trailing slash should redirect
-  // to the same URL without the trailing slash.
+  // At this point, the URI is guaranteed to start with a forward slash.
+  const uriParts = request.uri.split("/");
+  const uriFirstPart = uriParts[1];
+  const uriFirstPartLC = uriFirstPart.toLowerCase();
+
+  // Do we need to redirect to the properly-cased locale? We also ensure
+  // here that requests for the home page have a trailing slash, while
+  // all others do not.
   if (
-    request.uri.endsWith("/") &&
-    request.uri.toLowerCase().includes("/docs/")
+    VALID_LOCALES.has(uriFirstPartLC) &&
+    uriFirstPart !== VALID_LOCALES.get(uriFirstPartLC)
   ) {
-    return redirect(request.uri.slice(0, -1), {
+    // Assemble the rest of the path without a trailing slash.
+    const extra = uriParts.slice(2).filter(Boolean).join("/");
+    return redirect(`/${VALID_LOCALES.get(uriFirstPartLC)}/${extra}${qs}`);
+  }
+
+  // Handle cases related to the presence or absence of a trailing-slash.
+  if (LOCALE_URI_WITHOUT_TRAILING_SLASH.has(requestURILowerCase)) {
+    // Home page requests are the special case on MDN. They should
+    // always have a trailing slash. So a home page URL without a
+    // trailing slash should redirect to the same URL with a
+    // trailing slash. When the redirected home-page request is
+    // processed by this Lambda function, note that we'll remove
+    // the trailing slash before the request reaches S3 (see below).
+    return redirect(request.uri + "/" + qs, {
       status: 301,
-      cacheControlSeconds: 3600 * 24 * 30,
+      cacheControlSeconds: THIRTY_DAYS,
+    });
+  } else if (LOCALE_URI_WITH_TRAILING_SLASH.has(requestURILowerCase)) {
+    // We've received a proper request for a locale's home page (i.e.,
+    // it has a traling slash), but since that request will be served
+    // from S3, we need to strip the trailing slash before it reaches
+    // S3. This is required because we store the home pages in S3 as
+    // their path name itself, for example "en-us" for the English home
+    // page, not "en-us/index.html", which is what S3 would look for if
+    // we left the trailing slash.
+    request.uri = request.uri.slice(0, -1);
+  } else if (
+    request.uri.endsWith("/") &&
+    !LEGACY_URI_NEEDING_TRAILING_SLASH.test(requestURILowerCase)
+  ) {
+    // All other requests with a trailing slash should redirect to the
+    // same URL without the trailing slash.
+    return redirect(request.uri.slice(0, -1) + qs, {
+      status: 301,
+      cacheControlSeconds: THIRTY_DAYS,
     });
   }
+
   // This condition exists to accommodate AWS origin-groups, which
   // include two origins, the primary and the secondary, where the
   // secondary origin is only attempted if the primary fails. Since
