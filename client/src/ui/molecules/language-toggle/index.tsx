a2c96cdee2870c971a7e093a7431608b98beb85e
Schalk Neethling <sneethling@mozilla.com>
2021-03-03 17:09:53 +0200

    fix: redesign of language selector (#2839)
    * fix: redesign of language selector
    
    Implement redesign of language selector and related interactions.
    This also includes a change to how the underlying grid is implemented.
    
    Part of #1486
    
    * fix headless test
    
    * change let to const
    
    * changes based on review feedback
    
    * additional changes based on review
    
    * add key the li and fix grid row inconsistency
    
    * remove negative tabindex
    
    * update to latest implementation
    
    * fix failing headless test
    
    * fix failing headless test
    
    * add missing / at end of test URL
    
    * fix overall layout for desktop
    
    * updates to overall layout of top of document pages
    
    * update grid for standard aka non-document pages
    
    * correctly align lang toggle on mobile
    
    * handle all 5 possible states of breadcrumb-locale container
    
    * instrument view in English and change language links
    
    * also show language menu on non English pages
    
    * send GA event when changing language via language manu
    
    * changes based on review feedback
    
    * feedback on language selector
    
    * fix failing test
    
    * fix failing test
    
    * remove react import
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>

---
+++ client/src/ui/molecules/language-toggle/index.tsx
@@ -0,0 +1,74 @@
+import { Link } from "react-router-dom";
+
+import { Translation } from "../../../document/types";
+import { useGA } from "../../../ga-context";
+
+import "./index.scss";
+
+export function LanguageToggle({
+  locale,
+  translations,
+}: {
+  locale: string;
+  translations: Translation[];
+}) {
+  const ga = useGA();
+
+  function getEnglishLink() {
+    for (const translation of translations) {
+      if (translation.locale.toLowerCase() === "en-us") {
+        return (
+          <Link
+            to={translation.url}
+            className="view-in-english"
+            onClick={() => {
+              ga("send", {
+                hitType: "event",
+                eventCategory: "Language",
+                eventAction: "Switch to English",
+                eventLabel: `${window.location.pathname} to ${translation.url}`,
+              });
+            }}
+          >
+            <span className="show-desktop">View in</span> English
+          </Link>
+        );
+      }
+    }
+    return null;
+  }
+
+  return (
+    <ul
+      className={
+        locale.toLowerCase() === "en-us"
+          ? "language-toggle single-option"
+          : "language-toggle"
+      }
+    >
+      <li>
+        <a
+          href="#select-language"
+          className="language-icon"
+          onClick={(event) => {
+            const destination = document.querySelector("#select-language");
+            if (destination) {
+              destination.scrollIntoView({ behavior: "smooth" });
+              event.preventDefault();
+            }
+
+            ga("send", {
+              hitType: "event",
+              eventCategory: "Language",
+              eventAction: "Anchor to language choice",
+              eventLabel: window.location.pathname,
+            });
+          }}
+        >
+          <span className="show-desktop">Change language</span>
+        </a>
+      </li>
+      {locale.toLowerCase() !== "en-us" && <li>{getEnglishLink()}</li>}
+    </ul>
+  );
+}
