d3f9149dc30480d1bce6f3cbe16f95df058a3e54
Peter Bengtsson <mail@peterbe.com>
2021-03-08 07:16:02 -0500

    markdown with GFM

--- content/markdown-converter.js
+++ content/markdown-converter.js
@@ -1,5 +1,6 @@
 const unified = require("unified");
 const parse = require("remark-parse");
+const gfm = require("remark-gfm");
 const stringify = require("rehype-stringify");
 const remark2rehype = require("remark-rehype");
 const raw = require("rehype-raw");
@@ -13,6 +14,7 @@ function markdownToHTML(md) {
     .use(parse)
     .use(remark2rehype, { allowDangerousHtml: true })
     .use(raw)
+    .use(gfm)
     .use(stringify)
     .processSync(md)
     .toString();
