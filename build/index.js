169c9ccd2bdd220811d0af345d76fb9f0513156b
Peter Bengtsson <mail@peterbe.com>
2021-03-09 17:14:21 -0500

    debugging be gone

--- build/index.js
+++ build/index.js
@@ -358,7 +358,6 @@ async function buildDocument(document, documentOptions = {}) {
 
   // Check and scrutinize any local image references
   const fileAttachments = checkImageReferences(doc, $, options, document);
-  console.log("fileAttachments:", fileAttachments);
 
   // Check the img tags for possible flaws and possible build-time rewrites
   checkImageWidths(doc, $, options, document);
