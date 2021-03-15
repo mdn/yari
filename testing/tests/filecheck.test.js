918ea87296cf66b037de57b4ee256a17a79989ef
Peter Bengtsson <mail@peterbe.com>
2021-03-15 19:10:19 -0400

    Check of unsafe HTML in SVG files (#3250)
    * Check of unsafe HTML in SVG files
    
    Fixes #3249
    
    * mention in index.html file
    
    * feedbacked

---
+++ testing/tests/filecheck.test.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const { checkFile } = require("../../filecheck/checker");
+
+const SAMPLES_DIRECTORY = path.join(__dirname, "samplefiles");
+
+describe("checking files", () => {
+  it("should spot SVGs with scripts inside them", async () => {
+    const filePath = path.join(SAMPLES_DIRECTORY, "script.svg");
+    // Sanity check the test itself
+    console.assert(fs.existsSync(filePath), `${filePath} does not exist`);
+    await expect(checkFile(filePath)).rejects.toThrow(
+      "contains a <script> tag"
+    );
+  });
+  it("should spot SVGs with onLoad inside an element", async () => {
+    const filePath = path.join(SAMPLES_DIRECTORY, "onhandler.svg");
+    // Sanity check the test itself
+    console.assert(fs.existsSync(filePath), `${filePath} does not exist`);
+    await expect(checkFile(filePath)).rejects.toThrow(
+      "<path> contains an unsafe attribute: 'onload'"
+    );
+  });
+});
