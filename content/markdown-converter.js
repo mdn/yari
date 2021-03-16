b75eaed224bfdcada1f25ae29206a6dfb79f4af6
Peter Bengtsson <mail@peterbe.com>
2021-03-16 16:05:53 -0400

    fix eslint

--- content/markdown-converter.js
+++ content/markdown-converter.js
@@ -3,7 +3,8 @@ const parse = require("remark-parse");
 const stringify = require("rehype-stringify");
 const remark2rehype = require("remark-rehype");
 const raw = require("rehype-raw");
-const visit = require("unist-util-visit");
+
+const remarkCodeBlocks = require("./rehype-codeblocks");
 
 /**
  * Converts Markdown -> HTML using unified.
@@ -18,47 +19,6 @@ function markdownToHTML(md) {
     .use(stringify)
     .processSync(md)
     .toString();
-}
-
-function remarkCodeBlocks(options) {
-  options = options || {};
-
-  return (tree) => {
-    visit(tree, "element", visitor);
-  };
-
-  function visitor(node, index, parent) {
-    if (!parent || parent.tagName !== "pre" || node.tagName !== "code") {
-      return;
-    }
-    // When you use the triple-backtick and a language, like...
-    //
-    //    ```css
-    //    ...
-    //
-    // What you get is:
-    //
-    //    <pre><code class="language-css">...
-    //
-    // Let's now convert that to:
-    //
-    //    <pre class="brush: css">
-    //
-    // The reason for doing this is entirely to pretend that nothing has changed.
-    // This way, the Markdown gets converted to HTML in the way Yari can process
-    // all other existing HTML.
-
-    const classNames = node.properties.className || [];
-    for (const className of classNames) {
-      if (className.startsWith("language-")) {
-        const classNamesBefore = parent.properties.className || [];
-        parent.properties.className = [
-          `brush: ${className.replace("language-", "").toLowerCase()}`,
-          ...classNamesBefore,
-        ];
-      }
-    }
-  }
 }
 
 module.exports = {
