4db07d384d5859a8c2cab46751405bd522d59585
Peter Bengtsson <mail@peterbe.com>
2021-03-19 17:06:12 -0400

    /pagenotfound is rendering the homepage route
    Fixes #3224
    Fixes #3287

--- client/src/hooks.ts
+++ client/src/hooks.ts
@@ -1,5 +1,54 @@
 import { useParams } from "react-router-dom";
 
+// This is a bit of a necessary hack!
+// The only reason this list is needed is because of the PageNotFound rendering.
+// If someone requests `https://domain/some-random-word` what will happen is that
+// Lambda@Edge will send the `build/en-us/_spas/404.html` rendered page. That
+// rendered page has all the React stuff like routing.
+// The <App/> component can know what it needs to render but what's problematic
+// is that all and any other app will think that the locale is `some-random-word`
+// just because it's the first portion of the URL.
+// So, for example, the top navbar will think it can use the `useLocale()` hook
+// and get the current locale from the react-router context. Now the navbar menu
+// items, for example, will think the locale is `some-random-word` and make links
+// like `/some-random-word/docs/Web`.
+const VALID_LOCALES = new Set([
+  "ar",
+  "bg",
+  "bn",
+  "ca",
+  "de",
+  "el",
+  "en-US",
+  "es",
+  "fa",
+  "fi",
+  "fr",
+  "he",
+  "hi-IN",
+  "hu",
+  "id",
+  "it",
+  "ja",
+  "kab",
+  "ko",
+  "ms",
+  "my",
+  "nl",
+  "pl",
+  "pt-BR",
+  "pt-PT",
+  "ru",
+  "sv-SE",
+  "th",
+  "tr",
+  "uk",
+  "vi",
+  "zh-CN",
+  "zh-TW",
+]);
+
 export function useLocale() {
-  return useParams().locale || "en-US";
+  const { locale } = useParams();
+  return locale && VALID_LOCALES.has(locale) ? locale : "en-US";
 }
