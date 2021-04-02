62fc467c406a94bdb0b4c6959c5d7f2b6c733ce6
Peter Bengtsson <mail@peterbe.com>
2021-04-02 13:20:39 -0400

    remove 'MozMenu' event collection
    Fixes #3300

--- client/src/document/organisms/toc/index.tsx
+++ client/src/document/organisms/toc/index.tsx
@@ -1,35 +1,15 @@
-import React, { useState } from "react";
-import { useGA } from "../../../ga-context";
+import React from "react";
 
 import { Toc } from "../../types";
 
 import "./index.scss";
 
 export function TOC({ toc }: { toc: Toc[] }) {
-  const ga = useGA();
-  const [showTOC, setShowTOC] = useState(false);
-
-  /**
-   * Send a signal to GA when the user clicks on one of links
-   * in the table of contents
-   */
-  function sendTOCClicks(event: React.MouseEvent) {
-    if (event.target instanceof HTMLAnchorElement) {
-      const action = event.target.textContent;
-      const label = new URL(event.target.href).hash;
-      ga("send", {
-        hitType: "event",
-        eventCategory: "MozMenu",
-        eventAction: action,
-        eventLabel: label,
-      });
-    }
-  }
+  const [showTOC, setShowTOC] = React.useState(false);
 
   return (
     <aside className="document-toc-container">
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
-      <section className="document-toc" onClick={sendTOCClicks}>
+      <section className="document-toc">
         <header>
           <h2>Table of contents</h2>
           <button
