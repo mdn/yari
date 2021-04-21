6eba53d051bc1feff5853b358c7d90344f024518
Peter Bengtsson <mail@peterbe.com>
2021-04-21 17:29:20 -0400

    feedback on pr 3518

--- client/src/document/spec-table.tsx
+++ client/src/document/spec-table.tsx
@@ -17,45 +17,46 @@ export function SpecificationTable({
   }>;
   query: string;
 }) {
-  if (specifications) {
-    const rows = specifications.map((spec) => (
-      <tr key={spec.bcdSpecificationURL}>
-        <td>
-          <a href={spec.bcdSpecificationURL}>{spec.title}</a>
-        </td>
-      </tr>
-    ));
+  return (
+    <>
+      {title && !isH3 && <DisplayH2 id={id} title={title} />}
+      {title && isH3 && <DisplayH3 id={id} title={title} />}
 
-    return (
-      <>
-        {title && !isH3 && <DisplayH2 id={id} title={title} />}
-        {title && isH3 && <DisplayH3 id={id} title={title} />}
+      {/* XXX We could have a third condition; the specURL worked but yielded
+      exactly 0 specifications. If that's the case, perhaps the messaging
+      should be different. */}
+      {specifications.length > 0 ? (
         <table className="standard-table">
           <thead>
             <tr>
               <th scope="col">Specification</th>
             </tr>
           </thead>
-          {rows}
+          <tbody>
+            {specifications.map((spec) => (
+              <tr key={spec.bcdSpecificationURL}>
+                <td>
+                  <a href={spec.bcdSpecificationURL}>{spec.title}</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-      </>
-    );
-  } else {
-    return (
-      <>
-        {title && !isH3 && <DisplayH2 id={id} title={title} />}
-        {title && isH3 && <DisplayH3 id={id} title={title} />}
-        <p>
-          No specification data found for <code>{query}</code>.<br />
-          <a href="#on-github">Check for problems with this page</a> or
-          contribute a missing <code>spec_url</code> to{" "}
-          <a href="https://github.com/mdn/browser-compat-data">
-            mdn/browser-compat-data
-          </a>
-          . Also make sure the specification is included in{" "}
-          <a href="https://github.com/w3c/browser-specs">w3c/browser-specs</a>.
-        </p>
-      </>
-    );
-  }
+      ) : (
+        <div className="notecard warning">
+          <p>
+            No specification data found for <code>{query}</code>.<br />
+            <a href="#on-github">Check for problems with this page</a> or
+            contribute a missing <code>spec_url</code> to{" "}
+            <a href="https://github.com/mdn/browser-compat-data">
+              mdn/browser-compat-data
+            </a>
+            . Also make sure the specification is included in{" "}
+            <a href="https://github.com/w3c/browser-specs">w3c/browser-specs</a>
+            .
+          </p>
+        </div>
+      )}
+    </>
+  );
 }
