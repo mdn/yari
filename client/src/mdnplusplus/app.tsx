e1bf3b22effb78dc156ac9563db0cd440fd7a80b
Peter Bengtsson <mail@peterbe.com>
2021-04-25 06:41:07 -0400

    mdn++ landing page (#3608)

---
+++ client/src/mdnplusplus/app.tsx
@@ -0,0 +1,17 @@
+import "./index.scss";
+
+export default function App({ ...appProps }) {
+  return (
+    <div className="mdnplusplus">
+      {/* From https://app.alchemer.com/distribute/share/id/6295937 */}
+      <iframe
+        src="http://survey.alchemer.com/s3/6295937/MDN-Fake-Door-Survey"
+        title="MDN++ survey"
+        frameBorder="0"
+        width="700"
+        height="500"
+        style={{ overflow: "hidden" }}
+      ></iframe>
+    </div>
+  );
+}
