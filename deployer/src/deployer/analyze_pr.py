d185496cf2d783fb902df70b58e0925a081679ed
Peter Bengtsson <mail@peterbe.com>
2021-05-05 13:04:05 -0400

    only mention *new* external URLs if build/DIFF exists (#3709)
    * only mention *new* external URLs if build/DIFF exists
    
    Part of #3499
    
    * with test
    
    * make it stand out a bit more

--- deployer/src/deployer/analyze_pr.py
+++ deployer/src/deployer/analyze_pr.py
@@ -4,9 +4,11 @@ import json
 import re
 from collections import defaultdict
 from pathlib import Path
+from typing import Optional
 
 from github import Github
 from selectolax.parser import HTMLParser
+from unidiff import PatchSet
 
 from .utils import log
 
@@ -28,8 +30,13 @@ def analyze_pr(build_directory: Path, config):
         combined_comments.append(post_about_flaws(build_directory, **config))
 
     if config["analyze_dangerous_content"]:
+        diff_file = config["diff_file"]
+        patch = None
+        if diff_file:
+            with open(diff_file) as f:
+                patch = PatchSet(f.read())
         combined_comments.append(
-            post_about_dangerous_content(build_directory, **config)
+            post_about_dangerous_content(build_directory, patch, **config)
         )
 
     combined_comment = "\n\n".join(x for x in combined_comments if x)
@@ -98,7 +105,9 @@ def mdn_url_to_dev_url(prefix, mdn_url):
     return template.format(prefix=prefix, mdn_url=mdn_url)
 
 
-def post_about_dangerous_content(build_directory: Path, **config):
+def post_about_dangerous_content(
+    build_directory: Path, patch: Optional[PatchSet], **config
+):
 
     OK_URL_PREFIXES = [
         "https://github.com/mdn/",
@@ -106,34 +115,67 @@ def post_about_dangerous_content(build_directory: Path, **config):
 
     comments = []
 
+    patch_lines = {}
+    if patch:
+        for patched_file in patch:
+            # This value is the file path as it would appear in `git diff`
+            # so for example: `files/en-us/mdn/kitchensink/index.html`
+            file_path = patched_file.path
+            new_lines = []
+            for hunk in patched_file:
+                for line in hunk:
+                    if line.line_type == "+":
+                        new_lines.append(line.value)
+
+            patch_lines[file_path] = "".join(new_lines)
+
     for doc in get_built_docs(build_directory):
         rendered_html = "\n".join(
             x["value"]["content"]
             for x in doc["body"]
             if x["type"] == "prose" and x["value"]["content"]
         )
+
+        diff_lines = None
+        for file_path in patch_lines:
+            if file_path.endswith(
+                "/".join([doc["source"]["folder"], doc["source"]["filename"]])
+            ):
+                diff_lines = patch_lines[file_path]
+                break
+
         tree = HTMLParser(rendered_html)
         external_urls = defaultdict(int)
         for node in tree.css("a[href]"):
             href = node.attributes.get("href")
             href = href.split("#")[0]
+
             # We're only interested in external URLs at the moment
             if href.startswith("//") or "://" in href:
                 if any(href.lower().startswith(x.lower()) for x in OK_URL_PREFIXES):
                     # exceptions are skipped
                     continue
+                if diff_lines:
+                    if href not in diff_lines:
+                        continue
                 external_urls[href] += 1
 
         if external_urls:
             external_urls_list = []
             for url in sorted(external_urls):
                 count = external_urls[url]
-
-                external_urls_list.append(
+                line = (
                     f"  - {'🚨 ' if url.startswith('http://') else ''}"
                     f"<{url}> ({count} time{'' if count==1 else 's'})"
                 )
+                if diff_lines:
+                    # If this was available and it _did_ fine a URL, then
+                    # really make sure it's noticed.
+                    line += " (Note! This is a new URL 👀)"
+                external_urls_list.append(line)
             comments.append((doc, "\n".join(external_urls_list)))
+        elif diff_lines:
+            comments.append((doc, "No *new* external URLs"))
         else:
             comments.append((doc, "No external URLs"))
 
@@ -151,6 +193,7 @@ def post_about_dangerous_content(build_directory: Path, **config):
             lines.append(f"[on GitHub]({doc['source']['github_url']})")
             lines.append("")
             lines.append(comment)
+            lines.append("")
 
             per_doc_comments.append("\n".join(lines))
         return heading + "\n---\n".join(per_doc_comments)
