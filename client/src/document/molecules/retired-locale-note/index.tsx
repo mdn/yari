554ca85bf3fbbaac20dd70892847a5786490dd84
Schalk Neethling <sneethling@mozilla.com>
2021-05-12 15:32:15 +0200

    prettier

--- client/src/document/molecules/retired-locale-note/index.tsx
+++ client/src/document/molecules/retired-locale-note/index.tsx
@@ -4,8 +4,7 @@ export function RetiredLocaleNote() {
   const noteContent: NoteContent = {
     linkText:
       "The page you requested has been retired, so we've sent you to the English equivalent.",
-    url:
-      "https://hacks.mozilla.org/2021/03/mdn-localization-in-march-tier-1-locales-unfrozen-and-future-plans/",
+    url: "https://hacks.mozilla.org/2021/03/mdn-localization-in-march-tier-1-locales-unfrozen-and-future-plans/",
   };
   return NoteBanner(noteContent, NOTE_TYPES.general);
 }
