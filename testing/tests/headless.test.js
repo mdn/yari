558275e17bd2bb03f7f645bf632d7f7ab128ab1e
Peter Bengtsson <mail@peterbe.com>
2021-06-07 10:25:07 -0400

    fix tests

--- testing/tests/headless.test.js
+++ testing/tests/headless.test.js
@@ -10,10 +10,10 @@ function testURL(pathname = "/") {
 }
 
 describe("Basic viewing of functional pages", () => {
-  it("open the temporary home page", async () => {
+  it("open the home page", async () => {
     await page.goto(testURL("/"));
-    await expect(page).toMatch("Writer's home page");
-    await expect(page).toMatchElement("title", { text: /Writer's home page/ });
+    await expect(page).toMatch("MDN Web Docs");
+    await expect(page).toMatchElement("title", { text: /MDN Web Docs/ });
   });
 
   it("open the /en-US/docs/Web/Foo page", async () => {
