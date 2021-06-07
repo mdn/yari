6aa16773fed2a5a12b6591fcfbaf2d53ac75e327
Tanner Dolby <tannercdolby@gmail.com>
2021-06-07 04:31:30 -0700

    Add copy-to-clipboard feature to code examples (#3930)
    * Add clipboard to assets directory
    
    * Add styles needed for clipboard copy in code examples
    
    * Add useEffect for copy-to-clipboard functionality
    
    * Wrap <pre class='brush'> tags in a div to better absolutely position copy-to-clipboard btn
    
    * Fix absolute positioning for clipboard icon relative to parent .code-example div, cleanup
    
    * Append clipboard button and message to parent div .code-example
    
    * Add clipboard copy support for Internet Explorer
    
    * ideas on how to refactor
    
    * Move .wrapAll() so each <pre> tag is wrapped at initialization
    
    * Remove main DOM ID from nodes to be selected
    
    * Attach mouseover/out listeners to div.code-example, cleanup comments
    
    * Add short circuit OR to use '' if element is null
    
    * Keep clippy button visible with 0.4 opacity, transition to 1 for hover/focus states
    
    * Remove mouseover/mouseout handlers for changing aria-hidden value of clippy button
    
    * make the tests pass
    
    * Adds isClipboardSupported function and update logic
    
    * Refactor copyToClipboard and make return value a boolean, update onclick logic
    
    * Make generic class names namespaces
    
    * Update client/src/document/index.scss
    
    Co-authored-by: Schalk Neethling <volume4.schalk+github@gmail.com>
    
    * Refactor copyToClipboard function, add try/catch logic to click event
    
    * Run Prettier to fix code style issue and make tests pass
    
    * feedback on copy-to-clipboard support
    
    * type infereed
    
    * make it async
    
    * Add error message tooltip for when copy utility fails
    
    * Adds ternary to allow longer timeout for error tooltip
    
    Co-authored-by: Peter Bengtsson <mail@peterbe.com>
    Co-authored-by: Schalk Neethling <volume4.schalk+github@gmail.com>

--- client/src/document/hooks.ts
+++ client/src/document/hooks.ts
@@ -1,4 +1,7 @@
+import React from "react";
 import { useParams } from "react-router-dom";
+
+import { Doc } from "./types";
 
 export function useDocumentURL() {
   const { "*": slug, locale } = useParams();
@@ -7,4 +10,76 @@ export function useDocumentURL() {
   // on any URL. We can't keep that if we're going to compare the current
   // pathname with the document's mdn_url.
   return url.endsWith("/") ? url.substring(0, url.length - 1) : url;
+}
+
+export function useCopyExamplesToClipboard(doc: Doc | undefined) {
+  React.useEffect(() => {
+    if (!doc) {
+      return;
+    }
+    if (!navigator.clipboard) {
+      console.log(
+        "Copy-to-clipboard disabled because your browser does not appear to support it."
+      );
+      return;
+    }
+
+    [...document.querySelectorAll("div.code-example pre")].forEach(
+      (element) => {
+        const userMessage = document.createElement("span");
+        const button = document.createElement("button");
+        const span = document.createElement("span");
+        userMessage.textContent = "Copied!";
+        span.textContent = "Copy to Clipboard";
+
+        const wrapper = element.parentElement;
+
+        userMessage.setAttribute("class", "user-message");
+        userMessage.setAttribute("aria-hidden", "true");
+        button.setAttribute("aria-hidden", "false");
+        button.setAttribute("type", "button");
+        button.setAttribute("class", "copy-icon");
+        span.setAttribute("class", "visually-hidden");
+
+        button.appendChild(span);
+        wrapper?.appendChild(button);
+        wrapper?.appendChild(userMessage);
+
+        button.onclick = async () => {
+          let copiedSuccessfully = true;
+          try {
+            const text = element.textContent || "";
+            await navigator.clipboard.writeText(text);
+          } catch (err) {
+            console.error(
+              "Error when trying to use navigator.clipboard.writeText()",
+              err
+            );
+            copiedSuccessfully = false;
+          }
+
+          if (copiedSuccessfully) {
+            userMessage.classList.add("show");
+            userMessage.setAttribute("aria-hidden", "false");
+            button.classList.add("copied");
+            userMessage.style.top = "52px";
+          } else {
+            button.classList.add("failed");
+            userMessage.classList.add("show");
+            userMessage.setAttribute("aria-hidden", "false");
+            userMessage.style.top = "52px";
+            userMessage.textContent = "Error trying to copy to clipboard!";
+          }
+
+          setTimeout(
+            () => {
+              userMessage.classList.remove("show");
+              userMessage.setAttribute("aria-hidden", "true");
+            },
+            copiedSuccessfully ? 1000 : 3000
+          );
+        };
+      }
+    );
+  }, [doc]);
 }
