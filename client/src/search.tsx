864cb549243b2c73fac82b14122fb9e9380e8c6e
Gregor Weber <gregorwbr@gmail.com>
2021-06-22 12:51:56 +0200

    keep navigate() and reset input instead

--- client/src/search.tsx
+++ client/src/search.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useCombobox } from "downshift";
 import FlexSearch from "flexsearch";
 import useSWR from "swr";
@@ -166,6 +166,7 @@ function InnerSearchNavigateWidget(props: InnerSearchNavigateWidgetProps) {
     defaultSelection,
   } = props;
 
+  const navigate = useNavigate();
   const locale = useLocale();
 
   const [searchIndex, searchIndexError, initializeSearchIndex] =
@@ -251,9 +252,11 @@ function InnerSearchNavigateWidget(props: InnerSearchNavigateWidgetProps) {
     defaultIsOpen: isFocused,
     onSelectedItemChange: ({ selectedItem }) => {
       if (selectedItem) {
-        window.location.href = selectedItem.url;
+        navigate(selectedItem.url);
+        onChangeInputValue("");
         reset();
         toggleMenu();
+        inputRef.current?.blur();
         if (onResultPicked) {
           onResultPicked();
         }
