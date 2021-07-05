0f5520543239effd5f72ec9b63d29bed8fa96999
Gregor Weber <gregorwbr@gmail.com>
2021-07-05 13:34:48 +0200

    waitUntil page nav

--- testing/tests/headless.test.js
+++ testing/tests/headless.test.js
@@ -89,7 +89,8 @@ describe("Basic viewing of functional pages", () => {
       gridSample1Uri,
       gridSample2Uri,
     ]) {
-      await page.goto(testURL(sampleUri), { waitUntil: "networkidle1" });
+      await page.goto(testURL(sampleUri));
+      await page.waitForNavigation({ waitUntil: "networkidle1" });
       await expect(page).toMatchElement("body > div.wrapper > div.box1", {
         text: "One",
       });
