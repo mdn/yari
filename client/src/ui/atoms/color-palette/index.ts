c75d16cd789537148a553a471d6833b0704bdc0b
Schalk Neethling <sneethling@mozilla.com>
2021-07-17 23:56:37 +0200

    add v3 color palette

---
+++ client/src/ui/atoms/color-palette/index.ts
@@ -0,0 +1,16 @@
+export const colorPalette = {
+  black: "#000",
+  white: "#fff",
+  light: {
+    neutral050: "#f9f9fa",
+    neutral100: "#ededf0",
+    neutral200: "#cdcdd4",
+    neutral300: "#b1b1bc",
+    neutral400: "#9595a2",
+    neutral500: "#7a7a8b",
+    neutral600: "#5e5e72",
+    neutral700: "#42435a",
+    neutral800: "#1f2033",
+    neutral900: "#141422",
+  },
+};
