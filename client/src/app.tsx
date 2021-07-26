9a3034427db8201c7decbff7cea23bb160050be5
Schalk Neethling <sneethling@mozilla.com>
2021-07-26 20:18:27 +0200

    fix: switch to v1 version of plus page (#4317)
    This reinstates the previous plus page for the duration of the new market research phase.
    Only changes relate to the graphic next to "Make MDN your own", updates to the premium feature projects, and the addition of a costing section at the end.
    
    fix #4309

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -21,7 +21,7 @@ import { Settings } from "./settings";
 
 import { DeepDives } from "./plus/deep-dives";
 // import { DeepDivesHomepage } from "./plus/deep-dives/homepage";
-import { Plus } from "./plus";
+import { PlusV1 } from "./plus-v1";
 
 const AllFlaws = React.lazy(() => import("./flaws"));
 const AllTranslations = React.lazy(() => import("./translations"));
@@ -260,7 +260,7 @@ export function App(appProps) {
                 path="/plus"
                 element={
                   <StandardLayout>
-                    <Plus />
+                    <PlusV1 />
                   </StandardLayout>
                 }
               />
@@ -270,7 +270,7 @@ export function App(appProps) {
                 path="/plus/deep-dives"
                 element={
                   <StandardLayout>
-                    <Plus />
+                    <PlusV1 />
                   </StandardLayout>
                 }
               />
