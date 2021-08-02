6dbbb3d375930e0dd6f40d0f979b705f14b0f527
Tanner <tannercdolby@gmail.com>
2021-08-02 12:32:29 -0700

    Add logic to conditionally render DisplayH4 component

--- client/src/document/ingredients/prose.tsx
+++ client/src/document/ingredients/prose.tsx
@@ -1,4 +1,4 @@
-import { DisplayH2, DisplayH3 } from "./utils";
+import { DisplayH2, DisplayH3, DisplayH4 } from "./utils";
 
 export function Prose({ section }) {
   return <div dangerouslySetInnerHTML={{ __html: section.content }} />;
@@ -7,13 +7,21 @@ export function Prose({ section }) {
 export function ProseWithHeading({ id, section }) {
   return (
     <>
-      {section.isH3 ? (
+      {section.isH4 && (
+        <DisplayH4
+          id={id}
+          title={section.title}
+          titleAsText={section.titleAsText}
+        />
+      )}
+      {section.isH3 && !section.isH4 && (
         <DisplayH3
           id={id}
           title={section.title}
           titleAsText={section.titleAsText}
         />
-      ) : (
+      )}
+      {!section.isH4 && !section.isH3 && (
         <DisplayH2
           id={id}
           title={section.title}
