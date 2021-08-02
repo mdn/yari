aa416bbf03cf651c22c792fe06cab0b0d8d572b4
Peter Bengtsson <mail@peterbe.com>
2021-08-02 17:17:29 -0400

    Bookmarking (#4021)
    * notes exploration
    
    * ability to edit and delete notes
    
    * bookmarking
    
    * drop scss
    
    * wip
    
    * remove logging
    
    * local state for optimization
    
    * doomed bookmarks
    
    * new minimalist colors
    
    * new api URL endpoints

--- client/src/plus-v1/index.tsx
+++ client/src/plus-v1/index.tsx
@@ -1,28 +1,62 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import { Loading } from "../ui/atoms/loading";
 import { PageContentContainer } from "../ui/atoms/page-content";
-
+import { PageNotFound } from "../page-not-found";
 const App = React.lazy(() => import("./app"));
+// XXX temporary hack while we still have this plus-v1 dupe.
+const Bookmarks = React.lazy(() => import("../plus/bookmarks"));
 
-export function PlusV1() {
-  const pageTitle = "MDN Plus";
+export function PlusV1({ pageTitle }: { pageTitle?: string }) {
+  const defaultPageTitle = "MDN Plus";
   React.useEffect(() => {
-    document.title = pageTitle;
-  }, []);
+    document.title = pageTitle || defaultPageTitle;
+  }, [pageTitle]);
+
   const isServer = typeof window === "undefined";
-  const loading = <Loading message={`Loading ${pageTitle}…`} minHeight={800} />;
+  const loading = (
+    <Loading
+      message={`Loading ${pageTitle || defaultPageTitle}…`}
+      minHeight={800}
+    />
+  );
+
+  const routes = (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          isServer ? (
+            loading
+          ) : (
+            <React.Suspense fallback={loading}>
+              <App />
+            </React.Suspense>
+          )
+        }
+      />
+      <Route
+        path="bookmarks"
+        element={
+          isServer ? (
+            loading
+          ) : (
+            <React.Suspense fallback={loading}>
+              <div className="bookmarks">
+                <Bookmarks />
+              </div>
+            </React.Suspense>
+          )
+        }
+      />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
+
   return (
-    <div className="settings">
-      <PageContentContainer extraClasses="plus">
-        {isServer ? (
-          loading
-        ) : (
-          <React.Suspense fallback={loading}>
-            <App />
-          </React.Suspense>
-        )}
-      </PageContentContainer>
-    </div>
+    <PageContentContainer extraClasses="plus">
+      {!isServer && routes}
+    </PageContentContainer>
   );
 }
