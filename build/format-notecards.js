083ab6eecf4dbd0ccacddeb862b7d19697dfb541
Tanner <tannercdolby@gmail.com>
2021-08-04 09:11:00 -0700

    Refactor the .each() logic for formatNotecards

--- build/format-notecards.js
+++ build/format-notecards.js
@@ -1,21 +1,14 @@
+// Remove h4s from any existing notecards and transform them
+// from <div class="note notecard"><h4>Note:</h4>foobar</div> to
+// <div class="note notecard"><p><strong>Note:</strong>foobar</p></div>
 function formatNotecards($) {
-  $(".note.notecard").each(() => {
-    const $h4 = $("h4");
-    const id = $h4.attr("id");
-    const $p = $("p");
-    const $replacer = $(`<strong>${$h4.text()}:</strong>`);
-
-    if ($h4.length) {
-      $h4.empty();
-      $h4.replaceWith($replacer);
-      $p.prepend($replacer);
-      // if the h4 has id="note" add it as <strong id="note">
-      if (id === "note") {
-        $replacer.attr("id", id);
-      }
-    }
-
-    return;
+  $(".note.notecard h4").each((_, element) => {
+    const h4 = $(element);
+    const text = h4.text();
+    const p = $("p:first", h4.parents("div.notecard"));
+    p.html(`<strong>${text}:</strong> ${p.html()}`);
+    h4.empty();
+    h4.remove();
   });
 }
 
