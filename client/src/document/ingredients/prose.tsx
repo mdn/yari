2135fc3f93c4603d2b4d751e5ad4aac4e5ad34c0
Tanner <tannercdolby@gmail.com>
2021-08-03 17:01:06 -0700

    Render a single DisplayHeading component based on its level

--- client/src/document/ingredients/prose.tsx
+++ client/src/document/ingredients/prose.tsx
@@ -1,4 +1,4 @@
-import { DisplayH2, DisplayH3, DisplayH4 } from "./utils";
+import { DisplayHeading } from "./utils";
 
 export function Prose({ section }) {
   return <div dangerouslySetInnerHTML={{ __html: section.content }} />;
@@ -7,27 +7,12 @@ export function Prose({ section }) {
 export function ProseWithHeading({ id, section }) {
   return (
     <>
-      {section.isH4 && (
-        <DisplayH4
-          id={id}
-          title={section.title}
-          titleAsText={section.titleAsText}
-        />
-      )}
-      {section.isH3 && !section.isH4 && (
-        <DisplayH3
-          id={id}
-          title={section.title}
-          titleAsText={section.titleAsText}
-        />
-      )}
-      {!section.isH4 && !section.isH3 && (
-        <DisplayH2
-          id={id}
-          title={section.title}
-          titleAsText={section.titleAsText}
-        />
-      )}
+      <DisplayHeading
+        level={section.isH4 ? 4 : section.isH3 ? 3 : 2}
+        id={id}
+        title={section.title}
+        titleAsText={section.titleAsText}
+      />
       <Prose section={section} />
     </>
   );
