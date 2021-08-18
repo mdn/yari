24613f7d9c0b115a22d21bab46e3773a7b149a61
Peter Bengtsson <mail@peterbe.com>
2021-08-18 14:47:20 -0400

    networkidle

--- testing/tests/headless.plus.bookmarks.spec.js
+++ testing/tests/headless.plus.bookmarks.spec.js
@@ -43,6 +43,7 @@ test.describe("Bookmarking pages", () => {
 
   test("view your listing of all bookmarks", async ({ page }) => {
     await page.goto(testURL("/en-US/plus/bookmarks"));
+    await page.waitForLoadState("networkidle");
     await page.waitForSelector("main.plus");
     await page.waitForSelector("text=You have not signed in");
     expect(await page.isVisible("text=You have not signed in")).toBeTruthy();
