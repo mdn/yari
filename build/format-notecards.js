2422b16eaebeb43b6a5c158b5897fa7d2122410a
Tanner Dolby <tannercdolby@gmail.com>
2021-08-19 13:27:04 -0700

    Replaces all pre-existing h4 tags in notecards with <strong> (#4409)
    * Handle replacing h4s with strong tag in notecards
    
    * Call the formatNotecards utility in index.js
    
    * Ensure that id='note' styles are maintained by strong tags, for the time being
    
    * Move comment to function definition file
    
    * Refactor the .each() logic for formatNotecards
    
    * Remove strong tags to mimic the <h4 id='note'> as they aren't any in content
    
    * Make selector more precise, remove useless .empty() call
    
    * Setup a template for our test fixture
    
    * Write jest tests
    
    * Cleanup jest test
    
    * Refactor formatNotecards and add logic to handle edge case
    
    * Use .toBeFalsy() instead
    
    * Change 'var' to 'const'

---
+++ build/format-notecards.js
@@ -0,0 +1,34 @@
+// Remove h4s from any existing notecards and transform them
+// from <div class="note notecard"><h4>Note:</h4>foobar</div> to
+// <div class="note notecard"><p><strong>Note:</strong>foobar</p></div>
+function formatNotecards($) {
+  $("div.notecard h4").each((_, element) => {
+    const h4 = $(element);
+    const text = h4.text();
+    const div = h4.parent("div.notecard");
+    const p = $("p:first", div);
+
+    // Return filtered collection of elements that are
+    // text nodes and remove the node while were at it,
+    // also trimming away any leading/trailing space
+    const textNodes = div
+      .contents()
+      .filter((_, element) => {
+        return element.nodeType === 3;
+      })
+      .remove()
+      .text()
+      .trim();
+
+    if (!textNodes.length) {
+      p.html(`<strong>${text}:</strong> ${p.html()}`);
+    } else {
+      div.append(`<p>${p.html()}</p>`);
+      p.html(`<strong>${text}:</strong> ${textNodes}`);
+    }
+
+    h4.remove();
+  });
+}
+
+module.exports = { formatNotecards };
