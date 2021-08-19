774e92d2aa95bf013d795e940e9c8c70ebcb0fd7
Tanner <tannercdolby@gmail.com>
2021-08-19 00:37:51 -0700

    Refactor formatNotecards and add logic to handle edge case

--- build/format-notecards.js
+++ build/format-notecards.js
@@ -5,8 +5,28 @@ function formatNotecards($) {
   $("div.notecard h4").each((_, element) => {
     const h4 = $(element);
     const text = h4.text();
-    const p = $("p:first", h4.parents("div.notecard"));
-    p.html(`<strong>${text}:</strong> ${p.html()}`);
+    const div = h4.parent("div.notecard");
+    const p = $("p:first", div);
+
+    // Return filtered collection of elements that are
+    // text nodes and remove the node while were at it,
+    // also trimming away any leading/trailing space
+    var textNodes = div
+      .contents()
+      .filter((_, element) => {
+        return element.nodeType === 3;
+      })
+      .remove()
+      .text()
+      .trim();
+
+    if (!textNodes.length) {
+      p.html(`<strong>${text}:</strong> ${p.html()}`);
+    } else {
+      div.append(`<p>${p.html()}</p>`);
+      p.html(`<strong>${text}:</strong> ${textNodes}`);
+    }
+
     h4.remove();
   });
 }
