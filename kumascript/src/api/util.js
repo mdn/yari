mdn/yari

commit 49f5317df4963018c8be94dfc5f50b6e56b1079c

(Auto)EmbedLiveSample (#3973)
* first stab at auto-embed-live-sample

* add token information to EmbedLiveSamples to enrich flaws

* remove some logs

* Update kumascript/src/live-sample.js

Restrict the live-sample page builds to iframes referencing live-samples on the current page.

* test increased timeout

* globally increased timeout

* more timeout

* one last comical attempt at increasing test timoeut

* enough with the shenanigans

* use generator for collecting live-sample code

* increase page default timeout

* increase page default timeout

* more timeouting testing

* more timeouting testing

* more timeouting testing

* remove timeout stuff

* wait for networkIdle

* simplify live-sample extraction conditional control flow

* set iframe title for AutoEmbedLiveSamples

* waitUntil page nav

* change networkIdle value

* try networkIdle0

* add a bit of arbitrary timeout

* add a bit of arbitrary timeout

* use playwright for headless tests

* start server from playwright tests

* revert adding playwright

* upload built docs

* upload built docs & screenshot

* upload screenshot

* log grid files

* remove log

* lowercase live sample IDs

* remove upload artifacts action

* explicitly test for live_sample names

* fix lint

* use rawBody to construct KS livesample error

* remove logs

* stop collecting flawed liveSamples

* change "let" to "const"

* fix caching of EmbedLiveSample macros

If the macro is "EmbedLiveSample", there are cases when the same call signatures yield different results. For example, when the live-sample ID provided as the first argument can't be found or is not provided at all, the result depends on the macro's location in the document.

* make local live-samples behave like deployed

@shapkarin deserves most of the credit for this fix

* prettier fix

* improve live-sample error messages

kumascript/src/api/util.js
@@ -67,9 +67,68 @@ function safeDecodeURIComponent(text) {
   }
 }
 
+const findSectionStart = ($, sectionID) =>
+  $(`#${cssesc(sectionID, { isIdentifier: true })}`);
+
+const hasHeading = ($, sampleID) =>
+  !sampleID ? false : findSectionStart($, sampleID).length > 0;
+
+function findTopLevelParent($el) {
+  while ($el.siblings(":header").length == 0 && $el.parent().length > 0) {
+    $el = $el.parent();
+  }
+  return $el;
+}
+
+const getLevel = ($header) => parseInt($header[0].name[1], 10);
+
+const getHigherHeaderSelectors = (upTo) =>
+  Array.from({ length: upTo }, (_, i) => "h" + (i + 1)).join(", ");
+
+function* collectLevels($el) {
+  // Initialized to 7 so that we pick up the lowest heading level which is <h6>
+  let level = 7;
+  let $prev = $el;
+  while (level !== 1) {
+    const nextHigherLevel = getHigherHeaderSelectors(level - 1);
+    const $header = $prev.prevAll(nextHigherLevel).first();
+    if ($header.length == 0) {
+      return;
+    }
+    level = getLevel($header);
+    $prev = $header;
+    yield $header.add($header.nextUntil(nextHigherLevel));
+  }
+}
+
+function collectClosestCode($start) {
+  const $el = findTopLevelParent($start);
+  for (const $level of collectLevels($el)) {
+    const pairs = LIVE_SAMPLE_PARTS.map((part) => {
+      const selector = `.${part}, pre[class*="brush:${part}"], pre[class*="${part};"]`;
+      const $filtered = $level.find(selector).add($level.filter(selector));
+      return [
+        part,
+        $filtered
+          .map((i, element) => cheerio(element).text())
+          .get()
+          .join("\n"),
+      ];
+    });
+    if (pairs.some(([, code]) => !!code)) {
+      $start.prop("title", $level.first(":header").text());
+      return Object.fromEntries(pairs);
+    }
+  }
+  return null;
+}
+
 class HTMLTool {
   constructor(html, pathDescription) {
-    this.$ = cheerio.load(html, { decodeEntities: true });
+    this.$ =
+      typeof html == "string"
+        ? cheerio.load(html, { decodeEntities: true })
+        : html;
     this.pathDescription = pathDescription;
   }
 
@@ -139,7 +198,7 @@ class HTMLTool {
     // Kuma looks for the first HTML tag of a limited set of section tags with ANY
     // attribute equal to the "sectionID", but in practice it's always an "id" attribute,
     // so let's simplify this as well as make it much faster.
-    const sectionStart = $(`#${cssesc(sectionID, { isIdentifier: true })}`);
+    const sectionStart = findSectionStart($, sectionID);
     if (!sectionStart.length) {
       let errorMessage = `unable to find an HTML element with an "id" of "${sectionID}" within ${this.pathDescription}`;
       const hasMoreThanAscii = [...sectionID].some(
@@ -182,33 +241,44 @@ class HTMLTool {
   }
 
   extractLiveSampleObject(sampleID) {
-    const result = Object.create(null);
-    const sample = this.getSection(sampleID);
-    // We have to wrap the collection of elements from the section
-    // we've just acquired because we're going to search among all
-    // descendants and we want to include the elements themselves
-    // as well as their descendants.
-    const $ = cheerio.load(`<div>${cheerio.html(sample)}</div>`);
-    for (const part of LIVE_SAMPLE_PARTS) {
-      const src = $(
-        `.${part},pre[class*="brush:${part}"],pre[class*="${part};"]`
-      )
-        .map((i, element) => {
-          return $(element).text();
-        })
-        .get()
-        .join("\n");
-      // The string replacements below have been carried forward from Kuma:
-      //   * Bugzilla 819999: &nbsp; gets decoded to \xa0, which trips up CSS.
-      //   * Bugzilla 1284781: &nbsp; is incorrectly parsed on embed sample.
-      result[part] = src ? src.replace(/\u00a0/g, " ") : null;
-    }
-    if (!LIVE_SAMPLE_PARTS.some((part) => result[part])) {
-      throw new KumascriptError(
-        `unable to find any live code samples for "${sampleID}" within ${this.pathDescription}`
+    const sectionID = sampleID.substr("frame_".length);
+    if (hasHeading(this.$, sectionID)) {
+      const result = Object.create(null);
+      const sample = this.getSection(sectionID);
+      // We have to wrap the collection of elements from the section
+      // we've just acquired because we're going to search among all
+      // descendants and we want to include the elements themselves
+      // as well as their descendants.
+      const $ = cheerio.load(`<div>${cheerio.html(sample)}</div>`);
+      for (const part of LIVE_SAMPLE_PARTS) {
+        const src = $(
+          `.${part}, pre[class*="brush:${part}"], pre[class*="${part};"]`
+        )
+          .map((i, element) => $(element).text())
+          .get()
+          .join("\n");
+        // The string replacements below have been carried forward from Kuma:
+        //   * Bugzilla 819999: &nbsp; gets decoded to \xa0, which trips up CSS.
+        //   * Bugzilla 1284781: &nbsp; is incorrectly parsed on embed sample.
+        result[part] = src ? src.replace(/\u00a0/g, " ") : null;
+      }
+      if (!LIVE_SAMPLE_PARTS.some((part) => result[part])) {
+        throw new KumascriptError(
+          `unable to find any live code samples for "${sectionID}" within ${this.pathDescription}`
+        );
+      }
+      return result;
+    } else {
+      const result = collectClosestCode(
+        this.$("#" + cssesc(sampleID, { isIdentifier: true }))
       );
+      if (!result) {
+        throw new KumascriptError(
+          `unable to find any live code samples for "${sectionID}" within ${this.pathDescription}`
+        );
+      }
+      return result;
     }
-    return result;
   }
 
   html() {

build/index.js
@@ -31,6 +31,7 @@ const { gather: gatherGitHistory } = require("./git-history");
 const { buildSPAs } = require("./spas");
 const { renderCache: renderKumascriptCache } = require("../kumascript");
 const LANGUAGES_RAW = require("../content/languages.json");
+const { safeDecodeURIComponent } = require("../kumascript/src/api/util");
 
 const LANGUAGES = new Map(
   Object.entries(LANGUAGES_RAW).map(([locale, data]) => {
@@ -298,8 +299,8 @@ async function buildDocument(document, documentOptions = {}) {
 
   doc.flaws = {};
 
-  let renderedHtml = "";
   let flaws = [];
+  let renderedHtml = "";
   const liveSamples = [];
 
   if (options.clearKumascriptRenderCache) {
@@ -319,26 +320,21 @@ async function buildDocument(document, documentOptions = {}) {
         `MacroInvocationError trying to parse ${error.filepath}, line ${error.line} column ${error.column} (${error.error.message})`
       );
     }
-
     // Any other unexpected error re-thrown.
     throw error;
   }
 
-  const sampleIds = kumascript.getLiveSampleIDs(
-    document.metadata.slug,
+  const $ = cheerio.load(`<div id="_body">${renderedHtml}</div>`);
+
+  const liveSamplePages = kumascript.buildLiveSamplePages(
+    document.url,
+    document.metadata.title,
+    $,
     document.rawBody
   );
-  for (const sampleIdObject of sampleIds) {
-    const liveSamplePage = kumascript.buildLiveSamplePage(
-      document.url,
-      document.metadata.title,
-      renderedHtml,
-      sampleIdObject
-    );
-
-    let liveSampleHTML = liveSamplePage.html;
-    if (liveSamplePage.flaw) {
-      const flaw = liveSamplePage.flaw.updateFileInfo(fileInfo);
+  for (let { id, html, flaw } of liveSamplePages) {
+    if (flaw) {
+      flaw.updateFileInfo(fileInfo);
       if (flaw.name === "MacroLiveSampleError") {
         // As of April 2021 there are 0 pages in mdn/content that trigger
         // a MacroLiveSampleError. So we can be a lot more strict with en-US
@@ -356,7 +352,7 @@ async function buildDocument(document, documentOptions = {}) {
         }
       }
       flaws.push(flaw);
-      liveSampleHTML = `<!doctype html>
+      html = `<!doctype html>
         <html>
           <head>
             <meta charset="utf-8">
@@ -382,10 +378,7 @@ async function buildDocument(document, documentOptions = {}) {
         </html>
         `;
     }
-    liveSamples.push({
-      id: sampleIdObject.id.toLowerCase(),
-      html: liveSampleHTML,
-    });
+    liveSamples.push({ id: id.toLowerCase(), html });
   }
 
   if (flaws.length) {
@@ -438,7 +431,9 @@ async function buildDocument(document, documentOptions = {}) {
   // its output with the `folder`.
   validateSlug(metadata.slug);
 
-  const $ = cheerio.load(`<div id="_body">${renderedHtml}</div>`);
+  // EmbedLiveSamples carry their token information to enrich flaw error
+  // messages, these should not be in the final output
+  $("[data-token]").removeAttr("data-token");
 
   // Kumascript rendering can't know about FLAW_LEVELS when it's building,
   // because injecting it there would cause a circular dependency.
@@ -620,30 +615,30 @@ async function buildLiveSamplePageFromURL(url) {
     throw new Error(`Unexpected URL format to extract live sample ('${url}')`);
   }
   const [documentURL, sampleID] = url.split(/\.html$/)[0].split("/_sample_.");
+  const decodedSampleID = safeDecodeURIComponent(sampleID).toLowerCase();
   const document = Document.findByURL(documentURL);
   if (!document) {
     throw new Error(`No document found for ${documentURL}`);
   }
-  // Convert the lower-case sampleID we extract from the incoming URL into
-  // the actual sampleID object with the properly-cased live-sample ID.
-  for (const sampleIDObject of kumascript.getLiveSampleIDs(
-    document.metadata.slug,
-    document.rawBody
-  )) {
-    if (sampleIDObject.id.toLowerCase() === sampleID) {
-      const liveSamplePage = kumascript.buildLiveSamplePage(
-        document.url,
-        document.metadata.title,
-        (await kumascript.render(document.url))[0],
-        sampleIDObject
-      );
-      if (liveSamplePage.flaw) {
-        throw new Error(liveSamplePage.flaw.toString());
-      }
-      return liveSamplePage.html;
+  const liveSamplePage = kumascript
+    .buildLiveSamplePages(
+      document.url,
+      document.metadata.title,
+      (await kumascript.render(document.url))[0],
+      document.rawBody
+    )
+    .find((page) => page.id.toLowerCase() == decodedSampleID);
+
+  if (liveSamplePage) {
+    if (liveSamplePage.flaw) {
+      throw new Error(liveSamplePage.flaw.toString());
     }
+    return liveSamplePage.html;
   }
-  throw new Error(`No live-sample "${sampleID}" found within ${documentURL}`);
+
+  throw new Error(
+    `No live-sample "${decodedSampleID}" found within ${documentURL}`
+  );
 }
 
 // This is used by the builder (yarn build) and by the server (JIT).

client/src/document/index.tsx
@@ -71,6 +71,7 @@ export function Document(props /* TODO: define a TS interface for this */) {
           ? props.doc
           : null,
       revalidateOnFocus: CRUD_MODE,
+      refreshInterval: CRUD_MODE ? 500 : 0,
     }
   );
 

kumascript/index.js
@@ -11,7 +11,7 @@ const info = require("./src/info.js");
 const { render: renderMacros } = require("./src/render.js");
 const {
   getLiveSampleIDs,
-  buildLiveSamplePage,
+  buildLiveSamplePages,
   LiveSampleError,
 } = require("./src/live-sample.js");
 const { HTMLTool } = require("./src/api/util.js");
@@ -114,7 +114,7 @@ const renderFromURL = async (
 };
 
 module.exports = {
-  buildLiveSamplePage,
+  buildLiveSamplePages,
   getLiveSampleIDs,
   LiveSampleError,
   render: renderFromURL,

kumascript/macros/EmbedLiveSample.ejs
@@ -14,7 +14,7 @@
 //
 // See also : LiveSampleLink
 
-var sampleId = $0;
+var sampleId = $0 || $token.location.start.offset;
 var width = $1;
 var height = $2;
 var screenshotUrl = $3;
@@ -80,5 +80,6 @@ if (width) { %> width="<%= width %>"<% }
 if (height) { %> height="<%= height %>"<% }
 %> src="<%- url %>"<%
 if (allowedFeatures) { %> allow="<%= allowedFeatures %>"<% }
+if ($token) { %> data-token="<%= JSON.stringify($token) %>"<% }
 %>></iframe><%
 if (hasScreenshot) { %></td></tr></tbody></table><% } %>

kumascript/src/environment.js
@@ -159,7 +159,7 @@ class Environment {
 
   // Get a customized environment object that is specific to a single
   // macro on a page by including the arguments to be passed to that macro.
-  getExecutionContext(args) {
+  getExecutionContext(args, token = null) {
     const context = Object.create(this.prototypeEnvironment);
 
     // Make a defensive copy of the arguments so that macros can't
@@ -187,6 +187,10 @@ class Environment {
     for (let i = args.length; i < 10; i++) {
       context[`$${i}`] = "";
     }
+
+    // Position of the macro inside of the source, which we need
+    // to identify EmbedLiveSample macros which do not receive an ID
+    context["$token"] = token;
 
     return context;
   }

kumascript/src/live-sample.js
@@ -1,14 +1,8 @@
+const cheerio = require("cheerio");
 const ejs = require("ejs");
 
-const Parser = require("./parser.js");
 const { MacroLiveSampleError } = require("./errors.js");
-const { normalizeMacroName } = require("./render.js");
-const {
-  HTMLTool,
-  slugify,
-  safeDecodeURIComponent,
-  KumascriptError,
-} = require("./api/util.js");
+const { HTMLTool, KumascriptError, slugify } = require("./api/util.js");
 
 const LIVE_SAMPLE_HTML = `
 <!DOCTYPE html>
@@ -69,100 +63,39 @@ const LIVE_SAMPLE_HTML = `
 
 const liveSampleTemplate = ejs.compile(LIVE_SAMPLE_HTML);
 
-function buildLiveSamplePage(uri, title, source, sampleIDObject) {
+function buildLiveSamplePages(uri, title, $, rawBody) {
   // Given the URI, title, and rendered HTML of a document, build
-  // and return the HTML of the live-sample page for the given
-  // sampleID that this document or another references, or else
-  // return an instance of MacroLiveSampleError.
-  const result = { html: null, flaw: null };
-  const tool = new HTMLTool(source, uri);
-  let sampleData;
-  try {
-    sampleData = tool.extractLiveSampleObject(sampleIDObject.id);
-  } catch (e) {
-    if (e instanceof KumascriptError) {
-      result.flaw = sampleIDObject.createFlaw(e);
-      return result;
-    }
-    throw e;
+  // and return the HTML of the live-sample pages for the given
+  // document or else collect flaws
+  if (typeof $ == "string") {
+    $ = cheerio.load($);
   }
-  sampleData.sampleTitle = `${title} - ${sampleIDObject.id} - code sample`;
-  result.html = liveSampleTemplate(sampleData);
-  return result;
-}
-
-function normalizeSlug(slug) {
-  // Trim the slug, remove all leading and trailing forward
-  // slashes, and convert to lower case.
-  return slug
-    .trim()
-    .replace(/^\/+|\/+$/g, "")
-    .toLowerCase();
-}
-
-class LiveSampleID {
-  constructor(id, source, token) {
-    this.id = id;
-    this.source = source;
-    this.token = token;
-  }
-
-  createFlaw(message) {
-    return new MacroLiveSampleError(
-      message instanceof Error ? message : new Error(message),
-      this.source,
-      this.token
-    );
-  }
-}
-
-function getLiveSampleIDs(slug, source) {
-  // Given a slug and its raw source HTML, parses the source and returns
-  // a list of live-sample ID's to be extracted from this document.
-  const tokens = Parser.parse(source);
-  const currentSlug = normalizeSlug(slug);
-  // Loop through the tokens, looking for calls to the EmbedLiveSample macro.
-  // The first argument to the call is the live-sample ID, and there may also
-  // be an optional fifth argument that specifies a slug, that may or may not
-  // be different from the current slug, from which to extract the sample ID.
-  const result = [];
-  for (const token of tokens) {
-    if (token.type !== "MACRO") continue;
-    const normalizedMacroName = normalizeMacroName(token.name);
-    if (normalizedMacroName === "inheritancediagram") {
-      // The InheritanceDiagram is a special macro that, unlike other EmbedLiveSample
-      // macros in that it itself renders the "EmbedLiveSample" with a set of
-      // hardcoded options. The only thing that is variable and comes from the
-      // the raw HTML is the size. So make an exception for this otherwise, the
-      // HTML element ID is lost and this function won't discovered that it is
-      // in fact a live sample here.
-      result.push(new LiveSampleID("inheritance_diagram", source, token));
-    } else if (
-      (normalizedMacroName === "embedlivesample" ||
-        normalizedMacroName === "livesamplelink") &&
-      token.args.length
-    ) {
-      // Some of the localized pages URI-encode their first argument,
-      // the live-sample ID, even though they don't need to do that,
-      // so let's first call "safeDecodeURIComponent" just in case.
-      const sampleID = slugify(safeDecodeURIComponent(token.args[0]));
-      const sampleIDObject = new LiveSampleID(sampleID, source, token);
-      if (token.args.length > 4) {
-        // Some calls to EmbedLiveSample explicitly specify a slug from which
-        // to extract the live sample ID in the 5th argument (4th index), so
-        // we can't just assume that all of the sample ID's are to be extracted
-        // from the current document.
-        const slugArg = normalizeSlug(token.args[4]);
-        if (slugArg && slugArg !== currentSlug) {
-          // If this live-sample is to be extracted from another document,
-          // don't return it as part of this document's list of sample ID's.
-          continue;
+  return $("iframe")
+    .filter((i, iframe) => $(iframe).attr("src").includes(`${uri}/_sample_.`))
+    .map((i, iframe) => {
+      const iframeId = $(iframe).attr("id");
+      const id = slugify(iframeId.substr("frame_".length));
+      const result = { id, html: null, flaw: null };
+      const tool = new HTMLTool($, uri);
+      let sampleData;
+      try {
+        sampleData = tool.extractLiveSampleObject(iframeId);
+      } catch (error) {
+        if (error instanceof KumascriptError) {
+          result.flaw = new MacroLiveSampleError(
+            error,
+            rawBody,
+            JSON.parse($(iframe).attr("data-token"))
+          );
+          return result;
         }
+        throw error;
       }
-      result.push(sampleIDObject);
-    }
-  }
-  return result;
+      sampleData.sampleTitle = `${title} - ${id} - code sample`;
+      result.html = liveSampleTemplate(sampleData);
+      return result;
+    })
+    .get();
 }
 
-module.exports = { buildLiveSamplePage, getLiveSampleIDs };
+module.exports = { buildLiveSamplePages };

kumascript/src/render.js
@@ -199,16 +199,21 @@ async function render(
     };
 
     // If the token signature is already in the map, then we've
-    // already run the macro. We're only going to use the prior run
-    // if there were no errors. If there were errors in the prior run,
-    // let's re-run the macro in order to capture the context in fresh
-    // errors.
+    // already run the macro. We're only going to use the prior run if
+    // there were no errors and if the macro isn't "EmbedLiveSample".
+    // If there were errors in the prior run, let's re-run the macro in
+    // order to capture the context in fresh errors. If the macro is
+    // "EmbedLiveSample", there are cases when the same call signatures
+    // yield different results. For example, when the live-sample ID
+    // provided as the first argument can't be found or is not provided
+    // at all, the result depends on the macro's location in the document.
     const priorResult = signatureToResult.get(token.signature);
 
     if (
       priorResult &&
       !priorResult.errors.fatal &&
-      !priorResult.errors.nonFatal
+      !priorResult.errors.nonFatal &&
+      macroName !== "embedlivesample"
     ) {
       currentResult.output = priorResult.output;
     } else {
@@ -219,7 +224,7 @@ async function render(
       try {
         currentResult.output = await templates.render(
           macroName,
-          environment.getExecutionContext(token.args)
+          environment.getExecutionContext(token.args, token)
         );
       } catch (e) {
         let macroError;

commit 18cf3d21e58d6a11b04d83e9260fe1e87a950e3f

Fix EmbedLiveSample when there are subheadings
This fixes a regression introduced by commit 49f5317df4963018c8be94dfc5f50b6e56b1079c.

kumascript/src/api/util.js
@@ -280,9 +280,7 @@ class HTMLTool {
       }
       return result;
     } else {
-      const result = collectClosestCode(
-        this.$(`#${minimalIDEscape(sampleID)}`)
-      );
+      const result = collectClosestCode(findSectionStart(this.$, sectionID));
       if (!result) {
         throw new KumascriptError(
           `unable to find any live code samples for "${sectionID}" within ${this.pathDescription}`