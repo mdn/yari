89281b456e57905afc7afa9bee2c8cb5005dc958
Schalk Neethling <sneethling@mozilla.com>
2021-09-09 10:50:57 +0200

    chore: integrate style dictionary
    Integrate style dictionary to allow building of design tokens from Figma JSON exports.

---
+++ sd-config.js
@@ -0,0 +1,45 @@
+const StyleDictionary = require("style-dictionary"); // eslint-disable-line node/no-unpublished-require
+
+StyleDictionary.registerTransform({
+  name: "value/rewrite",
+  type: "value",
+  transformer: function (token) {
+    if (token.value.startsWith("$")) {
+      const baseTokenValue = token.value.replace("$", "");
+      return `{${baseTokenValue}.value}`;
+    }
+    return token.value;
+  },
+});
+
+module.exports = {
+  source: ["./client/src/ui/style-dictionary/**/*.json"],
+  platforms: {
+    scss: {
+      transformGroup: "scss",
+      transforms: ["value/rewrite", "name/cti/kebab"],
+      buildPath: "./client/src/ui/vars/sass/",
+      prefix: "token",
+      files: [
+        {
+          destination: "_variables.scss",
+          format: "scss/variables",
+          options: {
+            outputReferences: true,
+          },
+        },
+      ],
+    },
+    js: {
+      transformGroup: "js",
+      transforms: ["name/cti/camel", "value/rewrite"],
+      buildPath: "./client/src/ui/vars/js/",
+      files: [
+        {
+          destination: "variables.js",
+          format: "javascript/es6",
+        },
+      ],
+    },
+  },
+};
