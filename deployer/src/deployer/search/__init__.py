71c601a9869f3ca9d9de1ab9391b07bea1b2f319
rjohnson <rjohnson@mozilla.com>
2021-10-19 13:10:21 -0700

    fix elasticsearch error key

--- deployer/src/deployer/search/__init__.py
+++ deployer/src/deployer/search/__init__.py
@@ -108,9 +108,9 @@ def index(
                 count_worked += 1
             else:
                 count_errors += 1
-                print("Error dictionary:")
-                print(json.dumps(info["index"]["error"]))
-                errors_counter[info["index"]["error"]] += 1
+                error_data = info["index"]["error"]
+                error_key = f"{error_data['type']}: {error_data['reason']}"
+                errors_counter[error_key] += 1
             count_done += 1
             bar.update(1)
 
