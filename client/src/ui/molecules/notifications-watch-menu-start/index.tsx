448494a00484c5aeb36e8e05c11ea8aa2f4930ee
Michael Trythall <m@mtrythall.com>
2021-11-19 00:56:09 -0500

    Form submission for Watch Menu

--- client/src/ui/molecules/notifications-watch-menu-start/index.tsx
+++ client/src/ui/molecules/notifications-watch-menu-start/index.tsx
@@ -1,12 +1,76 @@
+import React, { useState } from "react";
+import useSWR, { mutate } from "swr";
+interface WatchModeData {
+  modeType: string;
+  csrfmiddlewaretoken: string;
+}
+
 export function NotificationsWatchMenuStart({ setStepHandler }) {
+  const [watchMode, setWatchMode] = useState<string>("major");
+
+  const setWatchAPIEndpoint = "/api/v1/plus/watch/set-watch/";
+  const apiPostURL = `${setWatchAPIEndpoint}${watchMode}`;
+  const slug = "page-slug"; // Unique ID for the page
+
+  // Get existing Watch Mode
+  // TODO: Is pathname correct here?
+  // Returns "major", "custom", or "unwatch"
+  const apiURL = `/api/v1/plus/watch/get-mode/${slug}`;
+  const { data, error } = useSWR<WatchModeData>(
+    apiURL,
+    async (url) => {
+      const response = await fetch(url);
+      if (!response.ok) {
+        const text = await response.text();
+        throw new Error(`${response.status} on ${url}: ${text}`);
+      }
+      return await response.json();
+    },
+    {
+      revalidateOnFocus: true,
+    }
+  );
+
+  const handleSelection = (event) => {
+    setWatchMode(event.currentTarget.value);
+  };
+
+  async function handleWatchSelection(event) {
+    event.preventDefault();
+
+    if (!data) {
+      return null;
+    }
+
+    const response = await fetch(apiPostURL, {
+      method: "POST",
+      headers: {
+        "X-CSRFToken": data.csrfmiddlewaretoken,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`${response.status} on ${response.url}`);
+    }
+    await mutate(apiURL);
+    return true;
+  }
+
   return (
-    <form action={""} method="POST">
+    <form
+      action={setWatchAPIEndpoint}
+      method="POST"
+      onSubmit={handleWatchSelection}
+    >
       <div className="watch-submenu-header">Notifications</div>
 
       <button
         role="menuitemradio"
         aria-checked="false"
         className="watch-menu-button"
+        value="major"
+        onClick={handleSelection}
       >
         <span className="watch-menu-button-wrap">
           <span className="watch-menu-button-status">✅</span>
@@ -28,7 +92,10 @@ export function NotificationsWatchMenuStart({ setStepHandler }) {
         aria-checked="true"
         aria-haspopup="true"
         className="watch-menu-button"
-        onClick={setStepHandler}
+        onClick={(event) => {
+          event.preventDefault();
+          setStepHandler(1);
+        }}
       >
         <span className="watch-menu-button-wrap">
           <span className="watch-menu-button-status"></span>
@@ -45,6 +112,8 @@ export function NotificationsWatchMenuStart({ setStepHandler }) {
         role="menuitemradio"
         aria-checked="false"
         className="watch-menu-button"
+        value="unwatch"
+        onClick={handleSelection}
       >
         <span className="watch-menu-button-wrap">
           <span className="watch-menu-button-status"></span>
