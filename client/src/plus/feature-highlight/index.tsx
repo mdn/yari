f7e1a760c12e029e73898e88ee5a01713b0b7f33
Daryl Alexsy <dalexsy@mozilla.com>
2021-12-01 17:20:27 +0100

    added feature page

--- client/src/plus/feature-highlight/index.tsx
+++ client/src/plus/feature-highlight/index.tsx
@@ -3,7 +3,46 @@ import "./index.scss";
 function FeatureHighlight() {
   return (
     <div className="feature-highlight">
-      <div className="wrapper">I am a feature highlight.</div>
+      <div className="wrapper">
+        <div className="ft-sidebar">
+          <a href="/">← Back to Overview</a>
+        </div>
+        <div className="ft-content">
+          <div className="ft-title">
+            <h4 className="ft-name">Bookmarking</h4>
+            <span className="ft-headline">
+              Get more from your note-ta&shy;king app
+            </span>
+            <p>
+              Remember everything and tackle any project with your notes, tasks,
+              and schedule all in one place.
+            </p>
+          </div>
+          <div className="ft-article">
+            <h1>Getting started</h1>
+            <p>
+              Collections are essentially just folders. Whether you’re planning
+              a presentation, preparing for an event or creating a website,
+              create a collection so all the important items are saved in one
+              central place.
+            </p>
+            <h2>Bookmarking a page</h2>
+            <p>
+              Collections are essentially just folders. Whether you’re planning
+              a presentation, preparing for an event or creating a website,
+              create a collection so all the important items are saved in one
+              central place.
+            </p>
+            <h2>Bookmarking a page</h2>
+            <p>
+              Collections are essentially just folders. Whether you’re planning
+              a presentation, preparing for an event or creating a website,
+              create a collection so all the important items are saved in one
+              central place.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
