7cac5a76400281644e984f8364b87384c4a6ac1b
Kasey Kelly <kasey@servee.com>
2022-01-11 17:20:59 -0500

    ref #371, code example styling

--- client/src/document/ingredients/browser-compatibility-table/feature-row.tsx
+++ client/src/document/ingredients/browser-compatibility-table/feature-row.tsx
@@ -327,7 +327,7 @@ function getNotes(
           : null,
         item.prefix
           ? {
-              iconName: "footnote",
+              iconName: "prefix",
               label: `Implemented with the vendor prefix: ${item.prefix}`,
             }
           : null,
