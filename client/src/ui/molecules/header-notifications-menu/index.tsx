390009422c7088f434f226b1799a05216cc4d364
Nicolas Lara <nicolaslara@gmail.com>
2022-01-25 14:53:31 +0100

    added placeholder for handling max watched pages for unsubscribed users

--- client/src/ui/molecules/header-notifications-menu/index.tsx
+++ client/src/ui/molecules/header-notifications-menu/index.tsx
@@ -166,7 +166,7 @@ export const HeaderNotificationsMenu = () => {
           <>
             <li className="notifications-submenu-header">
               <div className="notifications-submenu-item-heading">
-                Notifications
+                Notifications ({notificationCount})
               </div>
               <a
                 href={`/${locale}/plus/notifications/`}
