8378b87a5fc7a4350090e2877792291064b1a62a
Guy Sleigh <gsleigh@mozilla.com>
2022-02-09 13:21:53 +0100

    687 Frequently visited articles tab (#775)
    Add Frequently-visited-tab using localstorage.

--- client/src/document/hooks.ts
+++ client/src/document/hooks.ts
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-import { Doc } from "./types";
+import { Doc, FrequentlyViewedEntry } from "./types";
 
 export function useDocumentURL() {
   const { "*": slug, locale } = useParams();
@@ -110,4 +109,115 @@ function getCopiedMessageElement(wrapper: HTMLElement) {
     wrapper.appendChild(element);
   }
   return element;
+}
+
+const FREQUENTLY_VIEWED_STORAGE_KEY = "frequently-viewed-documents";
+const FREQUENTLY_VIEWED_MAX_ITEMS = 20;
+
+const sortByVisitsThenTimestampDesc = (
+  first: FrequentlyViewedEntry,
+  second: FrequentlyViewedEntry
+) => {
+  if (first.visitCount > second.visitCount) return -1;
+  if (first.visitCount < second.visitCount) return 1;
+  if (first.timestamp < second.timestamp) return 1;
+  if (first.timestamp > second.timestamp) return -1;
+  return 0;
+};
+
+export function useFrequentlyViewed(): [
+  FrequentlyViewedEntry[],
+  (arg: FrequentlyViewedEntry[]) => void
+] {
+  const [entries, setEntries] = useState<FrequentlyViewedEntry[]>([]);
+  const [updated, setUpdated] = useState(false);
+
+  useEffect(() => {
+    const entries = JSON.parse(
+      localStorage.getItem(FREQUENTLY_VIEWED_STORAGE_KEY) || "[]"
+    );
+    const newEntries: FrequentlyViewedEntry[] = [];
+
+    for (const entry of entries) {
+      newEntries.push({
+        url: entry.url,
+        title: entry.title,
+        timestamp: entry.timestamp,
+        parents: entry?.parents || [],
+        visitCount: entry.visitCount,
+      });
+    }
+    //Reset sorting in case of undelete
+    setEntries(newEntries.sort(sortByVisitsThenTimestampDesc));
+    setUpdated(false);
+  }, [updated]);
+
+  const setStoredEntries = (value: FrequentlyViewedEntry[]) => {
+    try {
+      setEntries(entries);
+      window.localStorage.setItem(
+        FREQUENTLY_VIEWED_STORAGE_KEY,
+        JSON.stringify(value.slice(0, FREQUENTLY_VIEWED_MAX_ITEMS))
+      );
+      setUpdated(true);
+    } catch (err) {
+      console.error(`Failed to write to localStorage: ${err}`);
+    }
+  };
+
+  return [entries, setStoredEntries];
+}
+
+/**
+ * @param  {Doc|undefined} doc
+ * Persists frequently viewed docs to localstorage as part of MDN Plus MVP.
+ *
+ */
+export function usePersistFrequentlyViewed(doc: Doc | undefined) {
+  useEffect(() => {
+    if (!doc) {
+      return;
+    }
+    let frequentlyViewed = JSON.parse(
+      localStorage.getItem(FREQUENTLY_VIEWED_STORAGE_KEY) || "[]"
+    );
+
+    const newEntry: FrequentlyViewedEntry = {
+      url: doc.mdn_url,
+      title: doc.title,
+      parents: doc.parents,
+      timestamp: new Date().getTime(),
+      visitCount: 1,
+    };
+
+    if (frequentlyViewed.length === 0) {
+      localStorage.setItem(
+        FREQUENTLY_VIEWED_STORAGE_KEY,
+        JSON.stringify([newEntry])
+      );
+      return;
+    }
+
+    const index = frequentlyViewed.findIndex(
+      (entry) => entry.url === newEntry.url
+    );
+
+    if (index !== -1) {
+      frequentlyViewed[index].timestamp = new Date().getTime();
+      frequentlyViewed[index].visitCount += 1;
+    } else {
+      frequentlyViewed.unshift(newEntry);
+    }
+
+    //Sort descending so most frequently viewed appears on top.
+    frequentlyViewed = frequentlyViewed.sort(sortByVisitsThenTimestampDesc);
+    try {
+      localStorage.setItem(
+        FREQUENTLY_VIEWED_STORAGE_KEY,
+        JSON.stringify(frequentlyViewed.slice(0, FREQUENTLY_VIEWED_MAX_ITEMS))
+      );
+    } catch (err) {
+      console.error(`Failed to write to localStorage: ${err}`);
+    }
+  });
 }
