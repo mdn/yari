10a1328bdc2e776a0a48d0e932722aedf850c310
Florian Merz <me@fiji-flo.de>
2022-02-16 18:12:30 +0100

    intial offline support

--- client/src/offline-settings/index.tsx
+++ client/src/offline-settings/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 import { PageContentContainer } from "../ui/atoms/page-content";
+import { useUserData } from "../user-context";
 
 const SettingsApp = React.lazy(() => import("./settings"));
 
-export function AppSettings() {
+export function OfflineSettings() {
+  const user = useUserData();
   const pageTitle = "Settings";
   return (
     <PageContentContainer extraClasses="settings">
       <h1 className="slab-highlight">{pageTitle} </h1>
-      <SettingsApp></SettingsApp>
+      {user?.isSubscriber && <SettingsApp />}
     </PageContentContainer>
   );
 }
