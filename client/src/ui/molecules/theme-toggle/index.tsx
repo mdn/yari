e6d6ee2792abdc0e4c400b435831e0e97bbfd6fb
Schalk Neethling <sneethling@mozilla.com>
2022-03-08 18:12:02 +0200

    switch to new layout showing both icons

--- client/src/ui/molecules/theme-toggle/index.tsx
+++ client/src/ui/molecules/theme-toggle/index.tsx
@@ -28,13 +28,14 @@ export function ThemeToggle() {
 
   return (
     <div className="theme-toggle">
-      {dark ? <Icon name="moon-fill"></Icon> : <Icon name="sun-fill"></Icon>}
+      <Icon name="sun-fill" />
       <Switch
         name="themeToggle"
         hiddenLabel={`toggle theme to ${dark ? "light" : "dark"} mode`}
         checked={dark}
         toggle={toggle}
       ></Switch>
+      <Icon name="moon-fill" />
     </div>
   );
 }
