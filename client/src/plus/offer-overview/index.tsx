bee15a5f824cd2e56e043effc8da8f331a712f3b
Florian Merz <me@fiji-flo.de>
2022-03-11 15:24:19 +0100

    plus page

--- client/src/plus/offer-overview/index.tsx
+++ client/src/plus/offer-overview/index.tsx
@@ -1,13 +1,13 @@
 import "./index.scss";
 import OfferHero from "./offer-hero";
-import OfferOverviewFeature from "./offer-overview-feature";
+import OfferOverviewFeatures from "./offer-overview-feature";
 import OfferOverviewSubscribe from "./offer-overview-subscribe";
 
 function OfferOverview() {
   return (
     <div className="offer-overview">
       <OfferHero />
-      <OfferOverviewFeature />
+      <OfferOverviewFeatures />
       <OfferOverviewSubscribe />
     </div>
   );
