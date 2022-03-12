5bc53110f94156b584290691b1c6bcac3e179163
alattalatta <alattalatta@sorto.me>
2022-03-12 23:24:28 +0900

    Remove unnecessary useReducer import

--- client/src/document/toolbar/flaws.tsx
+++ client/src/document/toolbar/flaws.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { annotate, annotationGroup } from "rough-notation";
 import { RoughAnnotation } from "rough-notation/lib/model";
