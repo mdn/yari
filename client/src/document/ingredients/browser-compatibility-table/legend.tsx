1a7867bf7740a9d70e0b2c7087c8f44f3648e12d
Daniel Jacobs <danielhunterjacobs@gmail.com>
2022-03-14 15:12:45 -0400

    Address all feeback

--- client/src/document/ingredients/browser-compatibility-table/legend.tsx
+++ client/src/document/ingredients/browser-compatibility-table/legend.tsx
@@ -11,8 +11,8 @@ const LEGEND_LABELS = {
   no: "No support",
   unknown: "Compatibility unknown",
   experimental: "Experimental. Expect behavior to change in the future.",
-  "non-standard": "Non-standard. Check cross-browser support before using.",
-  trash: "Deprecated. Not for use in new websites.",
+  nonstandard: "Non-standard. Check cross-browser support before using.",
+  deprecated: "Deprecated. Not for use in new websites.",
   footnote: "See implementation notes.",
   disabled: "User must explicitly enable this feature.",
   altname: "Uses a non-standard name.",
@@ -35,10 +35,10 @@ function getActiveLegendItems(
         legendItems.add("experimental");
       }
       if (status.deprecated) {
-        legendItems.add("trash");
+        legendItems.add("deprecated");
       }
       if (!status.standard_track) {
-        legendItems.add("non-standard");
+        legendItems.add("nonstandard");
       }
     }
 
