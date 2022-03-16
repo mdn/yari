c85495c5556e5589be7681d8b4bcb118129517f9
Florian Merz <me@fiji-flo.de>
2022-03-16 20:37:24 +0100

    add loading spinner for sw

--- client/src/offline-settings/settings.tsx
+++ client/src/offline-settings/settings.tsx
@@ -5,6 +5,7 @@ import useInterval from "@use-it/interval";
 import { useEffect, useState } from "react";
 import UpdateButton from "./update";
 import ClearButton from "./clear";
+import { Spinner } from "../ui/atoms/spinner";
 
 function displayEstimate({ usage = 0, quota = Infinity }: StorageEstimate) {
   const usageInMib = Math.round(usage / (1024 * 1024));
@@ -24,6 +25,7 @@ export default function SettingsApp({ ...appProps }) {
 function Settings() {
   document.title = "Settings | MDN";
   const [status, setStatus] = useState<UpdateStatus>();
+  const [saving, setSaving] = useState<boolean | null>(true);
 
   const [estimate, setEstimate] = useState<StorageEstimate | null>(null);
   const [settings, setSettings] = useState<SettingsData>();
@@ -36,8 +38,15 @@ function Settings() {
     };
     init().then(() => {});
   }, []);
+  useEffect(() => {
+    const init = async () => {
+      setSaving(false);
+    };
+    init();
+  }, [settings]);
 
   const updateSettings = async (change: SettingsData) => {
+    setSaving(true);
     let newSettings = await window.mdnWorker.setOfflineSettings(change);
     setSettings(newSettings);
   };
@@ -78,73 +87,71 @@ function Settings() {
       <li>
         <h4>Enable offline storage</h4>
         <span>Allow MDN content to be downloaded for offline access</span>
-        <Switch
-          name="offline"
-          checked={settings?.offline || false}
-          toggle={(e) =>
-            updateSettings({
-              offline: e.target.checked,
-            })
-          }
-        ></Switch>
+        {(saving === true && <Spinner extraClasses="loading" />) || (
+          <Switch
+            name="offline"
+            checked={settings?.offline || false}
+            toggle={(e) =>
+              updateSettings({
+                offline: e.target.checked,
+              })
+            }
+          ></Switch>
+        )}
       </li>
       {settings?.offline && (
-        <li>
-          <h4>Prefer online content</h4>
-          <span>
-            Do not use offline content while connected to the internet
-          </span>
-          <Switch
-            name="prefer-online"
-            checked={settings?.preferOnline || false}
-            toggle={(e) =>
-              updateSettings({
-                preferOnline: e.target.checked,
-              })
-            }
-          ></Switch>
-        </li>
-      )}
-      {settings?.offline && (
-        <li>
-          <UpdateButton updateStatus={status || null} update={update} />
-        </li>
-      )}
-      {settings?.offline && window?.location.hash === "#debug" && (
-        <li>
-          <h4>Debug</h4>
-          <span>{JSON.stringify(status, null, 2)}</span>
-        </li>
-      )}
-      {settings?.offline && usage && (
-        <li>
-          <h4>Storage used</h4>
-          <span>
-            MDN offline currently uses <b>{usage}</b>
-          </span>
-        </li>
-      )}
-      {settings?.offline && (
-        <li>
-          <h4>Enable auto-update</h4>
-          <span>
-            Automatically download updates to content enabled for download
-          </span>
-          <Switch
-            name="auto-update"
-            checked={settings?.autoUpdates || false}
-            toggle={(e) =>
-              updateSettings({
-                autoUpdates: e.target.checked,
-              })
-            }
-          ></Switch>
-        </li>
-      )}
-      {settings?.offline && (
-        <li>
-          <ClearButton updateStatus={status || null} clear={clear} />
-        </li>
+        <>
+          <li>
+            <h4>Prefer online content</h4>
+            <span>
+              Do not use offline content while connected to the internet
+            </span>
+            <Switch
+              name="prefer-online"
+              checked={settings?.preferOnline || false}
+              toggle={(e) =>
+                updateSettings({
+                  preferOnline: e.target.checked,
+                })
+              }
+            ></Switch>
+          </li>
+          <li>
+            <UpdateButton updateStatus={status || null} update={update} />
+          </li>
+          {window?.location.hash === "#debug" && (
+            <li>
+              <h4>Debug</h4>
+              <span>{JSON.stringify(status, null, 2)}</span>
+            </li>
+          )}
+          {usage && (
+            <li>
+              <h4>Storage used</h4>
+              <span>
+                MDN offline currently uses <b>{usage}</b>
+              </span>
+            </li>
+          )}
+          <li>
+            <h4>Enable auto-update</h4>
+            <span>
+              Automatically download updates to content enabled for download
+            </span>
+            <Switch
+              name="auto-update"
+              checked={settings?.autoUpdates || false}
+              toggle={(e) =>
+                updateSettings({
+                  autoUpdates: e.target.checked,
+                })
+              }
+            ></Switch>
+          </li>
+          <li>
+            <ClearButton updateStatus={status || null} clear={clear} />
+          </li>
+        </>
       )}
     </ul>
   );
