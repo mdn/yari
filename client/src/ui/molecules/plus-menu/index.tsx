5f32e6b03328e4fca769265a51d9734c9b1ddbb6
Claas Augner <caugner@mozilla.com>
2022-03-21 19:45:46 +0100

    chore(plus-menu): add public faq item

--- client/src/ui/molecules/plus-menu/index.tsx
+++ client/src/ui/molecules/plus-menu/index.tsx
@@ -12,38 +12,50 @@ export const PlusMenu = ({ visibleSubMenuId, toggleMenu }) => {
   const locale = useLocale();
   const userData = useUserData();
 
+  const isAuthenticated = userData && userData.isAuthenticated;
+
   const plusMenu = {
     label: "MDN Plus",
     id: "mdn-plus",
     items: [
+      ...(isAuthenticated
+        ? [
+            {
+              description: "More MDN. Your MDN.",
+              hasIcon: true,
+              extraClasses: "mobile-only",
+              iconClasses: "submenu-icon",
+              label: "Overview",
+              url: `/${locale}/plus`,
+            },
+            {
+              description: "Your saved articles from across MDN",
+              hasIcon: true,
+              iconClasses: "submenu-icon bookmarks-icon",
+              label: "Collections",
+              url: `/${locale}/plus/collection`,
+            },
+            isAuthenticated && {
+              description: "Updates from the pages you’re watching",
+              hasIcon: true,
+              iconClasses: "submenu-icon",
+              label: "Notifications",
+              url: `/${locale}/plus/notifications`,
+            },
+          ]
+        : []),
       {
-        description: "More MDN. Your MDN.",
+        description: "Frequently asked questions about MDN Plus",
         hasIcon: true,
-        extraClasses: "mobile-only",
         iconClasses: "submenu-icon",
-        label: "Overview",
-        url: `/${locale}/plus`,
-      },
-      {
-        description: "Your saved articles from across MDN",
-        hasIcon: true,
-        iconClasses: "submenu-icon bookmarks-icon",
-        label: "Collections",
-        url: `/${locale}/plus/collection`,
-      },
-      {
-        description: "Updates from the pages you’re watching",
-        hasIcon: true,
-        iconClasses: "submenu-icon",
-        label: "Notifications",
-        url: `/${locale}/plus/notifications`,
+        label: "FAQ",
+        url: `/${locale}/plus/faq`,
       },
     ],
   };
   const isOpen = visibleSubMenuId === plusMenu.id;
-  const isAuthenticated = userData && userData.isAuthenticated;
 
-  return isAuthenticated ? (
+  return (
     <li key={plusMenu.id} className="top-level-entry-container">
       <button
         id={`${plusMenu.id}-button`}
@@ -62,12 +74,6 @@ export const PlusMenu = ({ visibleSubMenuId, toggleMenu }) => {
       </Link>
 
       <Submenu menuEntry={plusMenu} defaultHidden={!isOpen} />
-    </li>
-  ) : (
-    <li>
-      <a href={`/${locale}/plus`} className="top-level-entry">
-        MDN Plus
-      </a>
     </li>
   );
 };
