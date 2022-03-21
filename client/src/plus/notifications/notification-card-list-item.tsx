7d82b58f4c7c665c4f575bc3301911e99386619d
Claas Augner <caugner@mozilla.com>
2022-03-21 20:38:28 +0100

    fix(offline): hide collection/notification actions when offline

--- client/src/plus/notifications/notification-card-list-item.tsx
+++ client/src/plus/notifications/notification-card-list-item.tsx
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 import React from "react";
+import { useOnlineStatus } from "../../hooks";
 import { Button } from "../../ui/atoms/button";
 import { DropdownMenu, DropdownMenuWrapper } from "../../ui/molecules/dropdown";
 import { Checkbox } from "../../ui/molecules/notifications-watch-menu/atoms/checkbox";
@@ -11,25 +12,30 @@ export default function NotificationCardListItem({
   handleDelete,
 }) {
   const [show, setShow] = React.useState(false);
+  const { isOnline } = useOnlineStatus();
 
   return (
     <li
       className={`notification-card ${!item.read ? "unread" : ""}`}
       key={item.id}
     >
-      <Checkbox
-        name="selected"
-        checked={item.checked}
-        onChange={(e) => toggleSelected(item, e.target.value)}
-      />
-      <Button
-        type="action"
-        extraClasses="notification-card-star"
-        icon={item.starred ? "star-filled" : "star"}
-        onClickHandler={() => toggleStarred(item)}
-      >
-        <span className="visually-hidden">Toggle Starring</span>
-      </Button>
+      {isOnline && (
+        <>
+          <Checkbox
+            name="selected"
+            checked={item.checked}
+            onChange={(e) => toggleSelected(item, e.target.value)}
+          />
+          <Button
+            type="action"
+            extraClasses="notification-card-star"
+            icon={item.starred ? "star-filled" : "star"}
+            onClickHandler={() => toggleStarred(item)}
+          >
+            <span className="visually-hidden">Toggle Starring</span>
+          </Button>
+        </>
+      )}
 
       <a href={item.url} className="notification-card-description">
         <h2 className="notification-card-title">{item.title}</h2>
@@ -43,31 +49,33 @@ export default function NotificationCardListItem({
         {dayjs(item.created).fromNow().toString()}
       </time>
 
-      <DropdownMenuWrapper
-        className="dropdown is-flush-right"
-        isOpen={show}
-        setIsOpen={setShow}
-      >
-        <Button
-          type="action"
-          icon="ellipses"
-          ariaControls="watch-card-dropdown"
-          ariaHasPopup={"menu"}
-          ariaExpanded={show || undefined}
-          onClickHandler={() => {
-            setShow(!show);
-          }}
-        />
-        <DropdownMenu>
-          <ul className="dropdown-list" id="watch-card-dropdown">
-            <li className="dropdown-item">
-              <Button type="action" onClickHandler={() => handleDelete(item)}>
-                Delete
-              </Button>
-            </li>
-          </ul>
-        </DropdownMenu>
-      </DropdownMenuWrapper>
+      {isOnline && (
+        <DropdownMenuWrapper
+          className="dropdown is-flush-right"
+          isOpen={show}
+          setIsOpen={setShow}
+        >
+          <Button
+            type="action"
+            icon="ellipses"
+            ariaControls="watch-card-dropdown"
+            ariaHasPopup={"menu"}
+            ariaExpanded={show || undefined}
+            onClickHandler={() => {
+              setShow(!show);
+            }}
+          />
+          <DropdownMenu>
+            <ul className="dropdown-list" id="watch-card-dropdown">
+              <li className="dropdown-item">
+                <Button type="action" onClickHandler={() => handleDelete(item)}>
+                  Delete
+                </Button>
+              </li>
+            </ul>
+          </DropdownMenu>
+        </DropdownMenuWrapper>
+      )}
     </li>
   );
 }
