cae6444531924389a4c894ab7af7e32cdd0aa3ae
Claas Augner <caugner@mozilla.com>
2022-03-21 21:13:40 +0100

    refactor(notifications): convert grid to flex
    Allows us to hide elements, without breaking the grid-template.

--- client/src/plus/notifications/notification-card-list-item.tsx
+++ client/src/plus/notifications/notification-card-list-item.tsx
@@ -19,63 +19,70 @@ export default function NotificationCardListItem({
       className={`notification-card ${!item.read ? "unread" : ""}`}
       key={item.id}
     >
-      {isOnline && (
-        <>
-          <Checkbox
-            name="selected"
-            checked={item.checked}
-            onChange={(e) => toggleSelected(item, e.target.value)}
-          />
-          <Button
-            type="action"
-            extraClasses="notification-card-star"
-            icon={item.starred ? "star-filled" : "star"}
-            onClickHandler={() => toggleStarred(item)}
-          >
-            <span className="visually-hidden">Toggle Starring</span>
-          </Button>
-        </>
-      )}
+      <div>
+        {isOnline && (
+          <>
+            <Checkbox
+              name="selected"
+              checked={item.checked}
+              onChange={(e) => toggleSelected(item, e.target.value)}
+            />
+            <Button
+              type="action"
+              extraClasses="notification-card-star"
+              icon={item.starred ? "star-filled" : "star"}
+              onClickHandler={() => toggleStarred(item)}
+            >
+              <span className="visually-hidden">Toggle Starring</span>
+            </Button>
+          </>
+        )}
 
-      <a href={item.url} className="notification-card-description">
-        <h2 className="notification-card-title">{item.title}</h2>
-        <p className="notification-card-text">{item.text}</p>
-      </a>
+        <a href={item.url} className="notification-card-description">
+          <h2 className="notification-card-title">{item.title}</h2>
+          <p className="notification-card-text">{item.text}</p>
+        </a>
+      </div>
 
-      <time
-        className="notification-card-created"
-        dateTime={dayjs(item.created).toISOString()}
-      >
-        {dayjs(item.created).fromNow().toString()}
-      </time>
-
-      {isOnline && (
-        <DropdownMenuWrapper
-          className="dropdown is-flush-right"
-          isOpen={show}
-          setIsOpen={setShow}
+      <div>
+        <time
+          className="notification-card-created desktop-only"
+          dateTime={dayjs(item.created).toISOString()}
         >
-          <Button
-            type="action"
-            icon="ellipses"
-            ariaControls="watch-card-dropdown"
-            ariaHasPopup={"menu"}
-            ariaExpanded={show || undefined}
-            onClickHandler={() => {
-              setShow(!show);
-            }}
-          />
-          <DropdownMenu>
-            <ul className="dropdown-list" id="watch-card-dropdown">
-              <li className="dropdown-item">
-                <Button type="action" onClickHandler={() => handleDelete(item)}>
-                  Delete
-                </Button>
-              </li>
-            </ul>
-          </DropdownMenu>
-        </DropdownMenuWrapper>
-      )}
+          {dayjs(item.created).fromNow().toString()}
+        </time>
+
+        {isOnline && (
+          <DropdownMenuWrapper
+            className="dropdown is-flush-right"
+            isOpen={show}
+            setIsOpen={setShow}
+          >
+            <Button
+              type="action"
+              icon="ellipses"
+              ariaControls="watch-card-dropdown"
+              ariaHasPopup={"menu"}
+              ariaExpanded={show || undefined}
+              onClickHandler={() => {
+                setShow(!show);
+              }}
+            />
+            <DropdownMenu>
+              <ul className="dropdown-list" id="watch-card-dropdown">
+                <li className="dropdown-item">
+                  <Button
+                    type="action"
+                    onClickHandler={() => handleDelete(item)}
+                  >
+                    Delete
+                  </Button>
+                </li>
+              </ul>
+            </DropdownMenu>
+          </DropdownMenuWrapper>
+        )}
+      </div>
     </li>
   );
 }
