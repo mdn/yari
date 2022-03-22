2a9c7516997480d7c29070b0a07ff4cb752a6fba
Claas Augner <caugner@mozilla.com>
2022-03-22 21:13:01 +0100

    fix(offer-overview): override docs locale for now
    Currently, it is not possible to access Plus docs with a different
    locale, as it will fail with a "Page not found" error.
    
    To avoid this, we explicitly link to the English versions for now.

--- client/src/plus/offer-overview/offer-overview-feature/index.tsx
+++ client/src/plus/offer-overview/offer-overview-feature/index.tsx
@@ -40,7 +40,7 @@ export default function OfferOverviewFeatures() {
             get customizable notifications when documentation changes, CSS
             features launch, and APIs ship.
           </p>
-          <Button href="./docs/features/notifications">Learn more →</Button>
+          <Button href="/en-US/docs/features/notifications">Learn more →</Button>
         </section>
       </OfferOverviewFeature>
       <OfferOverviewFeature
@@ -59,7 +59,7 @@ export default function OfferOverviewFeatures() {
             your inner curator and collect your favorite articles in one place
             for convenient consultation.
           </p>
-          <Button href="./docs/features/collections">Learn more →</Button>
+          <Button href="/en-US/docs/features/collections">Learn more →</Button>
         </section>
       </OfferOverviewFeature>
       <OfferOverviewFeature
@@ -75,7 +75,7 @@ export default function OfferOverviewFeatures() {
             inaccessible pages or cluttered tabs. With MDN Plus, have the fully
             navigable resources of MDN at your disposal even when offline.
           </p>
-          <Button href="./docs/features/offline">Learn more →</Button>
+          <Button href="/en-US/docs/features/offline">Learn more →</Button>
         </section>
       </OfferOverviewFeature>
     </section>
