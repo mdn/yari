f54002c7ee8e0dc748d3580325e3821d1f70496f
Guy Sleigh <gsleigh@mozilla.com>
2022-03-22 14:32:26 +0100

    Revert "allow list for mdn plus"

--- client/src/plus/offer-overview/index.tsx
+++ client/src/plus/offer-overview/index.tsx
@@ -1,18 +1,14 @@
-import { useUserData } from "../../user-context";
-import { isPlusAvailable } from "../../utils";
 import "./index.scss";
 import OfferHero from "./offer-hero";
 import OfferOverviewFeatures from "./offer-overview-feature";
 import OfferOverviewSubscribe from "./offer-overview-subscribe";
 
 function OfferOverview() {
-  const userData = useUserData();
-  const plusAvailable = isPlusAvailable(userData);
   return (
     <div className="offer-overview">
-      <OfferHero plusAvailable={plusAvailable} />
+      <OfferHero />
       <OfferOverviewFeatures />
-      {plusAvailable && <OfferOverviewSubscribe />}
+      <OfferOverviewSubscribe />
     </div>
   );
 }
--- client/src/plus/offer-overview/offer-hero/index.tsx
+++ client/src/plus/offer-overview/offer-hero/index.tsx
@@ -2,7 +2,7 @@ import { CRUD_MODE } from "../../../constants";
 import Mandala from "../../../ui/molecules/mandala";
 import "./index.scss";
 
-function OfferHero({ plusAvailable }: { plusAvailable: boolean }) {
+function OfferHero() {
   const animate = !CRUD_MODE;
   return (
     <div className="dark offer-hero">
@@ -11,25 +11,17 @@ function OfferHero({ plusAvailable }: { plusAvailable: boolean }) {
           <h1>
             More MDN. <u>Your</u> MDN.
           </h1>
-          {(plusAvailable && (
-            <>
-              <h2>
-                Support MDN <u>and</u> make it your own. For just $5 a month.
-              </h2>
-              <div className="button-wrapper">
-                <a href="#subscribe" className="button-primary">
-                  Get started
-                </a>
-                <a href="#features" className="button-secondary">
-                  What's included
-                </a>
-              </div>
-            </>
-          )) || (
-            <h2>
-              Coming to <u>your</u> region soon.
-            </h2>
-          )}
+          <h2>
+            Support MDN <u>and</u> make it your own. For just $5 a month.
+          </h2>
+          <div className="button-wrapper">
+            <a href="#subscribe" className="button-primary">
+              Get started
+            </a>
+            <a href="#features" className="button-secondary">
+              What's included
+            </a>
+          </div>
         </div>
       </header>
       <div className="mandala-wrapper">
