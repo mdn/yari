ef7546e346914051f67d399e1bf14a7415811bef
Claas Augner <caugner@mozilla.com>
2022-03-23 14:09:18 +0100

    fix(offer-overview): introduce aria-labelled sections

--- client/src/plus/offer-overview/offer-overview-feature/index.tsx
+++ client/src/plus/offer-overview/offer-overview-feature/index.tsx
@@ -28,49 +28,55 @@ export default function OfferOverviewFeatures() {
         img="/assets/notifications_light.png"
         imgAlt=""
       >
-        <h2>Notifications</h2>
-        <h3>
-          Development in real time:
-          <br />
-          Get custom alerts
-        </h3>
-        <p>
-          The Web doesn't have a changelog, but MDN can help. Follow pages and
-          get customizable notifications when documentation changes, CSS
-          features launch, and APIs ship.
-        </p>
-        <Button href="./docs/features/notifications">Learn more →</Button>
+        <section aria-labelledby="notifications-section-title">
+          <h2 id="notifications-section-title">Notifications</h2>
+          <h3>
+            Development in real time:
+            <br />
+            Get custom alerts
+          </h3>
+          <p>
+            The Web doesn't have a changelog, but MDN can help. Follow pages and
+            get customizable notifications when documentation changes, CSS
+            features launch, and APIs ship.
+          </p>
+          <Button href="./docs/features/notifications">Learn more →</Button>
+        </section>
       </OfferOverviewFeature>
       <OfferOverviewFeature
         id="collections"
         img="/assets/notifications_light.png"
         imgAlt=""
       >
-        <h2>Collections</h2>
-        <h3>
-          Build your perfect library. <br />
-          Or let us build it for you.
-        </h3>
-        <p>
-          No more haphazard hunting through the vast virtual library: unleash
-          your inner curator and collect your favorite articles in one place for
-          convenient consultation.
-        </p>
-        <Button href="./docs/features/collections">Learn more →</Button>
+        <section aria-labelledby="collections-section-title">
+          <h2 id="collections-section-title">Collections</h2>
+          <h3>
+            Build your perfect library. <br />
+            Or let us build it for you.
+          </h3>
+          <p>
+            No more haphazard hunting through the vast virtual library: unleash
+            your inner curator and collect your favorite articles in one place
+            for convenient consultation.
+          </p>
+          <Button href="./docs/features/collections">Learn more →</Button>
+        </section>
       </OfferOverviewFeature>
       <OfferOverviewFeature
         id="offline"
         img="/assets/notifications_light.png"
         imgAlt=""
       >
-        <h2>MDN Offline</h2>
-        <h3>MDN's entire library at your fingertips: offline</h3>
-        <p>
-          Taking your projects beyond the nearest wifi signal? Say goodbye to
-          inaccessible pages or cluttered tabs. With MDN Plus, have the fully
-          navigable resources of MDN at your disposal even when offline.
-        </p>
-        <Button href="./docs/features/offline">Learn more →</Button>
+        <section aria-labelledby="offline-section-title">
+          <h2 id="offline-section-title">MDN Offline</h2>
+          <h3>MDN's entire library at your fingertips: offline</h3>
+          <p>
+            Taking your projects beyond the nearest wifi signal? Say goodbye to
+            inaccessible pages or cluttered tabs. With MDN Plus, have the fully
+            navigable resources of MDN at your disposal even when offline.
+          </p>
+          <Button href="./docs/features/offline">Learn more →</Button>
+        </section>
       </OfferOverviewFeature>
     </section>
   );
