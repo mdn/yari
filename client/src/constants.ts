c44f2f67ca4a1c828a90099cad451f1368a063ec
Guy Sleigh <gsleigh@mozilla.com>
2022-03-24 13:21:27 +0100

    chore(plus): enable on prod (#5779)

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -39,17 +39,12 @@ export const VALID_LOCALES = new Set([
 export const MDN_PLUS_TITLE = "MDN Plus";
 
 export const PLUS_IS_ENABLED = Boolean(
-  JSON.parse(
-    (typeof window !== "undefined"
-      ? window.localStorage?.getItem("REACT_APP_ENABLE_PLUS")
-      : undefined) ||
-      process.env.REACT_APP_ENABLE_PLUS ||
-      "false"
-  )
+  process.env.REACT_APP_ENABLE_PLUS || "false"
 );
 export const PLUS_IS_AVAILABLE_OVERRIDE = JSON.parse(
   process.env.REACT_APP_PLUS_IS_AVAILABLE_OVERRIDE || "null"
 );
+
 export const MDN_PLUS_SUBSCRIBE_5M_URL = `${process.env.REACT_APP_MDN_PLUS_SUBSCRIBE_URL}?plan=${process.env.REACT_APP_MDN_PLUS_5M_PLAN}`;
 export const MDN_PLUS_SUBSCRIBE_5Y_URL = `${process.env.REACT_APP_MDN_PLUS_SUBSCRIBE_URL}?plan=${process.env.REACT_APP_MDN_PLUS_5Y_PLAN}`;
 export const MDN_PLUS_SUBSCRIBE_10M_URL = `${process.env.REACT_APP_MDN_PLUS_SUBSCRIBE_URL}?plan=${process.env.REACT_APP_MDN_PLUS_10M_PLAN}`;
