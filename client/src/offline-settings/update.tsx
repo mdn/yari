f92b22e1c6af70f4b3c38755c7ad3aef937c4318
Claas Augner <caugner@mozilla.com>
2022-03-24 10:22:47 +0100

    chore(offline-settings): prompt to download on first time

--- client/src/offline-settings/update.tsx
+++ client/src/offline-settings/update.tsx
@@ -9,13 +9,11 @@ export default function UpdateButton({
   update: () => void;
   disabled?: boolean;
 }) {
-  const current = `Last updated: ${
-    updateStatus?.currentDate
-      ? Intl.DateTimeFormat([], { dateStyle: "medium" }).format(
-          Date.parse(updateStatus.currentDate)
-        )
-      : "never"
-  }`;
+  const current =
+    updateStatus?.currentDate &&
+    `Last updated: ${Intl.DateTimeFormat([], { dateStyle: "medium" }).format(
+      Date.parse(updateStatus.currentDate)
+    )}`;
   let button: JSX.Element | null = null;
   let info: string | undefined;
   let progress = (updateStatus?.progress || 0) * 100;
@@ -27,20 +25,39 @@ export default function UpdateButton({
     button = <button disabled>Up to date</button>;
   }
   if (updateStatus?.state === STATE.updateAvailable) {
-    info = "Update available";
-    button = (
-      <button className="button" onClick={update} disabled={disabled}>
-        {" "}
-        Update now
-      </button>
-    );
+    if (updateStatus?.currentDate) {
+      info = "Update available";
+      button = (
+        <button className="button" onClick={update} disabled={disabled}>
+          {" "}
+          Update now
+        </button>
+      );
+    } else {
+      info =
+        "Start using MDN Offline by downloading the latest version of MDN Web Docs";
+      button = (
+        <button className="button" onClick={update} disabled={disabled}>
+          {" "}
+          Download
+        </button>
+      );
+    }
   }
   if (updateStatus?.state === STATE.downloading) {
-    info = "Update in progress…";
+    if (updateStatus?.currentDate) {
+      info = "Update in progress…";
+    } else {
+      info = "Download in progress…";
+    }
     button = <button disabled={disabled}>Downloading…</button>;
   }
   if (updateStatus?.state === STATE.unpacking) {
-    info = "Update in progress…";
+    if (updateStatus?.currentDate) {
+      info = "Update in progress…";
+    } else {
+      info = "Download in progress…";
+    }
     button = (
       <button disabled={disabled}>
         Unpacking…{" "}
@@ -52,7 +69,11 @@ export default function UpdateButton({
     );
   }
   if (updateStatus?.state === STATE.cleaning) {
-    info = "Update in progress…";
+    if (updateStatus?.currentDate) {
+      info = "Update in progress…";
+    } else {
+      info = "Download in progress…";
+    }
     button = <button disabled>Cleaning…</button>;
   }
   if (updateStatus?.state === STATE.clearing) {
