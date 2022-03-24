10f4ce63e8f9f705645cdcf7dcfaa05bb6368186
Claas Augner <caugner@mozilla.com>
2022-03-24 13:18:16 +0100

    Make MDN Plus Docs look more like regular MDN articles (#5761)

--- client/src/plus/plus-docs/index.tsx
+++ client/src/plus/plus-docs/index.tsx
@@ -1,7 +1,74 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams, useLocation } from "react-router-dom";
 import { MDN_PLUS_TITLE } from "../../constants";
 import StaticPage from "../../homepage/static-page";
-import { Button } from "../../ui/atoms/button";
+import "./index.scss";
+
+function PlusDocsNav() {
+  return (
+    <RelatedTopics
+      heading="MDN Plus"
+      items={[
+        {
+          slug: "plus/docs/features/collections",
+          title: "Collections",
+        },
+        {
+          slug: "plus/docs/features/notifications",
+          title: "Notifications",
+        },
+        {
+          slug: "plus/docs/features/offline",
+          title: "MDN Offline",
+        },
+        {
+          slug: "plus/docs/faq",
+          title: "Frequently asked questions",
+        },
+      ]}
+    />
+  );
+}
+
+function RelatedTopics({
+  heading = "Related Topics",
+  items,
+}: {
+  heading: string;
+  items: { slug: string; title: string }[];
+}) {
+  const { locale } = useParams();
+  const { pathname: locationPathname } = useLocation();
+
+  return (
+    <aside className="document-toc-container">
+      <section className="document-toc">
+        <header>
+          <h2 className="document-toc-heading">{heading}</h2>
+        </header>
+        <ul className="document-toc-list">
+          {items.map(({ slug, title }) => {
+            const itemPathname = `/${locale}/${slug}`;
+
+            return (
+              <li className="document-toc-item">
+                <Link
+                  className="document-toc-link"
+                  aria-current={
+                    itemPathname.toLowerCase() ===
+                    locationPathname.toLowerCase()
+                  }
+                  to={itemPathname}
+                >
+                  {title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </aside>
+  );
+}
 
 function PlusDocs({ ...props }) {
   const { locale, "*": slug } = useParams();
@@ -9,12 +76,12 @@ function PlusDocs({ ...props }) {
   return (
     <StaticPage
       {...{
+        extraClasses: "plus-docs",
         locale,
         slug: `plus/docs/${slug}`,
         title: MDN_PLUS_TITLE,
-        sidebarHeader: (
-          <Button href={`/${locale}/plus`}>← Back to Overview</Button>
-        ),
+        parents: [{ uri: `/${locale}/plus`, title: MDN_PLUS_TITLE }],
+        sidebarHeader: <PlusDocsNav />,
         initialData: props.hyData ? props : undefined,
       }}
     />
