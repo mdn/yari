dfe941ed44b93eed954baddad1898c427c209d77
Claas Augner <caugner@mozilla.com>
2022-03-25 14:51:24 +0100

    fix(plus/offer-overview): add useScrollEffect()
    This should make sure, that hash navigation works, e.g.
    when deep-linking to #features or #subscribe.

--- client/src/plus/offer-overview/index.tsx
+++ client/src/plus/offer-overview/index.tsx
@@ -1,3 +1,4 @@
+import { useScrollEffect } from "../../hooks";
 import { useUserData } from "../../user-context";
 import { isPlusAvailable } from "../../utils";
 import OfferHero from "./offer-hero";
@@ -7,6 +8,9 @@ import OfferOverviewSubscribe from "./offer-overview-subscribe";
 function OfferOverview() {
   const userData = useUserData();
   const plusAvailable = isPlusAvailable(userData);
+
+  useScrollEffect([]);
+
   return (
     <div className="offer-overview">
       <OfferHero plusAvailable={plusAvailable} />
