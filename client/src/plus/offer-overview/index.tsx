cf8ce8b075fe460881c1877d446f56828ca1c2e1
Claas Augner <caugner@mozilla.com>
2022-03-25 19:17:52 +0100

    Revert "fix(plus/offer-overview): add useScrollEffect()"
    This reverts commit dfe941ed44b93eed954baddad1898c427c209d77.

--- client/src/plus/offer-overview/index.tsx
+++ client/src/plus/offer-overview/index.tsx
@@ -1,4 +1,3 @@
-import { useScrollEffect } from "../../hooks";
 import { useUserData } from "../../user-context";
 import { isPlusAvailable } from "../../utils";
 import OfferHero from "./offer-hero";
@@ -8,9 +7,6 @@ import OfferOverviewSubscribe from "./offer-overview-subscribe";
 function OfferOverview() {
   const userData = useUserData();
   const plusAvailable = isPlusAvailable(userData);
-
-  useScrollEffect([]);
-
   return (
     <div className="offer-overview">
       <OfferHero plusAvailable={plusAvailable} />
