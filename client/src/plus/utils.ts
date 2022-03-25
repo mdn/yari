465a6ae2b1a68b2cbc180102814a1daf4384e355
Claas Augner <caugner@mozilla.com>
2022-03-25 17:00:59 +0100

    chore(plus): explain usePlusUrl() hook

--- client/src/plus/utils.ts
+++ client/src/plus/utils.ts
@@ -1,7 +1,11 @@
 import { useLocation } from "react-router-dom";
 import { useLocale } from "../hooks";
 
-export function usePlusUrl() {
+/**
+ * Returns the URL of the Plus product page, or, if we're
+ * already there, the hash of the "Choose a plan" section.
+ */
+export function usePlusUrl(): string {
   const { pathname } = useLocation();
   const locale = useLocale();
 
