d9d5b9ca662665d949e428be31305701d48ffbb1
Schalk Neethling <sneethling@mozilla.com>
2022-03-29 12:21:51 +0200

    prettier

--- client/src/document/molecules/note-banner/index.tsx
+++ client/src/document/molecules/note-banner/index.tsx
@@ -10,9 +10,7 @@ export function NoteBanner({
   type: "neutral" | "warning";
 }) {
   return (
-    <NoteCard
-      extraClasses={`localized-content-note inline ${type || ""}`}
-    >
+    <NoteCard extraClasses={`localized-content-note inline ${type || ""}`}>
       <p>
         <a href={url} className={!url.startsWith("/") ? "external" : undefined}>
           {linkText}
