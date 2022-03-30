5a772bd482b0cb12a85f2abe230d6c682bd6a66a
Claas Augner <caugner@mozilla.com>
2022-03-30 17:48:42 +0200

    fix(content/notify): implement filter criteria

--- content/notify.ts
+++ content/notify.ts
@@ -13,6 +13,7 @@ require("dotenv").config();
 const { CONTENT_ROOT } = process.env;
 
 const DATE_RANGE_REGEXP = /^(\d{4}-\d{2}-\d{2})+\.\.(\d{4}-\d{2}-\d{2}|\*)$/;
+const FORCE_NOTIFICATION_LABEL = "force-notifications";
 
 function main() {
   const merged = process.argv[2];
@@ -33,6 +34,14 @@ interface PullRequest {
   title: string;
   url: string;
   files: ChangedFile[];
+  labels: Label[];
+}
+
+interface Label {
+  id: string;
+  name: string;
+  description: string;
+  color: string;
 }
 
 interface ChangedFile {
@@ -44,7 +53,7 @@ interface ChangedFile {
 function processMergedPRs(merged: string) {
   const search = ["is:merged", `merged:${merged}`];
 
-  const fields = ["id", "number", "url", "title", "files"];
+  const fields = ["id", "number", "url", "labels", "title", "files"];
 
   const prs = JSON.parse(
     child_process.execSync(
@@ -60,21 +69,32 @@ function processMergedPRs(merged: string) {
 }
 
 function processPR(pr: PullRequest) {
-  for (const file of pr.files) {
-    let notify = false;
-    if (file.path.endsWith("index.md")) {
-      const net = netDiffSize(file);
+  // A PR labeled force-notifications must trigger a notification
+  // issued for every slug modified by the PR.
+  const force = pr.labels.some(({ name }) => name === FORCE_NOTIFICATION_LABEL);
 
-      if (!(net < 5)) {
-        notify = true;
-      }
+  const markdownFiles = pr.files.filter((file) =>
+    file.path.endsWith("index.md")
+  );
 
-      if (notify === true) {
-        const slug = getSlug(file.path);
-        console.log(`${pr.number}\t${slug}`);
-        // TODO: post to notifications API endpoint
-      }
+  if (!force && markdownFiles.length > 5) {
+    // If a merged PR modifies six or more Markdown files,
+    // then no notifications should be issued for that PR
+    // (to avoid issuing notifications for bulk edits).
+    return;
+  }
+
+  for (const file of markdownFiles) {
+    if (!force && netDiffSize(file) < 5) {
+      // If the net diff size for a Markdown file is less than 5,
+      // then no notifications should be issued for the affected
+      // slug (to avoid issuing notifications for minor edits).
+      continue;
     }
+
+    const slug = getSlug(file.path);
+    console.log(`${pr.number}\t${slug}`);
+    // TODO: post to notifications API endpoint
   }
 }
 
