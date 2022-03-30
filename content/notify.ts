090ba162a389a062813dc1ec6c5aca481f64c13e
Claas Augner <caugner@mozilla.com>
2022-03-30 17:36:31 +0200

    fix(content/notify): list merged PRs + use frontmatter

--- content/notify.ts
+++ content/notify.ts
@@ -5,17 +5,33 @@
 // Known issues: it doesn't ignore completely new slugs
 
 const child_process = require("child_process");
+const frontmatter = require("front-matter");
+const fs = require("fs");
 
 require("dotenv").config();
 
 const { CONTENT_ROOT } = process.env;
 
+const DATE_RANGE_REGEXP = /^(\d{4}-\d{2}-\d{2})+\.\.(\d{4}-\d{2}-\d{2}|\*)$/;
+
 function main() {
-  const id = Number(process.argv[2]);
-  processPR(id);
+  const merged = process.argv[2];
+
+  if (!DATE_RANGE_REGEXP.test(merged)) {
+    console.error(
+      "Please specify merged date as a range: YYYY-MM-DD..YYYY-MM-DD | YYYY-MM-DD..*"
+    );
+    process.exit(1);
+  }
+
+  processMergedPRs(merged);
 }
 
 interface PullRequest {
+  id: string;
+  number: number;
+  title: string;
+  url: string;
   files: ChangedFile[];
 }
 
@@ -25,25 +41,37 @@ interface ChangedFile {
   deletions: number;
 }
 
-function processPR(id: number) {
-  const ghJSON = JSON.parse(
-    child_process.execSync(`gh pr view ${id} --json files`, {
-      cwd: CONTENT_ROOT,
-      encoding: "utf-8",
-    })
-  ) as PullRequest;
+function processMergedPRs(merged: string) {
+  const search = ["is:merged", `merged:${merged}`];
 
-  for (const file of ghJSON.files) {
+  const fields = ["id", "number", "url", "title", "files"];
+
+  const prs = JSON.parse(
+    child_process.execSync(
+      `gh pr list --search '${search.join(" ")}' --json ${fields.join(",")}`,
+      {
+        cwd: CONTENT_ROOT,
+        encoding: "utf-8",
+      }
+    )
+  ) as PullRequest[];
+
+  prs.forEach((pr) => processPR(pr));
+}
+
+function processPR(pr: PullRequest) {
+  for (const file of pr.files) {
     let notify = false;
     if (file.path.endsWith("index.md")) {
       const net = netDiffSize(file);
+
       if (!(net < 5)) {
         notify = true;
       }
 
       if (notify === true) {
         const slug = getSlug(file.path);
-        console.log(`${id}\t${slug}`);
+        console.log(`${pr.number}\t${slug}`);
         // TODO: post to notifications API endpoint
       }
     }
@@ -55,11 +83,13 @@ function netDiffSize(file: ChangedFile) {
 }
 
 function getSlug(path: string) {
-  const cmd = `yq --front-matter=extract '.slug' ${path}`;
-  return child_process.execSync(cmd, {
-    cwd: CONTENT_ROOT,
-    encoding: "utf-8",
-  });
+  path = path.substring("files/".length);
+
+  const markdown = fs.readFileSync(`${CONTENT_ROOT}/${path}`, "utf8");
+
+  const frontMatter = frontmatter(markdown);
+
+  return frontMatter.attributes.slug;
 }
 
 main();
