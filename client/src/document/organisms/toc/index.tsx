51928656b8f5a6bd17ffe5482dc85810b2415696
Claas Augner <caugner@mozilla.com>
2022-04-05 12:38:42 +0200

    refactor(toc): extract useFirstVisibleElement() hook

--- client/src/document/organisms/toc/index.tsx
+++ client/src/document/organisms/toc/index.tsx
@@ -2,74 +2,40 @@ import React, { useState } from "react";
 
 import "./index.scss";
 import { Toc } from "../../types";
+import { useFirstVisibleElement } from "../../hooks";
 
 export function TOC({ toc }: { toc: Toc[] }) {
   const [currentViewedTocItem, setCurrentViewedTocItem] = useState("");
 
-  React.useEffect(() => {
-    // Observe headings and siblings.
-    const main = document.querySelector("main") ?? document;
-    const targets = main.querySelectorAll("h1, h1 ~ *, h2, h2 ~ *, h3, h3 ~ *");
-
-    // Tracks if observed elements are visible (intersection with viewport).
-    const intersectionMap = new Map<Element, boolean>();
-
-    // All ids referenced by the TOC.
-    const tocIds = toc.map(({ id }) => id);
-
-    // Maps every observed element to an id referenced by the TOC.
-    const idMap = new Map<Element, string>();
-
-    /**
-     * Updates the visibility of observed elements.
-     */
-    function manageIntersectionMap(entries: IntersectionObserverEntry[]) {
-      for (const entry of entries) {
-        intersectionMap.set(entry.target, entry.isIntersecting);
-      }
-    }
-
-    /**
-     * Sets the first visible item as the currently viewed TOC item.
-     */
-    function updateViewedTocItem() {
-      const visibleIds = Array.from(intersectionMap.entries())
-        .filter(([, value]) => value)
-        .map(([key]) => key)
-        .map((target) => idMap.get(target) ?? "");
-
-      if (visibleIds.length === 0) {
-        return;
-      }
-
-      setCurrentViewedTocItem(visibleIds[0]);
-    }
-
-    const intersectionObserver = new IntersectionObserver(
-      (entries: IntersectionObserverEntry[]) => {
-        manageIntersectionMap(entries);
-        updateViewedTocItem();
-      },
-      {
-        threshold: [0.0, 1.0],
-      }
+  const observedElements = React.useCallback(() => {
+    const mainElement = document.querySelector("main") ?? document;
+    const elements = mainElement.querySelectorAll(
+      "h1, h1 ~ *, h2, h2 ~ *, h3, h3 ~ *"
     );
+    return Array.from(elements);
+  }, []);
 
-    Array.from(targets).reduce((currentId, target) => {
-      const targetId = target.id.toLowerCase();
-      if (targetId && tocIds.includes(targetId)) {
-        currentId = targetId;
+  const referencedIds = toc.map(({ id }) => id);
+  const idByObservedElement = React.useRef(new Map<Element, string>());
+
+  React.useEffect(() => {
+    observedElements().reduce((currentId, observedElement) => {
+      const observedId = observedElement.id.toLowerCase();
+      if (observedId && referencedIds.includes(observedId)) {
+        currentId = observedId;
       }
-      idMap.set(target, currentId);
-
-      intersectionMap.set(target, false);
-      intersectionObserver.observe(target);
+      idByObservedElement.current.set(observedElement, currentId);
 
       return currentId;
     }, "");
+  }, [observedElements, referencedIds]);
 
-    return () => intersectionObserver.disconnect();
-  }, [toc]);
+  useFirstVisibleElement(observedElements, (element: Element | null) => {
+    const id = element ? idByObservedElement.current.get(element) ?? "" : "";
+    if (id !== currentViewedTocItem) {
+      setCurrentViewedTocItem(id);
+    }
+  });
 
   return (
     <aside className="document-toc-container">
