96a37db495966e0f0f0735318e369d60b73022e2
Claas Augner <caugner@mozilla.com>
2022-04-15 23:02:07 +0200

    fix(ui): wrap {404,search} in article.main-page-content

--- client/src/site-search/index.tsx
+++ client/src/site-search/index.tsx
@@ -51,26 +51,28 @@ export function SiteSearch() {
   return (
     <div className="main-wrapper site-search main-page-content">
       <PageContentContainer>
-        {query ? (
-          <h1>
-            Search results for: <span className="query-string">{query}</span>{" "}
-            {page && page !== "1" && `(page ${page})`}
-          </h1>
-        ) : (
-          <h1>No query, no results.</h1>
-        )}
+        <article className="main-page-content">
+          {query ? (
+            <h1>
+              Search results for: <span className="query-string">{query}</span>{" "}
+              {page && page !== "1" && `(page ${page})`}
+            </h1>
+          ) : (
+            <h1>No query, no results.</h1>
+          )}
 
-        {!isServer && (
-          <React.Suspense fallback={<Loading />}>
-            <SiteSearchForm />
-          </React.Suspense>
-        )}
+          {!isServer && (
+            <React.Suspense fallback={<Loading />}>
+              <SiteSearchForm />
+            </React.Suspense>
+          )}
 
-        {!isServer && query && (
-          <React.Suspense fallback={<Loading />}>
-            <SearchResults />
-          </React.Suspense>
-        )}
+          {!isServer && query && (
+            <React.Suspense fallback={<Loading />}>
+              <SearchResults />
+            </React.Suspense>
+          )}
+        </article>
       </PageContentContainer>
     </div>
   );
