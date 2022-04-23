bab815e8757efe79859439f733c53b7eea84e958
cw118 <carolyn94118@gmail.com>
2022-04-23 22:01:33 +0200

    fix prettier lint issues

--- client/src/document/molecules/localized-content-note/index.tsx
+++ client/src/document/molecules/localized-content-note/index.tsx
@@ -11,20 +11,17 @@ export function LocalizedContentNote({
     "en-US": {
       linkText:
         "This page was translated from English by the community. Learn more and join the MDN Web Docs community.",
-      url:
-        "/en-US/docs/MDN/Contribute/Localize#active_locales",
+      url: "/en-US/docs/MDN/Contribute/Localize#active_locales",
     },
     fr: {
       linkText:
         "Cette page a été traduite à partir de l'anglais par la communauté. Vous pouvez également contribuer en rejoignant la communauté francophone sur MDN Web Docs.",
-      url:
-        "/fr/docs/MDN/Contribute/Localize#langues_actives",
+      url: "/fr/docs/MDN/Contribute/Localize#langues_actives",
     },
     ja: {
       linkText:
         "このページはコミュニティーの尽力で英語から翻訳されました。MDN Web Docs コミュニティーについてもっと知り、仲間になるにはこちらから。",
-      url:
-        "/ja/docs/MDN/Contribute/Localize#active_locales",
+      url: "/ja/docs/MDN/Contribute/Localize#active_locales",
     },
     ko: {
       linkText:
@@ -37,8 +34,7 @@ export function LocalizedContentNote({
     "zh-CN": {
       linkText:
         "此页面由社区从英文翻译而来。了解更多并加入 MDN Web Docs 社区。",
-      url:
-        "/zh-CN/docs/MDN/Contribute/Localize#活跃语言",
+      url: "/zh-CN/docs/MDN/Contribute/Localize#活跃语言",
     },
   };
   const inactiveLocaleNoteContent = {
