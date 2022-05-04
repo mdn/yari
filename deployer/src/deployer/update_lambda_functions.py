648fdb6dc68088a88ad2f7d4f26b8a3adff2e2e7
Claas Augner <caugner@mozilla.com>
2022-05-04 18:07:01 +0200

    chore(deployer): deploy Lambda updates to CloudFront (#6129)
    * chore(deployer): deploy Lambda updates to CloudFront
    
    * chore(deployer): allow force-updating lambdas
    
    * docs(deployer): run flake8/black with poetry
    
    * build(prod): disable automatic Lambda deployment
    
    * fix(deployer): reset times of lambda files for reproducibility

--- deployer/src/deployer/update_lambda_functions.py
+++ deployer/src/deployer/update_lambda_functions.py
@@ -1,6 +1,7 @@
 import base64
 import hashlib
 import json
+import os
 import subprocess
 from pathlib import Path
 
@@ -42,6 +43,12 @@ def make_package(lambda_function_dir):
     # Remove any existing zip package(s).
     for zip_filename in lambda_function_dir.glob("*.zip"):
         zip_filename.unlink()
+
+    # Reset timestamp of files for reproducible zips.
+    times = (0, 0)
+    for file in lambda_function_dir.glob("**/*"):
+        os.utime(file, times)
+
     # Create a new zip package.
     subprocess_result = subprocess.run(
         "yarn run make-package",
@@ -62,7 +69,7 @@ def make_package(lambda_function_dir):
         )
 
 
-def update(lambda_function_dir, dry_run=False):
+def update(lambda_function_dir, dry_run=False, force=False):
     zip_filename = make_package(lambda_function_dir)
     function_name, region_name = get_aws_info(lambda_function_dir)
     client = boto3.client("lambda", region_name=region_name)
@@ -71,7 +78,7 @@ def update(lambda_function_dir, dry_run=False):
     function_hash = function_info["Configuration"]["CodeSha256"]
     zip_file_bytes = zip_filename.read_bytes()
     # If nothing has changed, don't do anything.
-    if get_sha256_hash(zip_file_bytes) != function_hash:
+    if force or get_sha256_hash(zip_file_bytes) != function_hash:
         if dry_run:
             # If this is a dry run, just return the existing function info.
             return function_info["Configuration"]
@@ -82,14 +89,17 @@ def update(lambda_function_dir, dry_run=False):
     return None
 
 
-def update_all(directory: Path, dry_run=False):
+def update_all(directory: Path, dry_run=False, force=False):
     log.info(f"Updating all lambda functions within: {directory}")
+
+    updated_functions = []
 
     for lambda_function_dir in get_lambda_function_dirs(directory):
         log.info(f"Found {lambda_function_dir.stem}: ", nl=False)
 
-        info = update(lambda_function_dir, dry_run=dry_run)
+        info = update(lambda_function_dir, dry_run=dry_run, force=force)
         if info:
+            updated_functions.append(info)
             if dry_run:
                 log.info(
                     f"would have published a new version of {info['FunctionName']}"
@@ -99,5 +109,114 @@ def update_all(directory: Path, dry_run=False):
                     f"published version {info['Version']} of {info['FunctionName']} "
                     f"as {info['FunctionArn']}",
                 )
+
         else:
             log.info(" no change (matches the latest version in AWS)")
+
+    return updated_functions
+
+
+def deploy(updated_functions: list, distribution_id, dry_run=False):
+    if len(updated_functions) == 0:
+        log.success(
+            "Skipping Lambda deployment to CloudFront, because no Lambdas were updated."
+        )
+        return
+
+    if not distribution_id:
+        log.warning(
+            "Skipping Lambda deployment to CloudFront, because no --distribution was specified."
+        )
+        return
+
+    # Pre-process updated functions.
+    function_arn_by_prefix = {}
+
+    for update in updated_functions:
+        arn = update["FunctionArn"]
+        version = update["Version"]
+
+        if arn.endswith(version):
+            # update_function_code() returns FunctionArn with version.
+            prefix = arn.replace(version, "")
+        else:
+            # get_function() returns FunctionArn without version.
+            prefix = arn + ":"
+            arn = prefix + version
+
+        function_arn_by_prefix[prefix] = arn
+
+    client = boto3.client("cloudfront")
+
+    # Get config.
+    response = client.get_distribution_config(Id=distribution_id)
+    etag = response["ETag"]
+    distribution_config = response["DistributionConfig"]
+    distribution_description = distribution_config["Comment"]
+
+    log.success(
+        f"Deploying lambda updates to CloudFront distribution {distribution_id} ({distribution_description})..."
+    )
+
+    behavior_items = [
+        distribution_config["DefaultCacheBehavior"]
+    ] + distribution_config["CacheBehaviors"]["Items"]
+
+    # Modify config.
+    changes = False
+    for behavior_item in behavior_items:
+        if "PathPattern" in behavior_item:
+            path_pattern = behavior_item["PathPattern"]
+        else:
+            path_pattern = "Default (*)"
+
+        if (
+            "LambdaFunctionAssociations" not in behavior_item
+            or "Items" not in behavior_item["LambdaFunctionAssociations"]
+        ):
+            continue
+
+        lambda_items = behavior_item["LambdaFunctionAssociations"]["Items"]
+
+        for lambda_item in lambda_items:
+            current_arn = lambda_item["LambdaFunctionARN"]
+
+            for prefix, arn in function_arn_by_prefix.items():
+                if not current_arn.startswith(prefix) or arn == current_arn:
+                    continue
+
+                if dry_run:
+                    log.info(
+                        f"Would update '{path_pattern}' ({lambda_item['EventType']}) from {current_arn} to {arn}"
+                    )
+                else:
+                    log.info(
+                        f"Updating '{path_pattern}' ({lambda_item['EventType']}) from {current_arn} to {arn}"
+                    )
+                    lambda_item["LambdaFunctionARN"] = arn
+
+                changes = True
+                break
+
+    if not changes:
+        log.info(
+            "CloudFront distribution was not changed, because it does not use any of the updated lambdas."
+        )
+        return
+
+    if dry_run:
+        # If this is a dry run, just return the config.
+        log.success(
+            f"Would have deployed lambda updates to CloudFront distribution {distribution_id} ({distribution_description})!"
+        )
+        return distribution_config
+
+    # Update config.
+    response = client.update_distribution(
+        Id=distribution_id, IfMatch=etag, DistributionConfig=distribution_config
+    )
+    log.success(
+        f"Deployed lambda updates to CloudFront distribution {distribution_id} ({distribution_description})!"
+    )
+
+    return response["Distribution"]["DistributionConfig"]
