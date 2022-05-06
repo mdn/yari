fafaf814fda2fbef2e73acb71e0c880e9c18679e
Claas Augner <caugner@mozilla.com>
2022-05-06 15:08:25 +0200

    fix(macros/Compat): move query from id to dataset (#6175)
    Previously, the macro would add `id="bcd:{QUERY}"`, which is an
    invalid id, and is being reported by html-validate 6.9.0 and later.
    
    Now, we add `data-query="{QUERY}"` instead of an invalid id.

--- build/bcd-urls.js
+++ build/bcd-urls.js
@@ -58,7 +58,7 @@ function normalizeBCDURLs(doc, options) {
     if (section.type !== "browser_compatibility") continue;
 
     // This happens if a query is "broken".
-    // E.g. <div class="bc-data" id="bcd:apii.TypoCatching">
+    // E.g. <div class="bc-data" data-query="apii.TypoCatching">
     if (!section.value.data) continue;
 
     for (const [key, data] of Object.entries(section.value.data)) {
@@ -127,7 +127,7 @@ function extractBCDData(doc) {
 
       if (!section.value.data) {
         // This happens if a query is "broken".
-        // E.g. <div class="bc-data" id="bcd:apii.TypoCatching">
+        // E.g. <div class="bc-data" data-query="apii.TypoCatching">
         continue;
       }
       const fileName = ++nextId > 1 ? `bcd-${nextId}.json` : "bcd.json";
