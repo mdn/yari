ea2fe1c33d7585b4b1727128cdc5b68ad850bdd7
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2022-05-14 18:36:11 -0700

    Add i18next-http-backend to dynamically load translations

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -1,15 +1,18 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
 
-i18n.use(initReactI18next).init({
-  resources: {},
-  debug: true,
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    debug: true,
 
-  fallbackLng: "en-US",
+    fallbackLng: "en-US",
 
-  interpolation: {
-    escapeValue: false, // not needed for react as it escapes by default
-  },
-});
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
 
 export default i18n;
