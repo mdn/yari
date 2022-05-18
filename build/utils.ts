de16b8f4de94193ddfe265a0826fbbdb8aca634e
Claas Augner <caugner@mozilla.com>
2022-05-18 18:20:11 +0200

    fix(build/utils): avoid assigning string to number variable

--- build/utils.ts
+++ build/utils.ts
@@ -16,15 +16,18 @@ const { VALID_MIME_TYPES } = require("../libs/constants");
 function humanFileSize(size) {
   if (size < 1024) return `${size} B`;
   const i = Math.floor(Math.log(size) / Math.log(1024));
-  let num = size / 1024 ** i;
+  const num = size / 1024 ** i;
   const round = Math.round(num);
+
+  let str: string;
   if (round < 10) {
-    num = num.toFixed(2);
+    str = num.toFixed(2);
   } else if (round < 100) {
-    num = num.toFixed(1);
+    str = num.toFixed(1);
   } else {
-    num = round;
+    str = String(round);
   }
+
   return `${num} ${"KMGTPEZY"[i - 1]}B`;
 }
 
