4ad16f2fb0e23e0a7624ee13629937d8d676d8b6
OnkarRuikar <87750369+OnkarRuikar@users.noreply.github.com>
2022-05-26 13:28:47 +0530

    Enhance code example blocks

--- build/syntax-highlight.js
+++ build/syntax-highlight.js
@@ -55,7 +55,7 @@ function syntaxHighlight($, doc) {
   // match. The wildcard would technically match `<pre class="brushetta">`
   // too. But within the loop, we do a more careful regex on the class name
   // and only proceed if it's something sensible we can use in Prism.
-  $("pre[class*=brush]").each((_, element) => {
+  $("pre[class*=brush]").each((index, element) => {
     // The language is whatever string comes after the `brush(:)`
     // portion of the class name.
     const $pre = $(element).wrapAll("<div class='code-example'>");
@@ -65,24 +65,113 @@ function syntaxHighlight($, doc) {
     if (!match) {
       return;
     }
-    const name = ALIASES.get(match[1]) || match[1];
-    if (IGNORE.has(name)) {
+    const language = ALIASES.get(match[1]) || match[1];
+    if (IGNORE.has(language)) {
       // Seems to exist a couple of these in our docs. Just bail.
       return;
     }
-    const grammar = Prism.languages[name];
+    const grammar = Prism.languages[language];
     if (!grammar) {
       console.warn(
-        `Unable to find a Prism grammar for '${name}' found in ${doc.mdn_url}`
+        `Unable to find a Prism grammar for '${language}' found in ${doc.mdn_url}`
       );
       return; // bail!
     }
-    const code = $pre.text();
-    const html = Prism.highlight(code, grammar, name);
-    const $code = $("<code>").html(html);
 
+    const addLineNumbers = className.match(/linenumbers/);
+    const code = $pre.text();
+    let html = "";
+
+    if (!addLineNumbers) {
+      html = Prism.highlight(code, grammar, language);
+    } else {
+      let highlights = className.match(/(?<=-)\d+/g);
+      highlights = highlights ? highlights : [];
+
+      const env = {
+        code: code,
+        grammar: grammar,
+        language: language,
+        codeBlockNo: index + 1,
+        highlights: highlights,
+      };
+
+      // use lower level APIs for finer control
+      env.tokens = Prism.tokenize(code, grammar);
+      Prism.plugins.enhance.addLines(env);
+      html = Prism.Token.stringify(Prism.util.encode(env.tokens), language);
+    }
+
+    const $code = $("<code>").html(html);
     $pre.empty().append($code);
   });
 }
+
+// plugin to add line numbers, highlighting, and anchors
+Prism.plugins.enhance = {
+  createLineToken(children, env, lineNo) {
+    const line = new Prism.Token("line", children);
+    line.codeBlockNo = env.codeBlockNo;
+    line.lineNo = lineNo;
+
+    if (env.highlights.includes(String(lineNo))) {
+      line.alias = "highlight";
+    }
+    return line;
+  },
+
+  createLineNumberToken(codeBlockNo, lineNo) {
+    const id = `E${codeBlockNo}L${lineNo}`;
+    const anchor = `<a id='${id}' href="#${id}" title="">${lineNo}</a>`;
+    return new Prism.Token("lineno", anchor);
+  },
+
+  addLines(env) {
+    if (Array.isArray(env.tokens) && env.tokens.length > 0) {
+      const newList = new Array();
+      let lineNo = 1;
+      let children = [this.createLineNumberToken(env.codeBlockNo, lineNo)];
+
+      // separate the tokens into lines
+      env.tokens.forEach((token) => {
+        if (typeof token === "string" && token.includes("\n")) {
+          let part = "";
+          while (token !== "") {
+            const position = token.indexOf("\n");
+            if (position >= 0) {
+              part = token.substring(0, position);
+              token = token.substring(position + 1);
+            } else {
+              part = token;
+              token = "";
+            }
+
+            children.push(part ? part : "\u200b");
+            if (position >= 0) {
+              newList.push(this.createLineToken(children, env, lineNo));
+              lineNo++;
+              children = [this.createLineNumberToken(env.codeBlockNo, lineNo)];
+            }
+          }
+        } else {
+          children.push(token);
+        }
+      });
+
+      if (children.length > 1) {
+        newList.push(this.createLineToken(children, env, lineNo));
+      }
+
+      env.tokens = newList;
+    }
+  },
+};
+
+// add a hook to unescape the anchor tag strings
+Prism.hooks.add("wrap", function (token) {
+  if (token.type === "lineno") {
+    token.content = token.content.replaceAll("&lt;", "<");
+  }
+});
 
 module.exports = { syntaxHighlight };
