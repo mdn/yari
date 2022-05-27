bcf58eeee252350b61f6f2a5e5702776b19ff88f
Claas Augner <caugner@mozilla.com>
2022-05-27 17:04:15 +0200

    chore(document-survey): add hints for final params

--- client/src/ui/molecules/document-survey/surveys.ts
+++ client/src/ui/molecules/document-survey/surveys.ts
@@ -12,8 +12,11 @@ export interface Survey {
 export const SURVEYS: Survey[] = [
   {
     key: "css_cascade_2022",
-    show: (doc: Doc) => /CSS/i.test(doc.mdn_url),
-    rate: 1,
+    show: (doc: Doc) =>
+      /CSS/i.test(
+        doc.mdn_url
+      ) /* && startTime <= Date.now() && Date.now() <= endTime */,
+    rate: 1 /* 0.05 */,
     src: "https://www.surveygizmo.com/s3/6818801/MDN-Short-survey-CSS-Cascade-Layers",
     teaser: "Shape the future of the web by taking a 3 questions micro survey:",
     question: "What's your experience with Cascade Layers?",
