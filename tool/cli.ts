2e4905597819ef12ccea0bb6c2b35416a79f7ece
Claas Augner <caugner@mozilla.com>
2022-06-03 18:30:07 +0200

    chore(tool): use import type

--- tool/cli.ts
+++ tool/cli.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { Doc } from "../client/src/document/types";
+import type { Doc } from "../client/src/document/types";
 
 const fs = require("fs");
 const path = require("path");
