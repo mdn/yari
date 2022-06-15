1e8276965308d6e1ed30242a9c11f33c84dc2995
Florian Merz <me@fiji-flo.de>
2022-06-15 14:50:36 +0200

    fix(bcd): update version and adjust types

--- client/src/document/ingredients/browser-compatibility-table/utils.ts
+++ client/src/document/ingredients/browser-compatibility-table/utils.ts
@@ -47,7 +47,11 @@ export function listFeatures(
   }
 
   for (const [subName, subIdentifier] of Object.entries(identifier)) {
-    if (subName !== "__compat" && subIdentifier.__compat) {
+    if (
+      subName !== "__compat" &&
+      "__compat" in subIdentifier &&
+      subIdentifier.__compat
+    ) {
       features.push({
         name: parentName ? `${parentName}.${subName}` : subName,
         compat: subIdentifier.__compat,
