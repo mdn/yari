b2b5ae77826118520ceb53eacc58923021bdf460
allo <yin199909@aliyun.com>
2022-06-27 17:02:41 +0800

    apply suggestions
    Co-authored-by: fiji-flo <3604775+fiji-flo@users.noreply.github.com>

--- markdown/cli.ts
+++ markdown/cli.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as os from "os";
+import * as path from "path";
 const fm = require("front-matter");
 import { program } from "@caporal/core";
 import * as chalk from "chalk";
@@ -141,11 +142,14 @@ program
       console.log(
         `Starting HTML to Markdown conversion in ${options.mode} mode`
       );
-      let folderSearch = args.folder + "/"; // add trailing slash to match folder only
-      // replace '\' and '/' with '\\' to make this regexp works on Windows
-      if (folderSearch && os.platform() === "win32") {
-        folderSearch = folderSearch.replace(/\\|\//g, "\\\\");
+      let folder = args.folder?.replace(/\\|\//g, path.sep); // using correct path separator
+      // add trailing path separator (if not exists) to match folder
+      if (folder && !folder.endsWith(path.sep)) {
+        folder += path.sep;
       }
+      // replace '\' with '\\' to make this regexp works on Windows
+      const folderSearch =
+        os.platform() === "win32" ? folder?.replace(/\\/g, "\\\\") : folder;
       const documents = Document.findAll({
         folderSearch: folderSearch,
         locales: buildLocaleMap(options.locale),
@@ -161,19 +165,17 @@ program
         string,
         { offset: number; invalid: []; unhandled: [] }
       >();
-      // replace '\' with '/' for Windows path
-      const slugPrefix = args.folder.replace(/\\/g, "/");
+      const folderPrefix = folder?.slice(0, -1);
       try {
         for (let doc of documents.iter()) {
           progressBar.increment();
           if (
             doc.isMarkdown ||
             // findAll's folderSearch is fuzzy which we don't want here
-            !doc.fileInfo.folder
-              .split(/\\|\//) // split by '\' or '/'
-              .slice(1) // remove the locale
-              .join("/")
-              .startsWith(slugPrefix)
+            (folderPrefix &&
+              !doc.fileInfo.folder
+                .slice(doc.fileInfo.folder.indexOf(path.sep) + 1)
+                .startsWith(folderPrefix))
           ) {
             continue;
           }
