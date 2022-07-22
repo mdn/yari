5152c00d64b63f5bf25791975fdc16156981b732
Florian Merz <me@fiji-flo.de>
2022-07-22 16:24:31 +0200

    remove old settings route

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -17,7 +17,6 @@ import { PageContentContainer } from "./ui/atoms/page-content";
 import { PageNotFound } from "./page-not-found";
 import { Plus } from "./plus";
 import { About } from "./about";
-import { Settings } from "./settings";
 import { docCategory } from "./utils";
 import { Contribute } from "./community";
 import { ContributorSpotlight } from "./contributor-spotlight";
@@ -220,16 +219,6 @@ export function App(appProps) {
                 element={
                   <StandardLayout extraClasses="plus">
                     <Plus {...appProps} />
-                  </StandardLayout>
-                }
-              />
-            )}
-            {PLUS_IS_ENABLED && (
-              <Route
-                path="/settings"
-                element={
-                  <StandardLayout>
-                    <Settings {...appProps} />
                   </StandardLayout>
                 }
               />
