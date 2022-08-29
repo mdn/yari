015d172d9dbe3831a31be088155c53be2805cb86
Claas Augner <caugner@mozilla.com>
2022-08-29 19:27:11 +0200

    ci(testing): separate lint from test (#6951)
    * ci(testing): separate lint from test
    * refactor(scripts): extract lint.sh

---
+++ scripts/lint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# Script equivalent of .github/workflows/testing.yml -> lint
+
+set -e
+
+echo "-------------------------"
+echo "Install all yarn packages"
+echo "-------------------------"
+
+yarn --frozen-lockfile
+
+echo "-------------"
+echo "Lint prettier"
+echo "-------------"
+
+yarn prettier-check
+
+echo "-----------"
+echo "Lint ESLint"
+echo "-----------"
+
+yarn eslint
