7d8dd564de548036701abeeff430cc6d07c93bf9
Onkar Ruikar <87750369+OnkarRuikar@users.noreply.github.com>
2022-09-02 19:18:27 +0530

    fix(interactive-examples): post theme message only to loaded iframes (#6560)
    Co-authored-by: Claas Augner <caugner@mozilla.com>

--- libs/constants/index.js
+++ libs/constants/index.js
@@ -76,9 +76,9 @@ const scriptSrcValues = [
    * we must always update the CSP hash (see instructions there).
    */
   // - Previous hash (to avoid cache invalidation issues):
-  "'sha256-x6Tv+AdV5e6dcolO0TEo+3BG4H2nG2ACjyG8mz6QCes='",
-  // - Current hash:
   "'sha256-GA8+DpFnqAM/vwERTpb5zyLUaN5KnOhctfTsqWfhaUA='",
+  // - Current hash:
+  "'sha256-R1d+R+j7z1re3EpSIuqRpflohOvTm3fAxKvfEiXgC1o='",
 ];
 const CSP_DIRECTIVES = {
   "default-src": ["'self'"],
