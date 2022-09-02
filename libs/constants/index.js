a335f8f255184bf19f33ada460284958aaccc7d4
Claas Augner <github@caugner.de>
2022-09-02 17:15:00 +0200

    fix(libs/constants): keep previous hash
    It has already been updated today.

--- libs/constants/index.js
+++ libs/constants/index.js
@@ -76,7 +76,7 @@ const scriptSrcValues = [
    * we must always update the CSP hash (see instructions there).
    */
   // - Previous hash (to avoid cache invalidation issues):
-  "'sha256-R1d+R+j7z1re3EpSIuqRpflohOvTm3fAxKvfEiXgC1o='",
+  "'sha256-GA8+DpFnqAM/vwERTpb5zyLUaN5KnOhctfTsqWfhaUA='",
   // - Current hash:
   "'sha256-GA8+DpFnqAM/vwERTpb5zyLUaN5KnOhctfTsqWfhaUA='",
 ];
