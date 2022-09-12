6de97642cb8c26a5bd17acce015d0a2151405a23
Claas Augner <caugner@mozilla.com>
2022-09-12 16:36:29 +0200

    fix(kumascript): remove misleading line/column from error
    The line/column are taken from the already pre-processed source,
    so they no longer correspond to the original file, even if
    accounting for frontmatter (what had already been done).

--- kumascript/src/errors.ts
+++ kumascript/src/errors.ts
@@ -47,34 +47,8 @@ export class SourceCodeError {
     ].join("\n");
   }
 
-  updateOffset(value) {
-    // Update the "offset" property to account for things like front-matter in the
-    // source. If the offset changes, this method will update related information.
-    // NOTE: We're not using a getter/setter for "offset", which would be a more
-    //       robust interface, so that the code that converts this instance to/from
-    //       JSON can remain simple.
-    if (this.offset !== value) {
-      // First, let's calculate the change in offset.
-      const offsetDelta = value - this.offset;
-      // Now, let's update things. First, the offset itself.
-      this.offset += offsetDelta;
-      // Next, let's update the line number.
-      this.line += offsetDelta;
-      // Finally, let's update the line numbers in the source context to reflect the new offset.
-      this.sourceContext = this.sourceContext.replace(
-        /^\s{0,4}(\d{1,5}) \| /gm,
-        (match, p1) => {
-          return `${(parseInt(p1) + offsetDelta).toString().padStart(5)} | `;
-        }
-      );
-    }
-  }
-
   updateFileInfo(fileInfo) {
     this.filepath = fileInfo.path;
-    // The extra `- 1` is because of the added newline that
-    // is only present because of the serialized linebreak.
-    this.updateOffset(fileInfo.frontMatterOffset - 1);
     return this;
   }
 
@@ -83,18 +57,10 @@ export class SourceCodeError {
   // doesn't really make sense. Perhaps we can modify this function to
   // show the relevant context in a more useful way.
   getSourceContext(source) {
-    function arrow(column) {
-      let arrow = "";
-      for (let i = 0; i < column + 7; i++) {
-        arrow += "-";
-      }
-      return `${arrow}^`;
-    }
+    const arrow = (line) => (line === this.line ? ">" : " ");
+    const arrowLine = (column) => " ".repeat(column + 3) + "^";
 
-    function formatLine(i, line) {
-      const lnum = `      ${i + 1}`.substr(-5);
-      return `${lnum} | ${line}`;
-    }
+    const formatLine = (i, lineSource) => `${arrow(i + 1)} | ${lineSource}`;
 
     const lines = source.split("\n");
 
@@ -110,7 +76,7 @@ export class SourceCodeError {
     for (let i = startLine; i < endLine; i++) {
       context.push(formatLine(i, lines[i]));
       if (i == errorLine) {
-        context.push(arrow(this.column));
+        context.push(arrowLine(this.column));
       }
     }
     return context.join("\n");
@@ -118,8 +84,7 @@ export class SourceCodeError {
 
   toString() {
     return (
-      `${this.name} error on ${this.macroName} ` +
-      `at line ${this.line}, column ${this.column} in:\n` +
+      `${this.name} error on ${this.macroName}:\n` +
       `${this.sourceContext}\nOriginal error: ${this.error.message}`
     );
   }
