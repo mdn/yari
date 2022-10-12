fb15e0fe5cfca1d5c12b00786f33dc040a258304
Guy Sleigh <gsleigh@mozilla.com>
2022-10-12 18:12:02 +0200

    Add browsers survey to 5% audience

--- client/src/ui/molecules/document-survey/surveys.ts
+++ client/src/ui/molecules/document-survey/surveys.ts
@@ -1,4 +1,5 @@
-import { Doc } from "../../../../../libs/types/document";
+import { Doc, BCDSection } from "../../../../../libs/types/document";
+import { survey_duration, survey_rates } from "../../../env";
 
 export interface Survey {
   key: SurveyKey;
@@ -17,12 +18,28 @@ export interface Survey {
 }
 
 enum SurveyBucket {
+  BROWSER_SURVEY_OCT_2022 = "BROWSER_SURVEY_OCT_2022",
   CSS_CASCADE_2022 = "CSS_CASCADE_2022",
 }
 
 enum SurveyKey {
+  BROWSER_SURVEY_OCT_2022 = "BROWSER_SURVEY_OCT_2022",
   CSS_CASCADE_2022_A = "CSS_CASCADE_2022_A",
   CSS_CASCADE_2022_B = "CSS_CASCADE_2022_B",
 }
 
-export const SURVEYS: Survey[] = [];
+export const SURVEYS: Survey[] = [
+  {
+    key: SurveyKey.BROWSER_SURVEY_OCT_2022,
+    bucket: SurveyBucket.BROWSER_SURVEY_OCT_2022,
+    show: (doc: Doc) =>
+      doc?.body.some((section) => section.type === "browser_compatibility"),
+    src: "https://www.surveygizmo.com/collab/7049440/Short-survey-browsers",
+    teaser:
+      "What is important to you when deciding which browser features to use? Take our 1 minute survey:",
+    question:
+      "What's important to you when deciding which browser features to use?",
+    ...survey_duration(SurveyBucket.BROWSER_SURVEY_OCT_2022),
+    ...survey_rates(SurveyKey.BROWSER_SURVEY_OCT_2022),
+  },
+];
