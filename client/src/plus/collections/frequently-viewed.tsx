1fca56181ea4e1a04b27382979356d5ef472cc32
Guy Sleigh <gsleigh@mozilla.com>
2022-11-17 12:59:35 +0100

    Migrate FV to new key

--- client/src/plus/collections/frequently-viewed.tsx
+++ client/src/plus/collections/frequently-viewed.tsx
@@ -14,15 +14,22 @@ export interface FrequentlyViewedCollection {
 export type FrequentlyViewedEntry = {
   parents?: ItemParent[];
   serial: number;
-  //This timestamp is deprecated and should be removed
-  timestamp?: number;
   timestamps: number[];
+  title: string;
+  url: string;
+};
+
+export type OldFrequentlyViewedEntry = {
+  parents?: ItemParent[];
+  serial: number;
+  timestamp: number;
   title: string;
   url: string;
   visitCount: number;
 };
 
-export const FREQUENTLY_VIEWED_STORAGE_KEY = "frequently-viewed-documents";
+export const FREQUENTLY_VIEWED_STORAGE_KEY = "frequently-viewed-documents-v2";
+export const FREQUENTLY_VIEWED_STORAGE_KEY_OLD = "frequently-viewed-documents";
 
 const ThirtyDaysMilliseconds = 30 * 24 * 60 * 60 * 1000;
 const isWithinLastThirtyDays = (date: Date): boolean => {
@@ -31,6 +38,7 @@ const isWithinLastThirtyDays = (date: Date): boolean => {
 };
 
 function getFrequentlyViewed(): FrequentlyViewedEntry[] {
+  migrateOld();
   let frequentlyViewed: string | null = null;
   try {
     frequentlyViewed = localStorage.getItem(FREQUENTLY_VIEWED_STORAGE_KEY);
@@ -45,29 +53,6 @@ function getFrequentlyViewed(): FrequentlyViewedEntry[] {
     frequentlyViewed || "[]"
   ) as FrequentlyViewedEntry[];
 
-  // Migrate old entries just once. Log their most recent timestamp 'visitCount' times. (If within last 30 days)
-  entries.forEach((val) => {
-    if (!val.timestamps) {
-      val.timestamps = [];
-      if (val.timestamp) {
-        if (isWithinLastThirtyDays(new Date(val.timestamp))) {
-          for (let i = 0; i < val.visitCount; i++) {
-            val.timestamps.push(val.timestamp);
-          }
-          //Delete old timestamp
-          val.timestamp = undefined;
-        }
-      }
-    }
-  });
-
-  // Assign serials to old entries.
-  entries.forEach((e) => {
-    e.serial =
-      e.serial === undefined
-        ? getNextFrequentlyViewedSerial(entries)
-        : e.serial;
-  });
   //Remove all timestamps older than 30 days and any pages with no more hits.
   return filterFrequentlyViewed(entries);
 }
@@ -86,20 +71,20 @@ function setFrequentlyViewed(frequentlyViewed: FrequentlyViewedEntry[]) {
   }
 }
 
-const sortByTimestampThenVistsDesc = (
+const sortByVisitsThenTimestampsDesc = (
   first: FrequentlyViewedEntry,
   second: FrequentlyViewedEntry
 ) => {
   //'Each timestamp represents one visit. The first is the most recent visit.
-  if (first.timestamps[0] < second.timestamps[0]) return 1;
-  if (first.timestamps[0] > second.timestamps[0]) return -1;
   if (first.timestamps.length > second.timestamps.length) return -1;
   if (first.timestamps.length < second.timestamps.length) return 1;
+  if (first.timestamps[0] < second.timestamps[0]) return 1;
+  if (first.timestamps[0] > second.timestamps[0]) return -1;
   return 0;
 };
 
 function getNextFrequentlyViewedSerial(
-  entries: FrequentlyViewedEntry[]
+  entries: OldFrequentlyViewedEntry[] | FrequentlyViewedEntry[]
 ): number {
   return (
     1 +
@@ -131,7 +116,6 @@ export function useFrequentlyViewed(
       setEnd && setEnd(true);
     }
     let paged: FrequentlyViewedItem[] = freqViewed
-      .sort(sortByTimestampThenVistsDesc)
       .slice(0, limit + offset)
       .map((val) => {
         const lastModified = val.timestamps[0]
@@ -163,6 +147,59 @@ export function useFrequentlyViewed(
   return collection;
 }
 
+function migrateOld() {
+  let frequentlyViewed: string | null = null;
+  try {
+    frequentlyViewed = localStorage.getItem(FREQUENTLY_VIEWED_STORAGE_KEY_OLD);
+  } catch (err) {
+    console.warn(
+      "Unable to read frequently viewed documents from localStorage",
+      err
+    );
+    return;
+  }
+
+  const entries = JSON.parse(
+    frequentlyViewed || "[]"
+  ) as OldFrequentlyViewedEntry[];
+
+  if (entries.length === 0) {
+    return;
+  }
+
+  const newEntries: FrequentlyViewedEntry[] = [];
+
+  // Migrate old entries. Log their most recent timestamp 'visitCount' times. (If within last 30 days)
+  entries.forEach((val) => {
+    if (isWithinLastThirtyDays(new Date(val.timestamp))) {
+      let timestamps: number[] = [];
+      for (let i = 0; i < val.visitCount; i++) {
+        timestamps.push(val.timestamp);
+      }
+
+      let newEntry: FrequentlyViewedEntry = {
+        serial:
+          val.serial === undefined
+            ? getNextFrequentlyViewedSerial(entries)
+            : val.serial,
+        timestamps,
+        title: val.title,
+        url: val.url,
+        parents: val.parents,
+      };
+      newEntries.push(newEntry);
+    }
+  });
+  newEntries.sort(sortByVisitsThenTimestampsDesc);
+  setFrequentlyViewed(newEntries);
+  try {
+    localStorage.removeItem(FREQUENTLY_VIEWED_STORAGE_KEY_OLD);
+  } catch (e) {
+    console.warn("Unable to remove old frequently viewed from localStorage");
+    return;
+  }
+}
+
 /**
  * @param  {Doc|undefined} doc
  * Persists frequently viewed docs to localstorage as part of MDN Plus MVP.
@@ -188,7 +225,6 @@ export function useIncrementFrequentlyViewed(doc: Doc | undefined) {
         url: doc.mdn_url,
         title: doc.title,
         parents: doc.parents,
-        visitCount: 1,
         timestamps: [new Date().getTime()],
       };
 
@@ -200,7 +236,7 @@ export function useIncrementFrequentlyViewed(doc: Doc | undefined) {
       }
     }
     //Sort descending so most frequently viewed appears on top.
-    frequentlyViewed = frequentlyViewed.sort(sortByTimestampThenVistsDesc);
+    frequentlyViewed = frequentlyViewed.sort(sortByVisitsThenTimestampsDesc);
     setFrequentlyViewed(frequentlyViewed);
   });
 }
