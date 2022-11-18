fdb041ceca044e28b121203709c0dd70063edc8f
Claas Augner <caugner@mozilla.com>
2022-11-18 19:08:02 +0100

    refactor(build): extract extract-sections.ts

--- build/index.ts
+++ build/index.ts
@@ -13,7 +13,7 @@ import { CONTENT_ROOT, REPOSITORY_URLS } from "../libs/env";
 import * as kumascript from "../kumascript";
 
 import { FLAW_LEVELS } from "../libs/constants";
-import { extractSections } from "./document-extractor";
+import { extractSections } from "./extract-sections";
 import { extractSidebar } from "./extract-sidebar";
 import { extractSummary } from "./extract-summary";
 export { default as SearchIndex } from "./search-index";
