000e371b21210644190cbd940c547585d1c2f9fb
Claas Augner <caugner@mozilla.com>
2022-12-19 17:52:58 +0100

    chore(paginator): add onChange prop

--- client/src/ui/molecules/paginator/index.tsx
+++ client/src/ui/molecules/paginator/index.tsx
@@ -4,11 +4,17 @@ import { range } from "../../../utils";
 function PageLink({
   page,
   children,
+  onClick,
 }: {
   page: number;
   children?: React.ReactNode;
+  onClick?: (page: number) => unknown;
 }) {
-  return <Link to={`?page=${page}`}>{children || page}</Link>;
+  return (
+    <Link to={`?page=${page}`} onClick={() => onClick && onClick(page)}>
+      {children || page}
+    </Link>
+  );
 }
 
 export function Paginator({
@@ -16,11 +22,13 @@ export function Paginator({
   last,
   endPadding = 2,
   middlePadding = 2,
+  onChange,
 }: {
   current: number;
   last: number;
   endPadding?: number;
   middlePadding?: number;
+  onChange?: (page: number, oldPage: number) => unknown;
 }) {
   const middleFirst = Math.max(current - middlePadding, 0);
   const middleLast = Math.min(middleFirst + middlePadding * 2 + 1, last + 1);
@@ -28,27 +36,37 @@ export function Paginator({
   const middle = range(middleFirst, middleLast);
   const right = range(Math.max(last + 1 - endPadding, middleLast), last + 1);
 
+  const onClick = (page: number) => onChange && onChange(page, current);
+
   return (
     <div className="pagination">
-      {current > 0 && <PageLink page={current - 1}>{"<"} Previous</PageLink>}
+      {current > 0 && (
+        <PageLink page={current - 1} onClick={onClick}>
+          {"<"} Previous
+        </PageLink>
+      )}
       {left.map((page) => (
-        <PageLink key={page} page={page} />
+        <PageLink key={page} page={page} onClick={onClick} />
       ))}
       {Boolean(left.length) && left[left.length - 1] + 1 !== middle[0] && "…"}
       {middle.map((page) =>
         current === page ? (
           <span key={page}>{page}</span>
         ) : (
-          <PageLink key={page} page={page} />
+          <PageLink key={page} page={page} onClick={onClick} />
         )
       )}
       {Boolean(right.length) &&
         middle[middle.length - 1] + 1 !== right[0] &&
         "…"}
       {right.map((page) => (
-        <PageLink key={page} page={page} />
+        <PageLink key={page} page={page} onClick={onClick} />
       ))}
-      {current < last && <PageLink page={current + 1}>Next {">"}</PageLink>}
+      {current < last && (
+        <PageLink page={current + 1} onClick={onClick}>
+          Next {">"}
+        </PageLink>
+      )}
     </div>
   );
 }
