c9f21e1b84738263d5e9d93c10304690a0db09a4
Leo McArdle <lmcardle@mozilla.com>
2023-02-02 18:02:40 +0000

    enhancement: attempt to fall back to static html when react unmounts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,6 +10,87 @@ import { GleanProvider } from "./telemetry/glean-context";
 
 // import * as serviceWorker from './serviceWorker';
 
+let attemptedFallback = false;
+window.addEventListener("error", async (error) => {
+  const root = document.querySelector("#root");
+  if (
+    !attemptedFallback &&
+    root &&
+    // only fall back if react gave up and unmounted, leaving an empty page
+    root.innerHTML.trim() === ""
+  ) {
+    attemptedFallback = true;
+    try {
+      const res = await fetch(window.location.pathname, {
+        cache: "force-cache",
+      });
+      const page = await res.text();
+      const parser = new DOMParser();
+      const fallbackRoot = parser
+        .parseFromString(page, "text/html")
+        .querySelector("#root");
+      if (fallbackRoot) {
+        root.innerHTML = fallbackRoot.innerHTML;
+        console.warn(
+          "Fell back to static HTML page due to unhandled error,",
+          "please report an issue on GitHub:",
+          generateGithubIssueLink(error.error)
+        );
+      } else {
+        throw new Error("no root");
+      }
+    } catch (fallbackError) {
+      console.warn(
+        "Failed falling back to static HTML page:",
+        fallbackError,
+        "please report an issue on GitHub:",
+        generateGithubIssueLink(error.error, fallbackError)
+      );
+    }
+  }
+  // Unlike other events, the error event is canceled by returning true from the handler instead of returning false.
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/error_event#usage_notes
+  return false;
+});
+
+function generateGithubIssueLink(error: Error, fallbackError?: any) {
+  try {
+    const params = new URLSearchParams();
+    params.append("title", `Unhandled error: ${error.message}`);
+    params.append("labels", "🐛 bug");
+    params.append(
+      "body",
+      `Location: ${"`"}${window.location}${"`"}
+
+Error:
+${"```"}
+${error.name}: ${error.message}
+${"```"}${
+        error.stack
+          ? `
+
+Stack:
+${"```"}
+${error.stack}
+${"```"}`
+          : ""
+      }${
+        fallbackError instanceof Error
+          ? `
+
+Error when attempting to fallback to HTML:
+${"```"}
+${fallbackError.name}: ${fallbackError.message}
+${"```"}`
+          : ""
+      }`
+    );
+    return `https://github.com/mdn/yari/issues/new?${params}`;
+  } catch {
+    return "https://github.com/mdn/yari/issues/new";
+  }
+}
+
 const container = document.getElementById("root");
 if (!container) {
   throw new Error("missing root element");
