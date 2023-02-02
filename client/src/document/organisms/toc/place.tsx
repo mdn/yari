51b678f72d09d3724c4e266f17e74637834b95c3
Florian Merz <me@fiji-flo.de>
2023-02-02 12:10:53 +0100

    url encdoe base64

--- client/src/document/organisms/toc/place.tsx
+++ client/src/document/organisms/toc/place.tsx
@@ -20,7 +20,9 @@ export function Place() {
         const intersectionObserver = new IntersectionObserver((entries) => {
           const [{ isIntersecting = false } = {}] = entries;
           if (isIntersecting && typeof navigator !== "undefined") {
-            navigator?.sendBeacon(`/pong/viewed?code=${pong?.impression}`);
+            navigator?.sendBeacon(
+              `/pong/viewed?code=${encodeURIComponent(pong?.impression)}`
+            );
             observer.current?.disconnect();
           }
         }, observerOptions);
@@ -41,7 +43,7 @@ export function Place() {
         <section ref={place} className="place">
           <a
             className="pong"
-            href={`/pong/click?code=${pong?.click}`}
+            href={`/pong/click?code=${encodeURIComponent(pong?.click)}`}
             target="_blank"
             rel="noreferrer"
           >
