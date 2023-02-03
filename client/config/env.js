82346ba97522c5381f48fd092191c043d6af9c8c
Claas Augner <caugner@mozilla.com>
2023-02-03 17:02:58 +0100

    chore(client): load only ENV_FILE or .env, remove dotenv-expand
    Previously, the client would also load other environment files,
    but to be consistent with the rest of yari, we only load .env,
    unless ENV_FILE is set, in which case we load that file instead.

--- client/config/env.js
+++ client/config/env.js
@@ -13,33 +13,12 @@ if (!NODE_ENV) {
 }
 
 const ENV_FILE = process.env.ENV_FILE;
+const dotenvFile = ENV_FILE
+  ? paths.dotenv.replace(".env", ENV_FILE)
+  : paths.dotenv;
 
-// https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
-const dotenvFiles = ENV_FILE
-  ? [paths.dotenv.replace(".env", ENV_FILE)]
-  : [
-      `${paths.dotenv}.${NODE_ENV}.local`,
-      // Don't include `.env.local` for `test` environment
-      // since normally you expect tests to produce the same
-      // results for everyone
-      NODE_ENV !== "test" && `${paths.dotenv}.local`,
-      `${paths.dotenv}.${NODE_ENV}`,
-      paths.dotenv,
-    ].filter(Boolean);
-
-// Load environment variables from .env* files. Suppress warnings using silent
-// if this file is missing. dotenv will never modify any environment variables
-// that have already been set.  Variable expansion is supported in .env files.
-// https://github.com/motdotla/dotenv
-// https://github.com/motdotla/dotenv-expand
-dotenvFiles.forEach((dotenvFile) => {
-  if (fs.existsSync(dotenvFile)) {
-    require("dotenv-expand").expand(
-      require("dotenv").config({
-        path: dotenvFile,
-      })
-    );
-  }
+require("dotenv").config({
+  path: dotenvFile,
 });
 
 // We support resolving modules according to `NODE_PATH`.
