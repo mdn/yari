2144b8d243db8f502e72f43ae27f6e8d7a5d73bb
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-02-12 05:51:06 -0800

    Remove now-irrelevant tests

--- kumascript/tests/macros/Deprecated.test.ts
+++ kumascript/tests/macros/Deprecated.test.ts
@@ -10,35 +10,4 @@ describeMacro("Deprecated_Inline", function () {
 </abbr>`
     );
   });
-  itMacro("HTML-prefixed version (en-US)", function (macro) {
-    return assert.eventually.equal(
-      macro.call("html4"),
-      `<span class="notecard inline deprecated" title="">Deprecated since <a href="/en-US/docs/HTML">HTML4</a></span>`
-    );
-  });
-  itMacro("JS-prefixed version (en-US)", function (macro) {
-    return assert.eventually.equal(
-      macro.call("js1.7"),
-      `<span class="notecard inline deprecated" title="">Deprecated since <a href="/en-US/docs/JavaScript/New_in_JavaScript/1.7">JavaScript 1.7</a></span>`
-    );
-  });
-  itMacro("CSS-prefixed version (en-US)", function (macro) {
-    return assert.eventually.equal(
-      macro.call("css2"),
-      `<span class="notecard inline deprecated" title="">Deprecated since CSS 2</span>`
-    );
-  });
-  itMacro("CSS-prefixed version (ja)", function (macro) {
-    macro.ctx.env.locale = "ja";
-    return assert.eventually.equal(
-      macro.call("css2"),
-      `<span class="notecard inline deprecated" title="">非推奨 CSS 2</span>`
-    );
-  });
-  itMacro("Nonsense-prefixed version (en-US)", function (macro) {
-    return assert.eventually.equal(
-      macro.call("foobar13"),
-      `<span class="notecard inline deprecated" title="">Deprecated</span>`
-    );
-  });
 });
