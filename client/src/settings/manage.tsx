08d626bf65e8f5cd14eb4d5f11025a1926f8a45b
Florian Merz <me@fiji-flo.de>
2023-02-15 14:41:41 +0100

    don't show ads settings

--- client/src/settings/manage.tsx
+++ client/src/settings/manage.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { FXA_MANAGE_SUBSCRIPTIONS_URL, FXA_SETTINGS_URL } from "../env";
+import {
+  FXA_MANAGE_SUBSCRIPTIONS_URL,
+  FXA_SETTINGS_URL,
+  PLACEMENT_ENABLED,
+} from "../env";
 import { toggleNoAds } from "../plus/common/api";
 import {
   TOGGLE_PLUS_ADS_FREE_DISABLED,
@@ -20,45 +24,49 @@ export function Manage() {
     <section className="field-group">
       <h2>Manage account</h2>
       <ul>
-        <li>
-          <h3>Go ads free</h3>
-          {isPlusSubscriber(user) ? (
-            <>
-              <span>
-                Turn off advertising on MDN. Read more about{" "}
-                <a href="/en-US/advertising">ads on MDN</a>
-              </span>
-              {saving ? (
-                <Spinner extraClasses="loading" />
-              ) : (
-                <Switch
-                  name="no_ads"
-                  checked={Boolean(user?.settings?.noAds)}
-                  toggle={async (e) => {
-                    setSaving(true);
-                    const checked = Boolean(e.target.checked);
-                    const source = checked
-                      ? TOGGLE_PLUS_ADS_FREE_DISABLED
-                      : TOGGLE_PLUS_ADS_FREE_ENABLED;
-                    gleanClick(source);
-                    await toggleNoAds(checked);
-                    if (user?.settings) {
-                      user.settings.noAds = checked;
-                    }
-                    user?.mutate();
-                    setSaving(false);
-                  }}
-                ></Switch>
-              )}
-            </>
-          ) : (
-            <p>
-              The ads free feature is only available to MDN Plus subscribers.{" "}
-              <a href={`/en-US/plus?ref=nope_settings#subscribe`}>Learn more</a>{" "}
-              about our plans.
-            </p>
-          )}
-        </li>
+        {PLACEMENT_ENABLED && (
+          <li>
+            <h3>Go ads free</h3>
+            {isPlusSubscriber(user) ? (
+              <>
+                <span>
+                  Turn off advertising on MDN. Read more about{" "}
+                  <a href="/en-US/advertising">ads on MDN</a>
+                </span>
+                {saving ? (
+                  <Spinner extraClasses="loading" />
+                ) : (
+                  <Switch
+                    name="no_ads"
+                    checked={Boolean(user?.settings?.noAds)}
+                    toggle={async (e) => {
+                      setSaving(true);
+                      const checked = Boolean(e.target.checked);
+                      const source = checked
+                        ? TOGGLE_PLUS_ADS_FREE_DISABLED
+                        : TOGGLE_PLUS_ADS_FREE_ENABLED;
+                      gleanClick(source);
+                      await toggleNoAds(checked);
+                      if (user?.settings) {
+                        user.settings.noAds = checked;
+                      }
+                      user?.mutate();
+                      setSaving(false);
+                    }}
+                  ></Switch>
+                )}
+              </>
+            ) : (
+              <p>
+                The ads free feature is only available to MDN Plus subscribers.{" "}
+                <a href={`/en-US/plus?ref=nope_settings#subscribe`}>
+                  Learn more
+                </a>{" "}
+                about our plans.
+              </p>
+            )}
+          </li>
+        )}
 
         <li>
           <h3>Firefox account</h3>
