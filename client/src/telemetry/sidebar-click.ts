c8755bb7a61143c738f5ed751a0ff1ece1a9226c
Claas Augner <caugner@mozilla.com>
2023-02-22 17:59:35 +0100

    chore(telemetry/sidebar): shorten event value

--- client/src/telemetry/sidebar-click.ts
+++ client/src/telemetry/sidebar-click.ts
@@ -6,7 +6,7 @@ export function handleSidebarClick(
 ) {
   const payload = getClickPayload(event);
   if (payload) {
-    const key = `${SIDEBAR_CLICK}: ${JSON.stringify(payload)}`;
+    const key = `${SIDEBAR_CLICK}@${payload.macro}: ${payload.current}/${payload.line_dist}/${payload.tree_dist}/${payload.slug_dist}->${payload.to}`;
     gleanClick(key);
     console.log({ key, length: key.length });
   }
