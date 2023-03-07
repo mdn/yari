63e5c448c4a09c178f8a91a5bd6c0ec35128a04b
Claas Augner <caugner@mozilla.com>
2023-03-07 17:33:23 +0100

    fix(gcp/function): use pathname for redirects

--- gcp/function/src/middlewares/redirects.ts
+++ gcp/function/src/middlewares/redirects.ts
@@ -15,6 +15,8 @@ export function redirects(
   res: express.Response,
   next: express.NextFunction
 ) {
+  const url = new URL(req.url, `${req.protocol}://${req.headers.host}`);
+
   // If the URL was something like `https://domain/en-US/search/`, our code
   // would make a that a redirect to `/en-US/search` (stripping the trailing slash).
   // But if it was `https://domain//en-US/search/` it *would* make a redirect
@@ -27,19 +29,28 @@ export function redirects(
   // Prevent any pathnames that start with a double //.
   // This essentially means that a request for `GET /////anything` becomes
   // 302 with `Location: /anything`.
-  if (req.url.startsWith("//")) {
-    res.redirect(`/${req.url.replace(/^\/+/g, "")}`);
+  if (url.pathname.startsWith("//")) {
+    const target = `/${url.pathname.replace(/^\/+/g, "")}`;
+    res.redirect(target);
     next();
     return;
   }
 
-  const { url, status } = resolveFundamental(req.url);
-  if (url) {
+  const redirect = resolveFundamental(url.pathname);
+  if (redirect.url) {
+    // NOTE: The query string is not forwarded for document requests,
+    //       as directed by their origin request policy, so it's safe to
+    //       assume "request.querystring" is empty for document requests.
+    if (url.search) {
+      redirect.url +=
+        (redirect.url.includes("?") ? "&" : "?") + url.search.substring(1);
+    }
+
     res.set("Cache-Control", `max-age=${THIRTY_DAYS}`);
-    if (status) {
-      res.redirect(status, url);
+    if (redirect.status) {
+      res.redirect(redirect.status, redirect.url);
     } else {
-      res.redirect(url);
+      res.redirect(redirect.url);
     }
     next();
     return;
@@ -49,7 +60,7 @@ export function redirects(
   // Example:
   // - Encoded: /zh-TW/docs/AJAX:%E4%B8%8A%E6%89%8B%E7%AF%87
   // - Decoded: /zh-TW/docs/AJAX:上手篇
-  const decodedUri = decodePath(req.url);
+  const decodedUri = decodePath(url.pathname);
   const decodedUriLC = decodedUri.toLowerCase();
 
   // Redirect moved pages (see `_redirects.txt` in content/translated-content).
