02b0058dd4d69b949efa368b985ce1e51f082c25
Florian Merz <me@fiji-flo.de>
2023-03-20 17:19:40 +0100

    add empty env.js

---
+++ deployer/aws-lambda/kevel/env.js
@@ -0,0 +1,6 @@
+// generated
+export const KEVEL_SITE_ID = undefined;
+export const KEVEL_NETWORK_ID = undefined;
+export const SIGN_SECRET = "undefined";
+export const CARBON_ZONE_KEY = "undefined";
+export const FALLBACK_ENABLED = false;
