dc60c6775d8de565c99a57d56a9cf3c834c455e6
Claas Augner <caugner@mozilla.com>
2023-03-23 00:49:54 +0100

    fix(gcp/function): add req.originalUrl workaround

--- gcp/function/src/middlewares/resolveIndexHTML.ts
+++ gcp/function/src/middlewares/resolveIndexHTML.ts
@@ -11,6 +11,9 @@ export function resolveIndexHTML(
   if (path.extname(resolvedUrl) === "") {
     resolvedUrl = path.join(resolvedUrl, "index.html");
   }
-  req.url = resolvedUrl;
+  req.originalUrl = req.url = resolvedUrl;
+  // Workaround for http-proxy-middleware v2 using `req.originalUrl`.
+  // See: https://github.com/chimurai/http-proxy-middleware/pull/731
+  req.originalUrl = req.url;
   next();
 }
