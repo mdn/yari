8237f162b676d8f378b089d4c9e24f3d004d0650
Claas Augner <caugner@mozilla.com>
2023-03-22 23:26:08 +0100

    refactor(gcp/function): extract redirect() helper

--- gcp/function/src/utils.ts
+++ gcp/function/src/utils.ts
@@ -11,3 +11,30 @@ export function getRequestCountry(req: express.Request): string {
     return DEFAULT_COUNTRY;
   }
 }
+
+export function redirect(
+  res: express.Response,
+  location: string,
+  { status = 302, cacheControlSeconds = 0 } = {}
+): void {
+  let cacheControlValue;
+  if (cacheControlSeconds) {
+    cacheControlValue = `max-age=${cacheControlSeconds},public`;
+  } else {
+    cacheControlValue = "no-store";
+  }
+
+  // We need to URL encode the pathname, but leave the query string as is.
+  // Suppose the old URL was `/search?q=text%2Dshadow` and all we need to do
+  // is to inject the locale to that URL, we should not URL encode the whole
+  // new URL otherwise you'd end up with `/en-US/search?q=text%252Dshadow`
+  // since the already encoded `%2D` would become `%252D` which is wrong and
+  // different.
+  const [pathname, querystring] = location.split("?", 2);
+  let newLocation = encodeURI(pathname || "");
+  if (querystring) {
+    newLocation += `?${querystring}`;
+  }
+
+  res.set("Cache-Control", cacheControlValue).redirect(status, newLocation);
+}
