0db5220da701f7fcbcf89d4db11fd2979abfe15a
Claas Augner <caugner@mozilla.com>
2023-03-23 01:20:45 +0100

    feat(gcp/function): handle 404 in content

--- gcp/function/src/handlers/content.ts
+++ gcp/function/src/handlers/content.ts
@@ -1,15 +1,33 @@
 import type express from "express";
-import { createProxyMiddleware } from "http-proxy-middleware";
+import {
+  createProxyMiddleware,
+  responseInterceptor,
+} from "http-proxy-middleware";
 
 import { withProxyResponseHeaders } from "../headers.js";
 import { Source, sourceUri } from "../env.js";
 
+const NOT_FOUND_PATH = "en-us/_spas/404.html";
+
 export function createContentProxy(): express.Handler {
+  const target = sourceUri(Source.content);
   return createProxyMiddleware({
     prependPath: true,
     changeOrigin: true,
-    target: sourceUri(Source.content),
+    target,
     autoRewrite: true,
-    onProxyRes: withProxyResponseHeaders,
+    selfHandleResponse: true,
+    onProxyRes: responseInterceptor(
+      async (responseBuffer, proxyRes, req, res) => {
+        withProxyResponseHeaders(proxyRes, req, res);
+        if (proxyRes.statusCode === 404) {
+          const response = await fetch(`${target}${NOT_FOUND_PATH}`);
+          res.setHeader("Content-Type", "text/html");
+          return Buffer.from(await response.arrayBuffer());
+        }
+
+        return responseBuffer;
+      }
+    ),
   });
 }
