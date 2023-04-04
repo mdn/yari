5083eb3aa1b585bcda309073e1be7e4a5d5515a0
Florian Dieminger <me@fiji-flo.de>
2023-04-04 15:55:55 +0200

    refactor(placement): introduce PlacementContext (#8522)
    * enhance(placement): introduce a placment context
    
    Previously the logic for fetching lived within the component.
    By refactoring this we start to enable more complex queries for future
    enhancements.
    
    Co-authored-by: Claas Augner <495429+caugner@users.noreply.github.com>

--- client/src/ui/organisms/placement/index.tsx
+++ client/src/ui/organisms/placement/index.tsx
@@ -1,46 +1,16 @@
 import { useCallback, useEffect, useRef } from "react";
-import { PLACEMENT_ENABLED } from "../../../env";
 import { useIsServer, usePageVisibility } from "../../../hooks";
-import useSWR from "swr";
 import { useUserData } from "../../../user-context";
 
 import "./index.scss";
 import { useGleanClick } from "../../../telemetry/glean-context";
+import { PlacementStatus, usePlacement } from "../../../placement-context";
 
 interface Timer {
   timeout: number | null;
   start: number | null;
   notVisible?: boolean;
 }
-
-enum Status {
-  success = "success",
-  geoUnsupported = "geo_unsupported",
-  capReached = "cap_reached",
-}
-
-export interface Fallback {
-  click: string;
-  view: string;
-  copy: string;
-  image: string;
-  by: string;
-}
-
-interface PlacementError {
-  status: Status.geoUnsupported | Status.capReached;
-}
-
-interface PlacementStatus {
-  status: Status.success;
-  click: string;
-  view: string;
-  copy?: string;
-  image?: string;
-  fallback?: Fallback;
-}
-
-type PlacementData = PlacementStatus | PlacementError;
 
 function viewed(
   pong: PlacementStatus,
@@ -57,48 +27,12 @@ function viewed(
 }
 
 export function Placement() {
-  const user = useUserData();
-  const gleanClick = useGleanClick();
-  const {
-    data: pong,
-    isLoading,
-    isValidating,
-  } = useSWR<PlacementData>(
-    !PLACEMENT_ENABLED || user?.settings?.noAds ? null : "/pong/get",
-    async (url) => {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ keywords: [] }),
-      });
+  const placementData = usePlacement();
 
-      gleanClick(`pong: pong->fetched ${response.status}`);
-
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-
-      try {
-        const placementResponse: PlacementData = await response.json();
-        gleanClick(`pong: pong->status ${placementResponse.status}`);
-        return placementResponse;
-      } catch (e) {
-        throw Error(response.statusText);
-      }
-    },
-    {
-      revalidateIfStale: true,
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    }
-  );
-
-  return isLoading || isValidating ? (
+  return !placementData ? (
     <section className="place"></section>
   ) : (
-    <PlacementInner pong={pong}></PlacementInner>
+    <PlacementInner pong={placementData}></PlacementInner>
   );
 }
 
