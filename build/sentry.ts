89175e98adede8b2788b98942db7e1135ad03677
Claas Augner <caugner@mozilla.com>
2023-04-04 14:02:33 +0200

    feat(build/sentry): capture console.warn/error/assert() calls

--- build/sentry.ts
+++ build/sentry.ts
@@ -1,11 +1,13 @@
 import Sentry from "@sentry/node";
+import { CaptureConsole } from "@sentry/integrations";
 
 export function initSentry(
   dsn: string,
-  options?: Omit<Sentry.NodeOptions, "dsn">
+  options?: Omit<Sentry.NodeOptions, "dsn" | "integrations">
 ) {
   Sentry.init({
     dsn,
+    integrations: [new CaptureConsole({ levels: ["warn", "error", "assert"] })],
     tracesSampleRate: 1.0,
     ...options,
   });
