0ce2dd89e71f77b4e3b6f14eb47f6c98c631e8f0
Claas Augner <caugner@mozilla.com>
2023-04-06 19:23:14 +0200

    chore(gcp/function): set logLevel = debug

--- gcp/function/src/handlers/telemetry.ts
+++ gcp/function/src/handlers/telemetry.ts
@@ -2,6 +2,7 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 export const proxyTelemetry = createProxyMiddleware({
   target: "https://incoming.telemetry.mozilla.org",
+  logLevel: "debug",
   changeOrigin: true,
   autoRewrite: true,
   proxyTimeout: 20000,
