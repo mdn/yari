8a1186a881dc41b62b1b18443bca936a32ac85ac
Claas Augner <caugner@mozilla.com>
2023-04-06 02:42:47 +0200

    fix(gcp/function): do not change origin

--- gcp/function/src/handlers/telemetry.ts
+++ gcp/function/src/handlers/telemetry.ts
@@ -2,7 +2,6 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 export const proxyTelemetry = createProxyMiddleware({
   target: "https://incoming.telemetry.mozilla.org",
-  changeOrigin: true,
   autoRewrite: true,
   xfwd: true,
 });
