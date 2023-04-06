48a10a0509182189538fcf2885b22231421c6e4a
Claas Augner <caugner@mozilla.com>
2023-04-06 03:53:02 +0200

    Revert "chore(gcp/function): stop redirecting leading slash"
    This reverts commit 423b8c4b57b4d704146fec6ee6c15d735600d088.

--- gcp/function/src/app.ts
+++ gcp/function/src/app.ts
@@ -8,6 +8,7 @@ import { plans } from "./handlers/plans.js";
 import { proxyTelemetry } from "./handlers/telemetry.js";
 import { pathnameLC } from "./middlewares/pathnameLC.js";
 import { resolveIndexHTML } from "./middlewares/resolveIndexHTML.js";
+import { redirectLeadingSlash } from "./middlewares/redirectLeadingSlash.js";
 import { redirectMovedPages } from "./middlewares/redirectMovedPages.js";
 import { redirectFundamental } from "./middlewares/redirectFundamental.js";
 import { redirectLocale } from "./middlewares/redirectLocale.js";
@@ -15,6 +16,7 @@ import { redirectTrailingSlash } from "./middlewares/redirectTrailingSlash.js";
 
 const mainRouter = Router();
 const proxyContent = createContentProxy();
+mainRouter.use(redirectLeadingSlash);
 mainRouter.all("/api/v1/stripe/plans", plans);
 mainRouter.all("/api/*", proxyRumba);
 mainRouter.all("/admin-api/*", proxyRumba);
