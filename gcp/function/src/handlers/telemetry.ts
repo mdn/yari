fe81cc43221dbcfdc9b27099d57154502e0e1b46
Claas Augner <caugner@mozilla.com>
2023-04-06 22:51:46 +0200

    chore(gcp/function): proxy telemetry with http-proxy
    Instead of http-proxy-middleware.

--- gcp/function/src/handlers/telemetry.ts
+++ gcp/function/src/handlers/telemetry.ts
@@ -1,9 +1,14 @@
-import { createProxyMiddleware } from "http-proxy-middleware";
+import express from "express";
+import httpProxy from "http-proxy";
 
-export const proxyTelemetry = createProxyMiddleware({
-  target: "https://incoming.telemetry.mozilla.org",
-  changeOrigin: true,
-  autoRewrite: true,
-  proxyTimeout: 20000,
-  xfwd: true,
-});
+const proxy = httpProxy.createProxyServer();
+
+export const proxyTelemetry = (req: express.Request, res: express.Response) => {
+  proxy.web(req, res, {
+    target: "https://incoming.telemetry.mozilla.org",
+    changeOrigin: true,
+    autoRewrite: true,
+    proxyTimeout: 20000,
+    xfwd: true,
+  });
+};
