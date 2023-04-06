f1beaaaefbc18d205747de2f3123c6232596493a
Claas Augner <caugner@mozilla.com>
2023-04-06 19:54:42 +0200

    fixup! chore(gcp/function): test with httpbin.org

--- gcp/function/src/handlers/telemetry.ts
+++ gcp/function/src/handlers/telemetry.ts
@@ -1,11 +1,14 @@
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 export const proxyTelemetry = createProxyMiddleware({
-  target: "https://httpbin.org/post",
+  target: "https://httpbin.org",
   logLevel: "debug",
   secure: false,
   changeOrigin: true,
   autoRewrite: true,
+  pathRewrite: function (path, req) {
+    return `/anything`;
+  },
   proxyTimeout: 20000,
   xfwd: true,
   headers: {
