ac2da1dde3f3cca4b41f66253b521242caf12387
Claas Augner <caugner@mozilla.com>
2023-04-12 00:36:25 +0200

    fix(gcp/function): set Cache-Control for content
    Hashed filenames are cached for 1 year, everything else for 1 day,
    except some files that don't get cached.

--- gcp/function/src/headers.ts
+++ gcp/function/src/headers.ts
@@ -3,8 +3,15 @@ import type express from "express";
 
 import { CSP_VALUE } from "./internal/constants/index.js";
 
+const HASHED_MAX_AGE = 60 * 60 * 24 * 365;
+const DEFAULT_MAX_AGE = 60 * 60 * 24;
+
+const NO_CACHE_VALUE = "no-store, must-revalidate";
+
+const HASHED_REGEX = /\.[a-f0-9]{8,32}\./;
+
 export function withContentResponseHeaders(
-  _proxyRes: IncomingMessage,
+  proxyRes: IncomingMessage,
   req: IncomingMessage,
   res: ServerResponse<IncomingMessage>
 ): ServerResponse<IncomingMessage> {
@@ -12,12 +19,14 @@ export function withContentResponseHeaders(
     return res;
   }
 
-  const isLiveSampleURI = req.url?.includes("/_sample_.") ?? false;
+  const url = req.url ?? "";
+
+  const isLiveSampleURI = url.includes("/_sample_.") ?? false;
 
   setContentResponseHeaders((name, value) => res.setHeader(name, value), {
     csp:
       !isLiveSampleURI &&
-      parseContentType(_proxyRes.headers["content-type"]).startsWith(
+      parseContentType(proxyRes.headers["content-type"]).startsWith(
         "text/html"
       ),
     xFrame: !isLiveSampleURI,
@@ -28,7 +37,36 @@ export function withContentResponseHeaders(
     res.setHeader("Content-Encoding", "gzip");
   }
 
+  const cacheControl = getCacheControl(proxyRes.statusCode ?? 0, url);
+  if (cacheControl) {
+    res.setHeader("Cache-Control", cacheControl);
+  }
+
   return res;
+}
+
+function getCacheControl(statusCode: number, url: string) {
+  if (
+    statusCode === 404 ||
+    url.endsWith("/service-worker.js") ||
+    url.includes("/_whatsdeployed/")
+  ) {
+    return NO_CACHE_VALUE;
+  }
+
+  if (200 <= statusCode && statusCode < 300) {
+    const maxAge = getCacheMaxAgeForUrl(url);
+    return `public, max-age=${maxAge}`;
+  }
+
+  return null;
+}
+
+function getCacheMaxAgeForUrl(url: string): number {
+  const isHashed = HASHED_REGEX.test(url);
+  const maxAge = isHashed ? HASHED_MAX_AGE : DEFAULT_MAX_AGE;
+
+  return maxAge;
 }
 
 function parseContentType(value: unknown): string {
