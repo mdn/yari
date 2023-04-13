0df5c1c308622da6fedb56bc9c8367da7b49ab22
Claas Augner <caugner@mozilla.com>
2023-04-13 17:22:31 +0200

    fix(gcp/function): use Cloudfront-Viewer-Country header

--- gcp/function/src/utils.ts
+++ gcp/function/src/utils.ts
@@ -2,8 +2,7 @@ import type express from "express";
 import { DEFAULT_COUNTRY } from "./constants.js";
 
 export function getRequestCountry(req: express.Request): string {
-  // https://cloud.google.com/appengine/docs/flexible/reference/request-headers#app_engine-specific_headers
-  const value = req.headers["x-appengine-country"];
+  const value = req.headers["cloudfront-viewer-country"];
 
   if (typeof value === "string" && value !== "ZZ") {
     return value;
