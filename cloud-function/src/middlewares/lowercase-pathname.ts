2f44e686ab02a3fd6bd5664ada133d8c9baba5a2
Claas Augner <caugner@mozilla.com>
2023-04-18 16:31:57 +0200

    fix(cloud-function): touch req.url only if url has pathname

--- cloud-function/src/middlewares/lowercase-pathname.ts
+++ cloud-function/src/middlewares/lowercase-pathname.ts
@@ -10,10 +10,10 @@ export async function lowercasePathname(
   const urlParsed = url.parse(req.url);
   if (urlParsed.pathname) {
     urlParsed.pathname = urlParsed.pathname.toLowerCase();
+    req.url = url.format(urlParsed);
+    // Workaround for http-proxy-middleware v2 using `req.originalUrl`.
+    // See: https://github.com/chimurai/http-proxy-middleware/pull/731
+    req.originalUrl = req.url;
   }
-  req.url = url.format(urlParsed);
-  // Workaround for http-proxy-middleware v2 using `req.originalUrl`.
-  // See: https://github.com/chimurai/http-proxy-middleware/pull/731
-  req.originalUrl = req.url;
   next();
 }
