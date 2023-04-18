704447a2e4a27ed904deed97395e16d5b726675e
Claas Augner <caugner@mozilla.com>
2023-04-18 16:21:10 +0200

    fix(cloud-function): add index.html to everything except assets

--- cloud-function/src/middlewares/resolve-index-html.ts
+++ cloud-function/src/middlewares/resolve-index-html.ts
@@ -4,14 +4,21 @@ import { NextFunction, Request, Response } from "express";
 
 import { slugToFolder } from "../internal/slug-utils/index.js";
 
+// These are the only extensions in client/build/*/docs/*.
+// `find client/build -type f | grep docs | xargs basename | sed 's/.*\.\([^.]*\)$/\1/' | sort | uniq`
+const ASSET_REGEXP = /\.(gif|html|jpeg|jpg|json|png|svg|txt)$/i;
+
+function isAsset(url: string) {
+  return ASSET_REGEXP.test(url);
+}
+
 export async function resolveIndexHTML(
   req: Request,
   _res: Response,
   next: NextFunction
 ) {
   let resolvedUrl = slugToFolder(req.path);
-  const ext = path.extname(resolvedUrl);
-  if (ext === "") {
+  if (!isAsset(resolvedUrl)) {
     resolvedUrl = path.join(resolvedUrl, "index.html");
   }
   req.url = resolvedUrl;
