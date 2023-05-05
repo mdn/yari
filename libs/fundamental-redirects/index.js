daa82702b9f001feec89c9c06c996a523dbd2a1d
Claas Augner <caugner@mozilla.com>
2023-05-05 20:39:07 +0200

    feat(fundamental-redirects): redirect archived media

--- libs/fundamental-redirects/index.js
+++ libs/fundamental-redirects/index.js
@@ -1232,6 +1232,20 @@ const MISC_REDIRECT_PATTERNS = [
     ({ prefix, subPath = "" }) => `/docs/${prefix}${subPath}`,
     { permanent: true }
   ),
+  // Content archived as part of the GCP migration.
+  redirect(/^diagrams\/?$/i, "https://mdn.dev/archives/media/diagrams/", {
+    permanent: false,
+  }),
+  redirect(
+    /^presentations\/?$/i,
+    "https://mdn.dev/archives/media/presentations/",
+    {
+      permanent: false,
+    }
+  ),
+  redirect(/^samples\/?$/i, "https://mdn.dev/archives/media/samples/", {
+    permanent: false,
+  }),
 ];
 
 const REDIRECT_PATTERNS = [].concat(
