f2cbc524b4dc31394933f2c8168c48bbb1f3fc80
Florian Dieminger <me@fiji-flo.de>
2023-05-22 16:59:17 +0200

    wip

--- client/src/search/use-ai.ts
+++ client/src/search/use-ai.ts
@@ -16,36 +16,12 @@ import {
 
 import { SSE } from "sse.js";
 
-const SUPABASE_URL = "https://xguihxuzqibwxjnimxev.supabase.co/";
-const SUPABASE_API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhndWloeHV6cWlid3hqbmlteGV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUwOTQ4MzUsImV4cCI6MTk5MDY3MDgzNX0.0PMlOxtKL4O9GGZuAP_Xl4f-Tut1qOnW4bNEmAtoB8w";
-
 type CreateChatCompletionResponseChoicesInnerDelta = Omit<
   CreateChatCompletionResponseChoicesInner,
   "message"
 > & {
   delta: Partial<ChatCompletionResponseMessage>;
 };
-
-function getEdgeFunctionUrl() {
-  const supabaseUrl = SUPABASE_URL?.replace(/\/$/, "");
-
-  if (!supabaseUrl) {
-    return undefined;
-  }
-
-  // https://github.com/supabase/supabase-js/blob/10d3423506cbd56345f7f6ab2ec2093c8db629d4/src/SupabaseClient.ts#L96
-  const isPlatform = supabaseUrl.match(/(supabase\.co)|(supabase\.in)/);
-
-  if (isPlatform) {
-    const [schemeAndProjectId, domain, tld] = supabaseUrl.split(".");
-    return `${schemeAndProjectId}.functions.${domain}.${tld}`;
-  } else {
-    return `${supabaseUrl}/functions/v1`;
-  }
-}
-
-const edgeFunctionUrl = getEdgeFunctionUrl();
 
 export enum MessageRole {
   User = "user",
@@ -92,7 +68,7 @@ type MessageAction =
   | ResetAction;
 
 function messageReducer(state: Message[], messageAction: MessageAction) {
-  let current = [...state];
+  let current = structuredClone(state);
   const { type } = messageAction;
 
   switch (type) {
@@ -146,8 +122,6 @@ export function useAiChat({
 
   const submit = useCallback(
     async (query: string) => {
-      if (!edgeFunctionUrl) return console.error("No edge function url");
-
       dispatchMessage({
         type: "new",
         message: {
@@ -168,10 +142,8 @@ export function useAiChat({
       setHasError(false);
       setIsLoading?.(true);
 
-      const eventSource = new SSE(`${edgeFunctionUrl}/ai-docs`, {
+      const eventSource = new SSE(`http://localhost:8000/api/v1/chat/stream`, {
         headers: {
-          apikey: SUPABASE_API_KEY,
-          Authorization: `Bearer ${SUPABASE_API_KEY}`,
           "Content-Type": "application/json",
         },
         payload: JSON.stringify({
@@ -248,7 +220,7 @@ export function useAiChat({
 
       setIsLoading?.(true);
     },
-    [currentMessageIndex, messages, messageTemplate, setIsLoading]
+    [currentMessageIndex, messages, messageTemplate]
   );
 
   function reset() {
