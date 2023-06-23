ec79825357c1f1d3c5a0b97228b132f03d639bf2
Florian Dieminger <me@fiji-flo.de>
2023-06-23 16:47:52 +0200

    fix(playground): console scroll to bottom

--- client/src/playground/console.tsx
+++ client/src/playground/console.tsx
@@ -1,13 +1,22 @@
+import { useEffect, useRef } from "react";
+
 export interface VConsole {
   prop: string;
   message: string;
 }
 
 export function Console({ vConsole }: { vConsole: VConsole[] }) {
+  const consoleUl = useRef<HTMLUListElement | null>(null);
+  const scrollToBottom = () => {
+    consoleUl.current?.scrollTo({ top: consoleUl.current?.scrollHeight });
+  };
+  useEffect(() => {
+    scrollToBottom();
+  }, [vConsole]);
   return (
     <div id="play-console">
       <span>Console</span>
-      <ul>
+      <ul ref={consoleUl}>
         {vConsole.map(({ prop, message }, i) => {
           return (
             <li key={i}>
