b0ef9a46a2c7150621416a0d77623d1fbab8047c
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-07-17 06:16:14 -0700

    Improve writer's homepage and sitemap

--- client/src/writers-homepage/index.tsx
+++ client/src/writers-homepage/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { MainContentContainer } from "../ui/atoms/page-content";
 import { Search } from "../ui/molecules/search";
+import Mandala from "../ui/molecules/mandala";
 import { useIsServer, useLocale } from "../hooks";
 import "./index.scss";
 
@@ -14,96 +15,112 @@ export default function WritersHomepage() {
 
   return (
     <MainContentContainer>
-      <div className="container" id="writers-homepage">
-        <h2>Writer's home page</h2>
+      <div id="writers-homepage">
+        <div className="homepage-hero dark">
+          <section>
+            <h1>
+              MDN <u>Content Writer</u> Homepage
+            </h1>
+            <p>
+              Welcome, content writer! This is your very own homepage, which
+              includes links to various tools, recently viewed documents and
+              sample documents.
+            </p>
+            <Search id="writers-hp-search" isHomepageSearch={true} />
+          </section>
+          <Mandala rotate={true} extraClasses="homepage-hero-bg" />
+        </div>
 
-        <Search id="writers-hp" />
+        <div className="container">
+          <h2>Tools</h2>
+          <ul id="tools">
+            <li>
+              <Link to={`/${locale}/_flaws`}>Flaws Dashboard</Link>
+            </li>
+            <li>
+              <Link to={`/${locale}/_sitemap`}>Sitemap</Link>
+            </li>
+            <li>
+              <Link to={`/${locale}/_translations`}>Translations</Link>
+            </li>
+          </ul>
 
-        {!isServer && (
-          <React.Suspense fallback={null}>
-            <ViewedDocuments />
-          </React.Suspense>
-        )}
+          <h2>Documents</h2>
 
-        <h3>Sample pages</h3>
-        <ul>
-          <li>
-            <Link to="/en-US/docs/MDN/Kitchensink">
-              The Kitchensink (en-US)
-            </Link>
-          </li>
-          <li>
-            <Link to={`/${locale}/docs/Web/HTML`}>Web/HTML index</Link>
-            <ul>
-              <li>
-                <Link to={`/${locale}/docs/Web/HTML/Element/video`}>
-                  HTML/video
-                </Link>
-              </li>
-            </ul>
-          </li>
+          {!isServer && (
+            <React.Suspense fallback={null}>
+              <ViewedDocuments />
+            </React.Suspense>
+          )}
 
-          <li>
-            <Link to={`/${locale}/docs/Web/API`}>Web/API index</Link>
-            <ul>
-              <li>
-                <Link to={`/${locale}/docs/Web/API/Fetch_API/Using_Fetch`}>
-                  Using Fetch API
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link to={`/${locale}/docs/Web/CSS`}>Web/CSS index</Link>
-            <ul>
-              <li>
-                <Link to={`/${locale}/docs/Web/CSS/Specificity`}>
-                  CSS Specificity
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link to={`/${locale}/docs/Web/JavaScript`}>
-              Web/JavaScript index
-            </Link>
-            <ul>
-              <li>
-                <Link
-                  to={`/${locale}/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach`}
-                >
-                  Array.prototype.forEach()
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link
-              to={`/${locale}/docs/Mozilla/Add-ons/WebExtensions/Browser_support_for_JavaScript_APIs`}
-            >
-              Page with lots of BCD tables
-            </Link>
-          </li>
-          <li>
-            <Link
-              to={`/${locale}/docs/Web/API/Document/#Browser_compatibility`}
-            >
-              Largest BCD table
-            </Link>
-          </li>
-        </ul>
-        <h3>Tools</h3>
-        <ul>
-          <li>
-            <Link to={`/${locale}/_translations`}>Translations</Link>
-          </li>
-          <li>
-            <Link to={`/${locale}/_sitemap`}>Sitemap</Link>
-          </li>
-          <li>
-            <Link to={`/${locale}/_flaws`}>Flaws Dashboard</Link>
-          </li>
-        </ul>
+          <h3>Sample pages</h3>
+          <ul id="sample-pages">
+            <li>
+              <Link to="/en-US/docs/MDN/Kitchensink">
+                The Kitchensink (en-US)
+              </Link>
+            </li>
+            <li>
+              <Link to={`/${locale}/docs/Web/HTML`}>Web/HTML index</Link>
+              <ul>
+                <li>
+                  <Link to={`/${locale}/docs/Web/HTML/Element/video`}>
+                    HTML/video
+                  </Link>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <Link to={`/${locale}/docs/Web/API`}>Web/API index</Link>
+              <ul>
+                <li>
+                  <Link to={`/${locale}/docs/Web/API/Fetch_API/Using_Fetch`}>
+                    Using Fetch API
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to={`/${locale}/docs/Web/CSS`}>Web/CSS index</Link>
+              <ul>
+                <li>
+                  <Link to={`/${locale}/docs/Web/CSS/Specificity`}>
+                    CSS Specificity
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to={`/${locale}/docs/Web/JavaScript`}>
+                Web/JavaScript index
+              </Link>
+              <ul>
+                <li>
+                  <Link
+                    to={`/${locale}/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach`}
+                  >
+                    Array.prototype.forEach()
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link
+                to={`/${locale}/docs/Mozilla/Add-ons/WebExtensions/Browser_support_for_JavaScript_APIs`}
+              >
+                Page with lots of BCD tables
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={`/${locale}/docs/Web/API/Document/#Browser_compatibility`}
+              >
+                Largest BCD table
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </MainContentContainer>
   );
