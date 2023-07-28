d5e91c67711da63c873c12034868605e79e9cd83
Leo McArdle <lmcardle@mozilla.com>
2023-07-28 14:26:43 +0000

    chore(docs/learn): add user research survey banner
    https://mozilla-hub.atlassian.net/browse/MP-538

--- client/src/document/index.tsx
+++ client/src/document/index.tsx
@@ -45,6 +45,7 @@ import { useIncrementFrequentlyViewed } from "../plus/collections/frequently-vie
 import { useInteractiveExamplesActionHandler as useInteractiveExamplesTelemetry } from "../telemetry/interactive-examples";
 import { SidePlacement } from "../ui/organisms/placement";
 import { BaselineIndicator } from "./baseline-indicator";
+import { UserResearchSurvey } from "../ui/molecules/user-research-survey";
 // import { useUIStatus } from "../ui-context";
 
 // Lazy sub-components
@@ -257,6 +258,7 @@ export function Document(props /* TODO: define a TS interface for this */) {
             </React.Suspense>
           )}
           <article className="main-page-content" lang={doc.locale}>
+            <UserResearchSurvey doc={doc} />
             <header>
               <h1>{doc.title}</h1>
               {doc.baseline && <BaselineIndicator status={doc.baseline} />}
