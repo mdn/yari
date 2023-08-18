e9f49cd51f9ef50cca3da5d69bf3e596d9715424
Florian Dieminger <me@fiji-flo.de>
2023-08-18 15:59:47 +0200

    feat(ai-help): add syntax highlighting for code examples (#9510)
    Uses prism for syntax highlighting.
    
    Co-authored-by: Claas Augner <caugner@mozilla.com>

--- client/src/plus/ai-help/index.tsx
+++ client/src/plus/ai-help/index.tsx
@@ -1,4 +1,12 @@
-import { MutableRefObject, useEffect, useRef, useState } from "react";
+import Prism from "prismjs";
+import {
+  Children,
+  MutableRefObject,
+  ReactElement,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
@@ -219,6 +227,57 @@ export function AIHelpInner() {
                             }
                             // eslint-disable-next-line jsx-a11y/anchor-has-content
                             return <a {...props} />;
+                          },
+                          pre: ({ node, className, children, ...props }) => {
+                            const code = Children.toArray(children)
+                              .map(
+                                (child) =>
+                                  /language-(\w+)/.exec(
+                                    (child as ReactElement)?.props?.className ||
+                                      ""
+                                  )?.[1]
+                              )
+                              .find(Boolean);
+
+                            if (!code) {
+                              return (
+                                <pre {...props} className={className}>
+                                  {children}
+                                </pre>
+                              );
+                            }
+                            return (
+                              <div className="code-example">
+                                <p className="example-header">
+                                  <span className="language-name">{code}</span>
+                                </p>
+                                <pre className={`brush: ${code}`}>
+                                  {children}
+                                </pre>
+                              </div>
+                            );
+                          },
+                          code: ({ inline, className, children, ...props }) => {
+                            const match = /language-(\w+)/.exec(
+                              className || ""
+                            );
+                            const lang = Prism.languages[match?.[1]];
+                            return !inline && lang ? (
+                              <code
+                                {...props}
+                                className={className}
+                                dangerouslySetInnerHTML={{
+                                  __html: Prism.highlight(
+                                    String(children),
+                                    lang
+                                  ),
+                                }}
+                              />
+                            ) : (
+                              <code {...props} className={className}>
+                                {children}
+                              </code>
+                            );
                           },
                         }}
                       >
