eecae0cc595d46350f8382531a2d0c27f31651e3
Florian Dieminger <me@fiji-flo.de>
2023-09-11 14:30:07 +0200

    "fix" test

--- client/src/document/index.test.tsx
+++ client/src/document/index.test.tsx
@@ -1,10 +1,35 @@
 import { Route, Routes, MemoryRouter } from "react-router-dom";
-import React from "react";
 import { render, waitFor } from "@testing-library/react";
 
 import { Document } from "./index";
 
 declare var global: Window;
+
+// TODO: move this somewhere better once we have more use cases.
+class IntersectionObserver {
+  readonly root: Element | null;
+
+  readonly rootMargin: string;
+
+  readonly thresholds: ReadonlyArray<number>;
+
+  constructor() {
+    this.root = null;
+    this.rootMargin = "";
+    this.thresholds = [];
+  }
+
+  disconnect() {}
+
+  observe() {}
+
+  takeRecords(): IntersectionObserverEntry[] {
+    return [];
+  }
+
+  unobserve() {}
+}
+window.IntersectionObserver = IntersectionObserver;
 
 const sampleDocumentState = Object.freeze({
   doc: Object.freeze({
