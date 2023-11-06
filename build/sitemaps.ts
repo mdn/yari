8318587f01dcdd1d59c8fec39f07150195370ad8
Claas Augner <caugner@mozilla.com>
2023-11-06 16:07:03 +0100

    refactor(build/sitemaps): avoid reusing parameter variable

--- build/sitemaps.ts
+++ build/sitemaps.ts
@@ -2,14 +2,13 @@ export function makeSitemapXML(
   locale: string,
   docs: { slug: string; modified: string }[]
 ) {
-  docs = docs.slice();
-  docs.sort((a, b) => a.slug.localeCompare(b.slug));
+  const sortedDocs = docs.slice().sort((a, b) => a.slug.localeCompare(b.slug));
 
   // Based on https://support.google.com/webmasters/answer/183668?hl=en
   return [
     '<?xml version="1.0" encoding="UTF-8"?>',
     '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
-    ...docs.map((doc) => {
+    ...sortedDocs.map((doc) => {
       const loc = `<loc>https://developer.mozilla.org/${locale}/docs/${doc.slug}</loc>`;
       const modified = doc.modified
         ? `<lastmod>${doc.modified.toString().split("T")[0]}</lastmod>`
@@ -21,18 +20,17 @@ export function makeSitemapXML(
   ].join("\n");
 }
 
-export function makeSitemapIndexXML(pathnames: string[]) {
-  pathnames = pathnames.slice();
-  pathnames.sort();
+export function makeSitemapIndexXML(paths: string[]) {
+  const sortedPaths = paths.slice().sort();
 
   // Based on https://support.google.com/webmasters/answer/75712
   return [
     '<?xml version="1.0" encoding="UTF-8"?>',
     '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
-    ...pathnames.map((pathname) => {
+    ...sortedPaths.map((path) => {
       return (
         "<sitemap>" +
-        `<loc>https://developer.mozilla.org${pathname}</loc>` +
+        `<loc>https://developer.mozilla.org${path}</loc>` +
         `<lastmod>${new Date().toISOString().split("T")[0]}</lastmod>` +
         "</sitemap>"
       );
