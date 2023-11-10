b1baa078fb979a9a045fbf3f4e05f738d2cefb77
Claas Augner <caugner@mozilla.com>
2023-11-10 21:09:38 +0100

    fixup! chore(telemetry): load Sentry on demand

--- client/src/telemetry/sentry.ts
+++ client/src/telemetry/sentry.ts
@@ -1,21 +1,34 @@
-import { SENTRY_ENVIRONMENT, SENTRY_RELEASE } from "../env";
+import { SENTRY_DSN, SENTRY_ENVIRONMENT, SENTRY_RELEASE } from "../env";
+
+let sentryPromise: Promise<any> | null = null;
+
+function loadSentry(): Promise<any> {
+  if (!sentryPromise) {
+    sentryPromise = import(
+      /* webpackChunkName: "sentry" */ "@sentry/react"
+    ).then((Sentry) => {
+      Sentry.init({
+        dsn: SENTRY_DSN,
+        release: SENTRY_RELEASE,
+        environment: SENTRY_ENVIRONMENT || "dev",
+      });
+      return Sentry;
+    });
+  }
+  return sentryPromise;
+}
 
 export function initSentry(dsn: string) {
-  let initSentry: Promise<any> | null = null;
+  let removeEventListener: (() => void) | null = null;
   const errorHandler = (event: ErrorEvent) => {
-    if (!initSentry) {
-      initSentry = import(
-        /* webpackChunkName: "sentry" */ "@sentry/react"
-      ).then((Sentry) => {
-        Sentry.init({
-          dsn,
-          release: SENTRY_RELEASE,
-          environment: SENTRY_ENVIRONMENT || "dev",
-        });
-        return Sentry;
-      });
-    }
-    initSentry.then((Sentry) => Sentry.captureException(event));
+    loadSentry().then((Sentry) => {
+      if (removeEventListener) {
+        removeEventListener();
+        removeEventListener = null;
+      }
+      Sentry.captureException(event);
+    });
   };
   window.addEventListener("error", errorHandler);
+  removeEventListener = () => window.removeEventListener("error", errorHandler);
 }
