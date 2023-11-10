7d326857481034a725703fdbbf4cf1bd7a24f36a
Claas Augner <caugner@mozilla.com>
2023-11-10 20:00:26 +0100

    chore(telemetry): load Sentry on demand

--- client/src/telemetry/sentry.ts
+++ client/src/telemetry/sentry.ts
@@ -1,20 +1,21 @@
 import { SENTRY_ENVIRONMENT, SENTRY_RELEASE } from "../env";
 
 export function initSentry(dsn: string) {
-  let events: ErrorEvent[] = [];
+  let initSentry: Promise<any> | null = null;
   const errorHandler = (event: ErrorEvent) => {
-    events.push(event);
+    if (!initSentry) {
+      initSentry = import(
+        /* webpackChunkName: "sentry" */ "@sentry/react"
+      ).then((Sentry) => {
+        Sentry.init({
+          dsn,
+          release: SENTRY_RELEASE,
+          environment: SENTRY_ENVIRONMENT || "dev",
+        });
+        return Sentry;
+      });
+    }
+    initSentry.then((Sentry) => Sentry.captureException(event));
   };
   window.addEventListener("error", errorHandler);
-  import(/* webpackChunkName: "sentry" */ "@sentry/react").then((Sentry) => {
-    Sentry.init({
-      dsn,
-      release: SENTRY_RELEASE,
-      environment: SENTRY_ENVIRONMENT || "dev",
-    });
-    window.removeEventListener("error", errorHandler);
-    for (const event of events) {
-      Sentry.captureException(event);
-    }
-  });
 }
