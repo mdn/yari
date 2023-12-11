5d7f740a50feb9bb026d8055b1c560e5def267c6
Claas Augner <caugner@mozilla.com>
2023-12-11 17:59:51 +0100

    chore(ai-help): highlight GPT-4 usage

--- client/src/plus/ai-help/index.tsx
+++ client/src/plus/ai-help/index.tsx
@@ -127,7 +127,7 @@ function AIHelpAuthenticated() {
         <div className="ai-help-banner">
           <p>
             <Icon name="bell-ring" />
-            <strong>GPT-4 Turbo-powered AI Help.</strong>
+            <strong>GPT-4-powered AI Help.</strong>
           </p>
           <p>Now with chat history, enhanced context, and optimised prompts.</p>
           <p>This is a beta feature.</p>
