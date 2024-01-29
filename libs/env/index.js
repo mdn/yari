5ce3a65e320b6005973be804c597283b2ff002f2
Florian Dieminger <me@fiji-flo.de>
2024-01-29 22:07:07 +0100

    feat(scripts): use pg instead of supabase (#10337)
    * feat(scripts): use pg instead of supabase
    
    * fix error destructing
    
    * use pg uri to connect
    
    * log formatted updates

--- libs/env/index.js
+++ libs/env/index.js
@@ -168,6 +168,7 @@ export const FAKE_V1_API = JSON.parse(process.env.SERVER_FAKE_V1_API || false);
 // ----
 
 export const OPENAI_KEY = process.env.OPENAI_KEY || "";
+export const PG_URI = process.env.PG_URI || "";
 export const SUPABASE_URL = process.env.SUPABASE_URL || "";
 export const SUPABASE_SERVICE_ROLE_KEY =
   process.env.SUPABASE_SERVICE_ROLE_KEY || "";
