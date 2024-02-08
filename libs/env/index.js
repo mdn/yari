bacf52544c1529b5f640837a740038c8011ccd6b
Claas Augner <495429+caugner@users.noreply.github.com>
2024-02-08 16:51:48 +0100

    chore(ai-help): remove old index script (#10484)
    It is no longer used and caused a stage build to fail.

--- libs/env/index.js
+++ libs/env/index.js
@@ -169,9 +169,6 @@ export const FAKE_V1_API = JSON.parse(process.env.SERVER_FAKE_V1_API || false);
 
 export const OPENAI_KEY = process.env.OPENAI_KEY || "";
 export const PG_URI = process.env.PG_URI || "";
-export const SUPABASE_URL = process.env.SUPABASE_URL || "";
-export const SUPABASE_SERVICE_ROLE_KEY =
-  process.env.SUPABASE_SERVICE_ROLE_KEY || "";
 
 export const SAMPLE_SIGN_KEY = process.env.BUILD_SAMPLE_SIGN_KEY
   ? Buffer.from(process.env.BUILD_SAMPLE_SIGN_KEY, "base64")
