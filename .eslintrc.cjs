007c637a26e418f34d29f8fa1abcfb8aab6171e2
Claas Augner <caugner@mozilla.com>
2024-02-26 14:50:28 +0100

    chore(eslint): re-enable typescript-eslint/no-var-requires

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -55,7 +55,6 @@ module.exports = {
       ],
       rules: {
         "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-var-requires": "off",
         "@typescript-eslint/no-unused-vars": [
           "warn",
           { ignoreRestSiblings: true },
