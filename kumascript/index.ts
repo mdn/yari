5fd48005e3d9cac2ff0cb821eb69242fffcc5ce4
Florian Dieminger <me@fiji-flo.de>
2024-02-27 16:30:57 +0100

    feat(curriculum): integrate MDN Curriculum (#10433)
    Integrate the [MDN Curriculum](https://github.com/mdn/curriculum) repo.
    
    ---------
    
    Co-authored-by: Claas Augner <caugner@mozilla.com>
    Co-authored-by: Leo McArdle <lmcardle@mozilla.com>

--- kumascript/index.ts
+++ kumascript/index.ts
@@ -14,7 +14,7 @@ import {
   LIVE_SAMPLES_BASE_URL,
 } from "../libs/env/index.js";
 import { SourceCodeError } from "./src/errors.js";
-import { Doc } from "../libs/types/document.js";
+import { BuildData } from "../libs/types/document.js";
 export { buildLiveSamplePages } from "./src/live-sample.js";
 
 const DEPENDENCY_LOOP_INTRO =
@@ -37,7 +37,7 @@ export async function render(
     selective_mode = false,
     invalidateCache = false,
   }: RenderOptions = {},
-  doc?: Doc
+  doc?: BuildData
 ): Promise<[cheerio.CheerioAPI, SourceCodeError[], any]> {
   const urlLC = url.toLowerCase();
   if (renderCache.has(urlLC)) {
