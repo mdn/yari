082f822265a77da5ebbe9857e51f2d97b8b158c1
Kartik Soneji <kartiksoneji@rocketmail.com>
2024-03-01 18:10:07 +0530

    fix: run `tsc` in parallel for `check:tsc`

--- scripts/check-tsc.ts
+++ scripts/check-tsc.ts
@@ -2,21 +2,25 @@ import { execSync, type SpawnSyncReturns } from "node:child_process";
 import { findDirWithFileRecursive } from "./find.js";
 
 async function main(): Promise<void> {
-  for (const project of await findDirWithFileRecursive("tsconfig.json"))
-    try {
-      console.log(`🔄 ${project}`);
-      execSync("npx tsc --noEmit", {
-        cwd: project,
-        stdio: "inherit",
-      });
-      console.log(`☑️ ${project}`);
-    } catch (e: any) {
-      const { error } = e as SpawnSyncReturns<Buffer>;
-      console.error(
-        `error checking project ${project}: "${error?.message ?? ""}"`
-      );
-      process.exitCode = 255;
-    }
+  const projects = await findDirWithFileRecursive("tsconfig.json");
+  await Promise.all(
+    projects.map((project) => {
+      try {
+        console.log(`🔄 ${project}`);
+        execSync("npx tsc --noEmit", {
+          cwd: project,
+          stdio: "inherit",
+        });
+        console.log(`☑️ ${project}`);
+      } catch (e: any) {
+        const { error } = e as SpawnSyncReturns<Buffer>;
+        console.error(
+          `error checking project ${project}: "${error?.message ?? ""}"`
+        );
+        process.exitCode = 255;
+      }
+    })
+  );
 }
 
 main();
