8ae0a596dae052df78fbeaf7d5f81da8ed7c46f2
Claas Augner <caugner@mozilla.com>
2024-03-15 13:55:52 +0100

    fixup! test(flaws): test that fallback links are fixabln broken link flaws

--- testing/tests/index.test.ts
+++ testing/tests/index.test.ts
@@ -1617,14 +1617,14 @@ test("translated content broken links can fall back to en-us", () => {
   const map = new Map(doc.flaws.broken_links.map((x) => [x.href, x]));
   expect(map.get("/fr/docs/Web/CSS/dumber")).toMatchObject({
     explanation: "Can't resolve /fr/docs/Web/CSS/dumber",
-    suggestion: "/en-US/docs/Web/CSS/dumber",
+    suggestion: "/fr/docs/Web/CSS/dumber",
     fixable: true,
     line: 19,
     column: 16,
   });
   expect(map.get("/fr/docs/Web/CSS/number")).toMatchObject({
     explanation: "Can't resolve /fr/docs/Web/CSS/number",
-    suggestion: "/en-US/docs/Web/CSS/number",
+    suggestion: "/fr/docs/Web/CSS/number",
     fixable: true,
     line: 21,
     column: 14,
