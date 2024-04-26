264750f8329f07882ead3a7672fcb10548247ab6
Claas Augner <caugner@mozilla.com>
2024-04-26 16:12:35 +0200

    chore(l10n): remove periods

--- libs/l10n/l10n.ts
+++ libs/l10n/l10n.ts
@@ -18,13 +18,13 @@ function localString(strings: Strings) {
 }
 
 export const ONLY_AVAILABLE_IN_ENGLISH = localString({
-  "en-US": "This page is currently only available in English.",
-  es: "Esta página está disponible solo en inglés.",
-  fr: "Cette page est actuellement disponible uniquement en anglais.",
+  "en-US": "This page is currently only available in English",
+  es: "Esta página está disponible solo en inglés",
+  fr: "Cette page est actuellement disponible uniquement en anglais",
   ja: "このページは現在、英語のみで利用可能です。",
-  ko: "이 페이지는 현재 영어로만 제공됩니다.",
-  "pt-BR": "Esta página está disponível apenas em inglês no momento.",
+  ko: "이 페이지는 현재 영어로만 제공됩니다",
+  "pt-BR": "Esta página está disponível apenas em inglês no momento",
   ru: "В настоящее время эта страница доступна только на английском языке",
-  "zh-CN": "此页面目前仅提供英文版本。",
-  "zh-TW": "此頁面目前僅提供英文版本。",
+  "zh-CN": "此页面目前仅提供英文版本",
+  "zh-TW": "此頁面目前僅提供英文版本",
 });
