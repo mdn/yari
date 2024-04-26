d73040ebeaa0910cba8bd1d45d8920334ca21dbe
Claas Augner <caugner@mozilla.com>
2024-04-26 16:26:59 +0200

    fixup! chore(l10n): remove periods

--- testing/tests/index.test.ts
+++ testing/tests/index.test.ts
@@ -1633,7 +1633,7 @@ test("translated content broken links can fall back to en-us", () => {
   expect($('article a[href="/en-US/docs/Web/CSS/number"]')).toHaveLength(2);
   expect($("article a.only-in-en-us")).toHaveLength(2);
   expect($("article a.only-in-en-us").attr("title")).toBe(
-    "Cette page est actuellement disponible uniquement en anglais."
+    "Cette page est actuellement disponible uniquement en anglais"
   );
 });
 
