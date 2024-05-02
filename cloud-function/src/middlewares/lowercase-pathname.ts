0aab2e63eda05b1c933d48cb24d9843d4e200b0f
Claas Augner <caugner@mozilla.com>
2024-05-03 00:41:34 +0200

    fixup! chore(cloud-function): migrate `url.parse()` to `new URL()`

--- cloud-function/src/middlewares/lowercase-pathname.ts
+++ cloud-function/src/middlewares/lowercase-pathname.ts
@@ -1,5 +1,3 @@
-import * as url from "node:url";
-
 import { NextFunction, Request, Response } from "express";
 
 export async function lowercasePathname(
@@ -7,10 +5,10 @@ export async function lowercasePathname(
   _res: Response,
   next: NextFunction
 ) {
-  const urlParsed = new URL(req.url);
+  const urlParsed = new URL(req.url, `${req.protocol}://${req.headers.host}`);
   if (urlParsed.pathname) {
     urlParsed.pathname = urlParsed.pathname.toLowerCase();
-    req.url = url.format(urlParsed);
+    req.url = urlParsed.toString();
     // Workaround for http-proxy-middleware v2 using `req.originalUrl`.
     // See: https://github.com/chimurai/http-proxy-middleware/pull/731
     req.originalUrl = req.url;
--- cloud-function/src/middlewares/resolve-runner-html.ts
+++ cloud-function/src/middlewares/resolve-runner-html.ts
@@ -1,5 +1,3 @@
-import * as url from "node:url";
-
 import { NextFunction, Request, Response } from "express";
 
 export async function resolveRunnerHtml(
@@ -7,10 +5,10 @@ export async function resolveRunnerHtml(
   _res: Response,
   next: NextFunction
 ) {
-  const urlParsed = new URL(req.url);
+  const urlParsed = new URL(req.url, `${req.protocol}://${req.headers.host}`);
   if (urlParsed.pathname && urlParsed.pathname.endsWith("/runner.html")) {
     urlParsed.pathname = "/runner.html";
-    req.url = url.format(urlParsed);
+    req.url = urlParsed.toString();
     // Workaround for http-proxy-middleware v2 using `req.originalUrl`.
     // See: https://github.com/chimurai/http-proxy-middleware/pull/731
     req.originalUrl = req.url;
