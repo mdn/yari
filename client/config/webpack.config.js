d83555c29ea05419ae4a6850c24f7d0fb8545038
Leo McArdle <lmcardle@mozilla.com>
2024-05-08 10:04:54 +0000

    chore(webpack): merge avif config into config for other images
    image/avif has been in mime-db since 2020: https://github.com/jshttp/mime-db/blob/master/HISTORY.md#1450--2020-09-22

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -329,15 +329,8 @@ function config(webpackEnv) {
           // match the requirements. When no loader matches it will fall
           // back to the "file" loader at the end of the loader list.
           oneOf: [
-            // TODO: Merge this config once `image/avif` is in the mime-db
-            // https://github.com/jshttp/mime-db
             {
-              test: [/\.avif$/],
-              type: "asset/resource",
-              mimetype: "image/avif",
-            },
-            {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+              test: [/\.avif$/, /\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
               type: "asset/resource",
             },
             {
