1418f82db09bc9cd42b0264265db0331174017ee
Leo McArdle <lmcardle@mozilla.com>
2024-05-09 10:27:42 +0000

    chore(webpack): remove react-native-web specific config options

--- client/config/paths.js
+++ client/config/paths.js
@@ -24,19 +24,7 @@ const publicUrlOrPath = getPublicUrlOrPath(
 
 const buildPath = process.env.BUILD_PATH || "build";
 
-const moduleFileExtensions = [
-  "web.mjs",
-  "mjs",
-  "web.js",
-  "js",
-  "web.ts",
-  "ts",
-  "web.tsx",
-  "tsx",
-  "json",
-  "web.jsx",
-  "jsx",
-];
+const moduleFileExtensions = ["mjs", "js", "ts", "tsx", "json", "jsx"];
 
 // Resolve file paths in the same order as webpack
 const resolveModule = (resolveFn, filePath) => {
--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -266,13 +266,8 @@ function config(webpackEnv) {
       // We also include JSX as a common component filename extension to support
       // some tools, although we do not recommend using it, see:
       // https://github.com/facebook/create-react-app/issues/290
-      // `web` extension prefixes have been added for better support
-      // for React Native Web.
       extensions: paths.moduleFileExtensions.map((ext) => `.${ext}`),
       alias: {
-        // Support React Native Web
-        // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-        "react-native": "react-native-web",
         // Allows for better profiling with ReactDevTools
         ...(isEnvProductionProfile && {
           "react-dom$": "react-dom/profiling",
