f844aabc6cb1392cd24e6eaabbc96fccde57d1c8
Claas Augner <caugner@mozilla.com>
2024-05-15 20:43:00 -0700

    fix(robots): remove /search from robots.txt

--- tool/build-robots-txt.ts
+++ tool/build-robots-txt.ts
@@ -24,12 +24,6 @@ Disallow: /
 `;
 
 export async function runBuildRobotsTxt(outfile: string) {
-  let content = ALWAYS_ALLOW_ROBOTS ? ALLOW_TEXT : DISALLOW_TEXT;
-  if (ALWAYS_ALLOW_ROBOTS) {
-    // Append extra lines specifically when we do allow robots.
-    for (const locale of VALID_LOCALES.values()) {
-      content += `Disallow: /${locale}/search\n`;
-    }
-  }
+  const content = ALWAYS_ALLOW_ROBOTS ? ALLOW_TEXT : DISALLOW_TEXT;
   fs.writeFileSync(outfile, `${content.trim()}\n`, "utf-8");
 }
