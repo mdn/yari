2407b24f90284191500896bdda696d0fec9f57d4
Claas Augner <caugner@mozilla.com>
2024-05-20 10:01:25 -0400

    fix(sentry): specify imports
    Sentry v8 no longer provides a default export.

--- build/sentry.ts
+++ build/sentry.ts
@@ -1,14 +1,14 @@
-import Sentry from "@sentry/node";
+import { init, captureConsoleIntegration, NodeOptions } from "@sentry/node";
 
 export function initSentry(
   dsn: string,
-  options?: Omit<Sentry.NodeOptions, "dsn" | "environment" | "integrations">
+  options?: Omit<NodeOptions, "dsn" | "environment" | "integrations">
 ) {
-  Sentry.init({
+  init({
     dsn,
     environment: process.env.SENTRY_ENVIRONMENT || "dev",
     integrations: [
-      Sentry.captureConsoleIntegration({ levels: ["warn", "error", "assert"] }),
+      captureConsoleIntegration({ levels: ["warn", "error", "assert"] }),
     ],
     tracesSampleRate: 1.0,
     ...options,
