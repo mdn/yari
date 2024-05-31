8af2993108effc1116a78cf95ced2798202c8e13
Claas Augner <caugner@mozilla.com>
2024-05-31 11:19:18 +0200

    test(redirects): remove obsolete cases

--- testing/tests/redirects.test.ts
+++ testing/tests/redirects.test.ts
@@ -27,18 +27,6 @@ function url_test(from, to, { statusCode = 301 } = {}) {
 }
 
 const SCL3_REDIRECT_URLS = [].concat(
-  url_test("/media/redesign/css/foo-min.css", "/static/build/styles/foo.css"),
-  url_test("/media/css/foo-min.css", "/static/build/styles/foo.css"),
-  url_test("/media/redesign/js/foo-min.js", "/static/build/js/foo.js"),
-  url_test("/media/js/foo-min.js", "/static/build/js/foo.js"),
-  url_test("/media/redesign/img.foo", "/static/img.foo"),
-  url_test("/media/img.foo", "/static/img.foo"),
-  url_test("/media/redesign/css.foo", "/static/styles.foo"),
-  url_test("/media/css.foo", "/static/styles.foo"),
-  url_test("/media/redesign/js.foo", "/static/js.foo"),
-  url_test("/media/js.foo", "/static/js.foo"),
-  url_test("/media/redesign/fonts.foo", "/static/fonts.foo"),
-  url_test("/media/fonts.foo", "/static/fonts.foo"),
   url_test(
     "/media/uploads/demos/foobar123",
     "/docs/Web/Demos_of_open_web_technologies/",
@@ -234,14 +222,6 @@ const SCL3_REDIRECT_URLS = [].concat(
     "/en/demos/detail/bananabread",
     "https://github.com/kripken/BananaBread/"
   ),
-  url_test(
-    "/en-US/demos/detail/bananabread/launch",
-    "https://kripken.github.io/BananaBread/cube2/index.html"
-  ),
-  url_test(
-    "/en/demos/detail/bananabread/launch",
-    "https://kripken.github.io/BananaBread/cube2/index.html"
-  ),
   url_test("/en-US/demos", "/en-US/docs/Web/Demos_of_open_web_technologies"),
   url_test("/en/demos", "/en/docs/Web/Demos_of_open_web_technologies"),
   url_test(
@@ -258,233 +238,6 @@ const SCL3_REDIRECT_URLS = [].concat(
   url_test(
     "/Web/JavaScript/Reference/Global_Objects/Array/from",
     "/docs/Web/JavaScript/Reference/Global_Objects/Array/from"
-  )
-);
-
-const GITHUB_IO_URLS = [].concat(
-  url_test(
-    "/samples/raycaster/input.js",
-    "http://mdn.github.io/canvas-raycaster/input.js"
-  ),
-  url_test(
-    "/samples/raycaster/Level.js",
-    "http://mdn.github.io/canvas-raycaster/Level.js"
-  ),
-  url_test(
-    "/samples/raycaster/Player.js",
-    "http://mdn.github.io/canvas-raycaster/Player.js"
-  ),
-  url_test(
-    "/samples/raycaster/RayCaster.html",
-    "http://mdn.github.io/canvas-raycaster/index.html"
-  ),
-  url_test(
-    "/samples/raycaster/RayCaster.js",
-    "http://mdn.github.io/canvas-raycaster/RayCaster.js"
-  ),
-  url_test(
-    "/samples/raycaster/trace.css",
-    "http://mdn.github.io/canvas-raycaster/trace.css"
-  ),
-  url_test(
-    "/samples/raycaster/trace.js",
-    "http://mdn.github.io/canvas-raycaster/trace.js"
-  ),
-  url_test(
-    "/samples/webgl/sample1",
-    "http://mdn.github.io/webgl-examples/tutorial/sample1"
-  ),
-  url_test(
-    "/samples/webgl/sample1/index.html",
-    "http://mdn.github.io/webgl-examples/tutorial/sample1/index.html"
-  ),
-  url_test(
-    "/samples/webgl/sample1/webgl-demo.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sample1/webgl-demo.js"
-  ),
-  url_test(
-    "/samples/webgl/sample1/webgl.css",
-    "http://mdn.github.io/webgl-examples/tutorial/webgl.css"
-  ),
-  url_test(
-    "/samples/webgl/sample2",
-    "http://mdn.github.io/webgl-examples/tutorial/sample2"
-  ),
-  url_test(
-    "/samples/webgl/sample2/glUtils.js",
-    "http://mdn.github.io/webgl-examples/tutorial/glUtils.js"
-  ),
-  url_test(
-    "/samples/webgl/sample2/index.html",
-    "http://mdn.github.io/webgl-examples/tutorial/sample2/index.html"
-  ),
-  url_test(
-    "/samples/webgl/sample2/sylvester.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sylvester.js"
-  ),
-  url_test(
-    "/samples/webgl/sample2/webgl-demo.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sample2/webgl-demo.js"
-  ),
-  url_test(
-    "/samples/webgl/sample2/webgl.css",
-    "http://mdn.github.io/webgl-examples/tutorial/webgl.css"
-  ),
-  url_test(
-    "/samples/webgl/sample3",
-    "http://mdn.github.io/webgl-examples/tutorial/sample3"
-  ),
-  url_test(
-    "/samples/webgl/sample3/glUtils.js",
-    "http://mdn.github.io/webgl-examples/tutorial/glUtils.js"
-  ),
-  url_test(
-    "/samples/webgl/sample3/index.html",
-    "http://mdn.github.io/webgl-examples/tutorial/sample3/index.html"
-  ),
-  url_test(
-    "/samples/webgl/sample3/sylvester.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sylvester.js"
-  ),
-  url_test(
-    "/samples/webgl/sample3/webgl-demo.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sample3/webgl-demo.js"
-  ),
-  url_test(
-    "/samples/webgl/sample3/webgl.css",
-    "http://mdn.github.io/webgl-examples/tutorial/webgl.css"
-  ),
-  url_test(
-    "/samples/webgl/sample4",
-    "http://mdn.github.io/webgl-examples/tutorial/sample4"
-  ),
-  url_test(
-    "/samples/webgl/sample4/glUtils.js",
-    "http://mdn.github.io/webgl-examples/tutorial/glUtils.js"
-  ),
-  url_test(
-    "/samples/webgl/sample4/index.html",
-    "http://mdn.github.io/webgl-examples/tutorial/sample4/index.html"
-  ),
-  url_test(
-    "/samples/webgl/sample4/sylvester.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sylvester.js"
-  ),
-  url_test(
-    "/samples/webgl/sample4/webgl-demo.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sample4/webgl-demo.js"
-  ),
-  url_test(
-    "/samples/webgl/sample4/webgl.css",
-    "http://mdn.github.io/webgl-examples/tutorial/webgl.css"
-  ),
-  url_test(
-    "/samples/webgl/sample5",
-    "http://mdn.github.io/webgl-examples/tutorial/sample5"
-  ),
-  url_test(
-    "/samples/webgl/sample5/glUtils.js",
-    "http://mdn.github.io/webgl-examples/tutorial/glUtils.js"
-  ),
-  url_test(
-    "/samples/webgl/sample5/index.html",
-    "http://mdn.github.io/webgl-examples/tutorial/sample5/index.html"
-  ),
-  url_test(
-    "/samples/webgl/sample5/sylvester.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sylvester.js"
-  ),
-  url_test(
-    "/samples/webgl/sample5/webgl-demo.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sample5/webgl-demo.js"
-  ),
-  url_test(
-    "/samples/webgl/sample5/webgl.css",
-    "http://mdn.github.io/webgl-examples/tutorial/webgl.css"
-  ),
-  url_test(
-    "/samples/webgl/sample6",
-    "http://mdn.github.io/webgl-examples/tutorial/sample6"
-  ),
-  url_test(
-    "/samples/webgl/sample6/cubetexture.png",
-    "http://mdn.github.io/webgl-examples/tutorial/sample6/cubetexture.png"
-  ),
-  url_test(
-    "/samples/webgl/sample6/glUtils.js",
-    "http://mdn.github.io/webgl-examples/tutorial/glUtils.js"
-  ),
-  url_test(
-    "/samples/webgl/sample6/index.html",
-    "http://mdn.github.io/webgl-examples/tutorial/sample6/index.html"
-  ),
-  url_test(
-    "/samples/webgl/sample6/sylvester.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sylvester.js"
-  ),
-  url_test(
-    "/samples/webgl/sample6/webgl-demo.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sample6/webgl-demo.js"
-  ),
-  url_test(
-    "/samples/webgl/sample6/webgl.css",
-    "http://mdn.github.io/webgl-examples/tutorial/webgl.css"
-  ),
-  url_test(
-    "/samples/webgl/sample7",
-    "http://mdn.github.io/webgl-examples/tutorial/sample7"
-  ),
-  url_test(
-    "/samples/webgl/sample7/cubetexture.png",
-    "http://mdn.github.io/webgl-examples/tutorial/sample7/cubetexture.png"
-  ),
-  url_test(
-    "/samples/webgl/sample7/glUtils.js",
-    "http://mdn.github.io/webgl-examples/tutorial/glUtils.js"
-  ),
-  url_test(
-    "/samples/webgl/sample7/index.html",
-    "http://mdn.github.io/webgl-examples/tutorial/sample7/index.html"
-  ),
-  url_test(
-    "/samples/webgl/sample7/sylvester.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sylvester.js"
-  ),
-  url_test(
-    "/samples/webgl/sample7/webgl-demo.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sample7/webgl-demo.js"
-  ),
-  url_test(
-    "/samples/webgl/sample7/webgl.css",
-    "http://mdn.github.io/webgl-examples/tutorial/webgl.css"
-  ),
-  url_test(
-    "/samples/webgl/sample8",
-    "http://mdn.github.io/webgl-examples/tutorial/sample8"
-  ),
-  url_test(
-    "/samples/webgl/sample8/Firefox.ogv",
-    "http://mdn.github.io/webgl-examples/tutorial/sample8/Firefox.ogv"
-  ),
-  url_test(
-    "/samples/webgl/sample8/glUtils.js",
-    "http://mdn.github.io/webgl-examples/tutorial/glUtils.js"
-  ),
-  url_test(
-    "/samples/webgl/sample8/index.html",
-    "http://mdn.github.io/webgl-examples/tutorial/sample8/index.html"
-  ),
-  url_test(
-    "/samples/webgl/sample8/sylvester.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sylvester.js"
-  ),
-  url_test(
-    "/samples/webgl/sample8/webgl-demo.js",
-    "http://mdn.github.io/webgl-examples/tutorial/sample8/webgl-demo.js"
-  ),
-  url_test(
-    "/samples/webgl/sample8/webgl.css",
-    "http://mdn.github.io/webgl-examples/tutorial/webgl.css"
   )
 );
 
@@ -681,15 +434,6 @@ for (const [zoneRoot, wikiSlug, childPath, locales] of ZONE_REDIRECTS) {
   }
 }
 
-const REDIRECT_URLS = [].concat(
-  url_test(
-    "/en-US/fellowship",
-    "/en-US/docs/Archive/2015_MDN_Fellowship_Program"
-  )
-);
-
-const marionette_client_docs_url =
-  "https://marionette-client.readthedocs.io/en/latest/";
 const marionette_docs_root_url =
   "https://firefox-source-docs.mozilla.org/testing/marionette/marionette/";
 const marionette_locales = "{/en-US,/fr,/ja,/pl,/pt-BR,/ru,/zh-CN,}";
@@ -704,7 +448,6 @@ const MARIONETTE_URLS = [].concat(
     `${marionette_multi_base}/Builds`,
     `${marionette_docs_root_url}Building.html`
   ),
-  url_test(`${marionette_multi_base}/Client`, marionette_client_docs_url),
   url_test(
     `${marionette_multi_base}/Developer_setup`,
     `${marionette_docs_root_url}Contributing.html`
@@ -725,7 +468,6 @@ const MARIONETTE_URLS = [].concat(
     `${marionette_base}/Protocol`,
     `${marionette_docs_root_url}Protocol.html`
   ),
-  url_test(`${marionette_base}/Python_Client`, marionette_client_docs_url),
   url_test(
     `${marionette_base}/WebDriver/status`,
     "https://bugzilla.mozilla.org/showdependencytree.cgi?id=721859&hide_resolved=1"
@@ -883,21 +625,6 @@ const WEBEXT_URLS = [].concat(
       `{/en-US,/fr,}/docs/Mozilla/Add-ons/${aoPath}`,
       `https://extensionworkshop.com/documentation/${ewPath}`
     )
-  )
-);
-
-const FIREFOX_ACCOUNTS_URLS = [].concat(
-  url_test(
-    "{/en-US,}/{docs/,}Mozilla/Firefox_Accounts_OAuth_Dashboard{/,}",
-    "https://mozilla.github.io/ecosystem-platform/docs/welcome"
-  ),
-  url_test(
-    "{/en-US,/pl,}/{docs/,}Mozilla/{Tech/,}Firefox_Accounts{/Introduction,/Atttached_APIs,/WebExtensions,}",
-    "https://mozilla.github.io/ecosystem-platform/docs/welcome"
-  ),
-  url_test(
-    "{/en-US,/pl,}/{docs/,}Archive/Mozilla/Firefox/Accounts{/Introduction,/Atttached_APIs,/WebExtensions,}",
-    "https://mozilla.github.io/ecosystem-platform/docs/welcome"
   )
 );
 
@@ -1265,12 +992,6 @@ describe("scl3 redirects", () => {
   }
 });
 
-describe("github io redirects", () => {
-  for (const [url, t] of GITHUB_IO_URLS) {
-    it(url, t);
-  }
-});
-
 describe("default samples redirects", () => {
   for (const [url, t] of DEFAULT_SAMPLES_URLS) {
     it(url, t);
@@ -1289,12 +1010,6 @@ describe("zone redirects", () => {
   }
 });
 
-describe("one off redirects", () => {
-  for (const [url, t] of REDIRECT_URLS) {
-    it(url, t);
-  }
-});
-
 describe("marionette redirects", () => {
   for (const [url, t] of MARIONETTE_URLS) {
     it(url, t);
@@ -1303,12 +1018,6 @@ describe("marionette redirects", () => {
 
 describe("webext redirects", () => {
   for (const [url, t] of WEBEXT_URLS) {
-    it(url, t);
-  }
-});
-
-describe("firefox accounts redirects", () => {
-  for (const [url, t] of FIREFOX_ACCOUNTS_URLS) {
     it(url, t);
   }
 });
