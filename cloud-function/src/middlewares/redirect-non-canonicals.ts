af9cebd1baea9c8f6dc3c9674719525301505725
Claas Augner <caugner@mozilla.com>
2024-06-08 00:46:59 +0200

    fix(cloud-function): parse url with URL() + support query strings

--- cloud-function/src/middlewares/redirect-non-canonicals.ts
+++ cloud-function/src/middlewares/redirect-non-canonicals.ts
@@ -2,7 +2,6 @@ import { createRequire } from "node:module";
 
 import { NextFunction, Request, Response } from "express";
 
-import { decodePath } from "@yari-internal/slug-utils";
 import { THIRTY_DAYS } from "../constants.js";
 import { normalizeSlug, redirect } from "../utils.js";
 
@@ -15,28 +14,24 @@ export async function redirectNonCanonicals(
   res: Response,
   next: NextFunction
 ) {
-  // Important: The requestURI may be URI-encoded.
-  // Example:
-  // - Encoded: /zh-TW/docs/AJAX:%E4%B8%8A%E6%89%8B%E7%AF%87
-  // - Decoded: /zh-TW/docs/AJAX:上手篇
-  const decodedUri = decodePath(req.path);
-  const decodedUriLC = decodedUri.toLowerCase();
+  const parsedUrl = new URL(req.url, `${req.protocol}://${req.headers.host}/`);
+  const { pathname } = parsedUrl;
 
   // Redirect to canonical version.
   // Example:
   // - Source: /en-US/docs/web/guide/ajax/getting_started
   // - Target: /en-US/docs/Web/Guide/AJAX/Getting_Started
   for (const suffix of REDIRECT_SUFFIXES) {
-    if (!decodedUriLC.endsWith(suffix)) {
+    if (!pathname.endsWith(suffix)) {
       continue;
     }
-    const originalSource = decodedUriLC.substring(
+    const originalSource = pathname.substring(
       0,
-      decodedUriLC.length - suffix.length
+      pathname.length - suffix.length
     );
     const source = normalizeSlug(originalSource);
     if (typeof REDIRECTS[source] == "string" && REDIRECTS[source] !== source) {
-      const target = REDIRECTS[source] + suffix;
+      const target = REDIRECTS[source] + suffix + parsedUrl.search;
       return redirect(res, target, {
         status: 301,
         cacheControlSeconds: THIRTY_DAYS,
