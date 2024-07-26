f8a312e4f065a77182a4b69cc373af0136c88ab4
Leo McArdle <leo@mozilla.com>
2024-07-26 15:25:11 +0000

    fix(baseline): hide when feature status differs from BCD key status
    https://mozilla-hub.atlassian.net/browse/MP-874

--- build/web-features.ts
+++ build/web-features.ts
@@ -1,16 +1,37 @@
 import { features } from "web-features";
+import { computeBaseline } from "compute-baseline";
 
-export function getWebFeatureStatus(...bcdKeys: string[]) {
-  if (bcdKeys.length === 0) {
-    return;
+import type { Doc } from "../libs/types/document.js";
+
+export function addBaseline(doc: Partial<Doc>) {
+  if (doc.browserCompat && !doc.mdn_url?.includes("/docs/MDN/")) {
+    if (doc.browserCompat.length !== 1) {
+      return;
+    }
+
+    const key = doc.browserCompat[0];
+    const { featureStatus, keyStatus } = getStatuses(key);
+
+    if (!featureStatus) {
+      return;
+    }
+
+    if (featureStatus.baseline !== keyStatus.baseline) {
+      return;
+    }
+
+    return featureStatus;
   }
+}
 
+function getStatuses(key: string) {
   for (const feature of Object.values(features)) {
-    if (
-      feature.status &&
-      feature.compat_features?.some((feature) => bcdKeys.includes(feature))
-    ) {
-      return feature.status;
+    if (feature.status && feature.compat_features?.includes(key)) {
+      return {
+        featureStatus: feature.status,
+        keyStatus: computeBaseline({ compatKeys: [key] }),
+      };
     }
   }
+  return {};
 }
