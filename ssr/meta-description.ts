8fa63e755cb01a78a3508367ec5a99dd9021f025
Claas Augner <caugner@mozilla.com>
2024-08-21 01:38:23 +0200

    refactor(ssr): use Intl.ListFormat

--- ssr/meta-description.ts
+++ ssr/meta-description.ts
@@ -29,8 +29,12 @@ export function getMetaDescription(doc: Doc): string {
       .filter(([section]) => sections.includes(section))
       .map(([, text]) => text);
 
-    const syntaxContents = enumerate(...syntaxItems);
-    const otherContents = enumerate(...otherItems);
+    const listFormatter = new Intl.ListFormat("en", {
+      style: "long",
+      type: "conjunction",
+    });
+    const syntaxContents = listFormatter.format(syntaxItems);
+    const otherContents = listFormatter.format(otherItems);
     const contents = [
       syntaxContents ? `its ${syntaxContents}` : "",
       otherContents,
@@ -70,12 +74,4 @@ export function getMetaDescription(doc: Doc): string {
   }
 
   return doc.summary;
-}
-
-function enumerate(...items: string[]) {
-  items = items.filter(Boolean);
-
-  const lastItem = items.pop();
-
-  return [items.join(", "), lastItem].filter(Boolean).join(", and ");
 }
