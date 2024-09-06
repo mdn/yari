5943f3493c3e58debe12cedee793f33a7fe256cf
Leo McArdle <leo@mca.is>
2024-09-06 10:27:13 +0000

    fix(syntax-highlight): handle aliases and dependencies

--- client/src/document/highlight.ts
+++ client/src/document/highlight.ts
@@ -1,4 +1,15 @@
 import Prism from "prismjs";
+import components from "prismjs/components";
+
+const PRISM_LANGUAGES = components.languages as Record<
+  string,
+  {
+    alias?: string | string[];
+    require?: string | string[];
+    optional?: string | string[];
+    [key: string]: any;
+  }
+>;
 
 Prism.manual = true;
 
@@ -8,31 +19,78 @@ Prism.manual = true;
 // have to stick to the exact naming conventions that Prism uses
 // because Prism is an implementation detail.
 const ALIASES = new Map([
-  ["sh", "shell"],
   ["vue", "markup"], // See https://github.com/PrismJS/prism/issues/1665#issuecomment-536529608
+  ...Object.entries(PRISM_LANGUAGES).flatMap(([lang, config]) => {
+    if (config.alias) {
+      const aliases =
+        typeof config.alias === "string" ? [config.alias] : config.alias;
+      return aliases.map((alias) => [alias, lang] satisfies [string, string]);
+    }
+    return [];
+  }),
 ]);
 
 export async function highlightSyntax(element: Element, language: string) {
   const resolvedLanguage = ALIASES.get(language) || language;
 
-  let prismLanguage = Prism.languages[resolvedLanguage];
-  if (!prismLanguage) {
-    if (resolvedLanguage === "svelte") {
-      await import("prism-svelte");
-    } else {
-      try {
-        await import(
-          /* webpackChunkName: "prism" */
-          `prismjs/components/prism-${resolvedLanguage}.js`
-        );
-      } catch {
-        return;
-      }
-    }
+  try {
+    await importLanguage(resolvedLanguage);
+  } catch {
+    return;
   }
 
-  prismLanguage = Prism.languages[resolvedLanguage];
+  const prismLanguage = Prism.languages[resolvedLanguage];
   if (prismLanguage) {
     element.innerHTML = `<code>${Prism.highlight(element.textContent || "", prismLanguage, resolvedLanguage)}</code>`;
+  }
+}
+
+async function importLanguage(language: string) {
+  const prismLanguage = Prism.languages[language];
+
+  if (!prismLanguage) {
+    if (language === "svelte") {
+      try {
+        await import(
+          /* webpackChunkName: "prism-svelte" */
+          "prism-svelte"
+        );
+      } catch (e) {
+        console.warn(`Failed to import ${language} prism language`);
+        throw e;
+      }
+    } else {
+      const config = PRISM_LANGUAGES[language];
+      if (config.require) {
+        try {
+          await Promise.all(
+            (typeof config.require === "string"
+              ? [config.require]
+              : config.require
+            ).map((dependency) => importLanguage(dependency))
+          );
+        } catch {
+          return;
+        }
+      }
+      if (config.optional) {
+        await Promise.allSettled(
+          (typeof config.optional === "string"
+            ? [config.optional]
+            : config.optional
+          ).map((dependency) => importLanguage(dependency))
+        );
+      }
+      try {
+        await import(
+          /* webpackChunkName: "[request]" */
+          /* webpackExclude: /\.min\.js$/ */
+          `prismjs/components/prism-${language}.js`
+        );
+      } catch (e) {
+        console.warn(`Failed to import ${language} prism language`);
+        throw e;
+      }
+    }
   }
 }
