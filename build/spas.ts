dcb19675e5f29064c033adc9fc39e53519dcca65
Leo McArdle <leo@mca.is>
2024-09-30 11:09:05 +0000

    fix(community): avoid '| Contribute to MDN' title

--- build/spas.ts
+++ build/spas.ts
@@ -320,7 +320,9 @@ export async function buildSPAs(options: {
       };
       const context: HydrationData = {
         hyData,
-        pageTitle: `${frontMatter.attributes.title || ""} | ${title}`,
+        pageTitle: frontMatter.attributes.title
+          ? `${frontMatter.attributes.title} | ${title}`
+          : title,
         url,
       };
 
