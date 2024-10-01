3e07e84902a6d69663d148758b736c19b0e484c0
Leo McArdle <leo@mca.is>
2024-10-01 11:19:57 +0000

    wip(community): clip-path: inset(); doesn't work properly on webkit

--- client/src/community/contributor-list.ts
+++ client/src/community/contributor-list.ts
@@ -52,8 +52,32 @@ export class ContributorList extends LitElement {
       <div class="inner">
         <ul>
           <svg>
-            <ellipse rx="100%" ry="50%" cx="100%" cy="50%" />
-            <ellipse rx="50%" ry="25%" cx="100%" cy="50%" />
+            <defs>
+              <mask id="hide-half">
+                <rect
+                  x="0%"
+                  y="0%"
+                  width="100%"
+                  height="100%"
+                  fill="#fff"
+                  stroke="#fff"
+                />
+              </mask>
+            </defs>
+            <ellipse
+              rx="100%"
+              ry="50%"
+              cx="100%"
+              cy="50%"
+              mask="url(#hide-half)"
+            />
+            <ellipse
+              rx="50%"
+              ry="25%"
+              cx="100%"
+              cy="50%"
+              mask="url(#hide-half)"
+            />
           </svg>
           ${this._contributors.map(({ name, github, org }) => {
             const imgSrc = `https://avatars.githubusercontent.com/${github
