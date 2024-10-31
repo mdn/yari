45196cb870cdfac85ec93a3918a9d2358fafdae8
Claas Augner <caugner@mozilla.com>
2024-10-31 17:20:44 +0100

    refactor(libs): extract getRepositoryUrlByLocale()

--- build/index.ts
+++ build/index.ts
@@ -44,7 +44,7 @@ import {
   postProcessSmallerHeadingIDs,
 } from "./utils.js";
 import { addBaseline } from "./web-features.js";
-import { getRepositoryByLocale } from "../libs/locale-utils/index.js";
+import { getRepositoryUrlByLocale } from "../libs/locale-utils/index.js";
 export { default as SearchIndex } from "./search-index.js";
 export { gather as gatherGitHistory } from "./git-history.js";
 export { buildSPAs } from "./spas.js";
@@ -132,11 +132,10 @@ function injectNotecardOnWarnings($: cheerio.CheerioAPI) {
 /**
  * Return the full URL directly to the file in GitHub based on this folder.
  * @param {String} folder - the current folder we're processing.
- */
+ */ r;
 function getGitHubURL(root: string, folder: string, filename: string) {
   const [locale] = folder.split("/", 2);
-  const repo = getRepositoryByLocale(locale);
-  const baseURL = `https://github.com/mdn/${repo}`;
+  const baseURL = getRepositoryUrlByLocale(locale);
 
   return `${baseURL}/blob/${getCurrentGitBranch(
     root
@@ -148,8 +147,7 @@ function getGitHubURL(root: string, folder: string, filename: string) {
  * @param {String} hash - the full hash to point to.
  */
 export function getLastCommitURL(locale: string, hash: string) {
-  const repo = getRepositoryByLocale(locale);
-  const baseURL = `https://github.com/mdn/${repo}`;
+  const baseURL = getRepositoryUrlByLocale(locale);
   return `${baseURL}/commit/${hash}`;
 }
 
--- libs/locale-utils/index.js
+++ libs/locale-utils/index.js
@@ -76,6 +76,13 @@ export function isValidLocale(locale) {
   return typeof locale === "string" && VALID_LOCALES.has(locale.toLowerCase());
 }
 
+export function getRepositoryUrlByLocale(locale) {
+  const repo = getRepositoryByLocale(locale);
+  const url = `https://github.com/mdn/${repo}`;
+
+  return url;
+}
+
 export function getRepositoryByLocale(locale) {
   switch (locale.toLowerCase()) {
     case "en-us":
