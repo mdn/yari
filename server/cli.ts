54337d040efa87efec75f7fac5eaa119bcce7c5e
Florian Dieminger <me@fiji-flo.de>
2024-10-31 15:27:34 +0100

    fix server cli

--- server/cli.ts
+++ server/cli.ts
@@ -2,7 +2,7 @@ import { concurrently } from "concurrently";
 import { rariBin } from "@mdn/rari";
 import { filename } from "./filename.js";
 
-const { result } = concurrently(
+const { commands, result } = concurrently(
   [
     {
       command: `node ${filename}`,
@@ -10,21 +10,32 @@ const { result } = concurrently(
       env: {
         NODE_OPTIONS: "--no-warnings=ExperimentalWarning --loader ts-node/esm",
       },
+      prefixColor: "red",
     },
     {
       command: `${rariBin} serve -vv`,
       name: "rari",
+      prefixColor: "blue",
     },
   ],
   {
-    prefix: "🙈",
     killOthers: ["failure", "success"],
     restartTries: 0,
+    handleInput: true,
+    inputStream: process.stdin,
   }
 );
 
+const stop = new Promise((resolve, reject) => {
+  process.on("SIGINT", () => {
+    commands.forEach((cmd) => cmd.kill()); // Terminate all concurrently-run processes
+    reject();
+  });
+  result.finally(() => resolve(null));
+});
 try {
-  await result;
+  await stop;
+  console.log("All tasks completed successfully.");
 } catch {
-  console.error("NOPE");
+  console.log("Killed ☠️");
 }
