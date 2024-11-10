1561d3aa120ddb51a910aca8c8ddb0de7593aca3
Florian Dieminger <me@fiji-flo.de>
2024-11-10 14:51:35 +0100

    fix cloud function

--- cloud-function/src/env.ts
+++ cloud-function/src/env.ts
@@ -34,16 +34,14 @@ export const SOURCE_CONTENT: string =
 export const SOURCE_API: string =
   process.env["SOURCE_API"] || "https://developer.allizom.org/";
 
+const RUNNER_RE = /.*\/(play-)?runner.(html|js)$/i;
+
 export function getOriginFromRequest(req: Request): Origin {
-  if (
-    req.hostname === ORIGIN_MAIN &&
-    !req.path.includes("/_sample_.") &&
-    !req.path.endsWith("/runner.html")
-  ) {
+  if (req.hostname === ORIGIN_MAIN && !RUNNER_RE.test(req.path)) {
     return Origin.main;
   } else if (
     req.hostname === ORIGIN_LIVE_SAMPLES &&
-    !req.path.endsWith("/runner.html")
+    !RUNNER_RE.test(req.path)
   ) {
     return Origin.liveSamples;
   } else if (req.hostname.endsWith(ORIGIN_PLAY)) {
