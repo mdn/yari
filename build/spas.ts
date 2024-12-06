c9437ce4412874b66cf4429162c07524c67109e8
Leo McArdle <leo@mca.is>
2024-12-06 14:12:02 +0000

    feat: new about page (#12069)
    https://mozilla-hub.atlassian.net/browse/MP-1362
    
    ---------
    
    Co-authored-by: Florian Dieminger <me@fiji-flo.de>

--- build/spas.ts
+++ build/spas.ts
@@ -25,7 +25,12 @@ import {
 } from "../libs/env/index.js";
 import { isValidLocale } from "../libs/locale-utils/index.js";
 import { DocFrontmatter, DocParent, NewsItem } from "../libs/types/document.js";
-import { getSlugByBlogPostUrl, makeTOC } from "./utils.js";
+import {
+  getSlugByBlogPostUrl,
+  injectLoadingLazyAttributes,
+  makeTOC,
+  postProcessExternalLinks,
+} from "./utils.js";
 import { findByURL } from "../content/document.js";
 import { buildDocument } from "./index.js";
 import { findPostBySlug } from "./blog.js";
@@ -309,6 +314,8 @@ export async function buildSPAs(options: {
       };
       const [$] = await kumascript.render(url, {}, d);
       wrapTables($);
+      postProcessExternalLinks($);
+      injectLoadingLazyAttributes($);
       const [sections] = await extractSections($);
       const toc = makeTOC({ body: sections });
 
@@ -355,6 +362,7 @@ export async function buildSPAs(options: {
       OBSERVATORY_TITLE
     );
     await buildStaticPages(path.join(GENERIC_CONTENT_ROOT, "community"));
+    await buildStaticPages(path.join(GENERIC_CONTENT_ROOT, "about"));
   }
 
   // Build all the home pages in all locales.
