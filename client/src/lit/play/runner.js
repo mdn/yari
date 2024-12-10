f7d0b2028c607d25b23e04e1e80d5da5489831eb
Leo McArdle <leo@mca.is>
2024-12-10 17:00:07 +0000

    chore(playground): migrate runner iframe to web component
    https://mozilla-hub.atlassian.net/browse/MP-1806

---
+++ client/src/lit/play/runner.js
@@ -0,0 +1,123 @@
+import { html, LitElement } from "lit";
+import { compressAndBase64Encode } from "../../playground/utils.ts";
+import { PLAYGROUND_BASE_HOST } from "../../env.ts";
+import { createComponent } from "@lit/react";
+import { Task } from "@lit/task";
+import React from "react";
+
+import styles from "./runner.scss?css" with { type: "css" };
+
+/**
+ * @import { EditorContent } from "../../playground/utils"
+ * @import { VConsole } from "./types"
+ * @import { EventName } from "@lit/react"
+ * */
+
+export class PlayRunner extends LitElement {
+  static properties = {
+    code: { type: Object },
+    srcPrefix: { type: String, attribute: "src-prefix" },
+    _src: { state: true },
+  };
+
+  static styles = styles;
+
+  constructor() {
+    super();
+    /** @type {EditorContent | undefined} */
+    this.code = undefined;
+    /** @type {string | undefined} */
+    this.srcPrefix = undefined;
+    this._src = "about:blank";
+
+    this._subdomain = crypto.randomUUID();
+    this._flipFlop = 0;
+  }
+
+  /** @param {MessageEvent} e  */
+  _onMessage({ data: { typ, prop, message } }) {
+    if (typ === "console") {
+      if (
+        (prop === "log" || prop === "error" || prop === "warn") &&
+        typeof message === "string"
+      ) {
+        /** @type {VConsole} */
+        const detail = { prop, message };
+        this.dispatchEvent(
+          new CustomEvent("console", { bubbles: true, composed: true, detail })
+        );
+      } else {
+        const warning = "[Playground] Unsupported console message";
+        /** @type {VConsole} */
+        const detail = {
+          prop: "warn",
+          message: `${warning} (see browser console)`,
+        };
+        this.dispatchEvent(
+          new CustomEvent("console", { bubbles: true, composed: true, detail })
+        );
+        console.warn(warning, { prop, message });
+      }
+    }
+  }
+
+  _updateSrc = new Task(this, {
+    args: () => /** @type {const} */ ([this.code, this.srcPrefix]),
+    task: async ([code, srcPrefix], { signal }) => {
+      if (code) {
+        const { state } = await compressAndBase64Encode(JSON.stringify(code));
+        signal.throwIfAborted();
+        // We're using a random subdomain for origin isolation.
+        const url = new URL(
+          window.location.hostname.endsWith("localhost")
+            ? window.location.origin
+            : `${window.location.protocol}//${
+                PLAYGROUND_BASE_HOST.startsWith("localhost")
+                  ? ""
+                  : `${this._subdomain}.`
+              }${PLAYGROUND_BASE_HOST}`
+        );
+        url.searchParams.set("state", state);
+        // ensure iframe reloads even if code doesn't change
+        url.searchParams.set("f", this._flipFlop.toString());
+        url.pathname = `${srcPrefix || code.src || ""}/runner.html`;
+        this._src = url.href;
+        this._flipFlop = (this._flipFlop + 1) % 2;
+      } else {
+        this._src = "about:blank";
+      }
+    },
+  });
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._onMessage = this._onMessage.bind(this);
+    window.addEventListener("message", this._onMessage);
+  }
+
+  render() {
+    return html`
+      <iframe
+        title="runner"
+        src=${this._src}
+        sandbox="allow-scripts allow-same-origin allow-forms"
+      ></iframe>
+    `;
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    window.removeEventListener("message", this._onMessage);
+  }
+}
+
+customElements.define("play-runner", PlayRunner);
+
+export const ReactPlayRunner = createComponent({
+  tagName: "play-runner",
+  elementClass: PlayRunner,
+  react: React,
+  events: {
+    onConsole: /** @type {EventName<CustomEvent<VConsole>>} */ ("console"),
+  },
+});
--- client/src/playground/index.tsx
+++ client/src/playground/index.tsx
@@ -11,20 +11,15 @@ import prettierPluginHTML from "prettier/plugins/html";
 import { Button } from "../ui/atoms/button";
 import Editor, { EditorHandle } from "./editor";
 import { SidePlacement } from "../ui/organisms/placement";
-import {
-  compressAndBase64Encode,
-  decompressFromBase64,
-  EditorContent,
-  SESSION_KEY,
-} from "./utils";
+import { decompressFromBase64, EditorContent, SESSION_KEY } from "./utils";
 
 import "./index.scss";
-import { PLAYGROUND_BASE_HOST } from "../env";
 import { FlagForm, ShareForm } from "./forms";
 import { ReactPlayConsole } from "../lit/play/console";
+import { ReactPlayRunner } from "../lit/play/runner";
+import type { VConsole } from "../lit/play/types";
 import { useGleanClick } from "../telemetry/glean-context";
 import { PLAYGROUND } from "../telemetry/constants";
-import type { VConsole } from "../lit/play/types";
 
 const HTML_DEFAULT = "";
 const CSS_DEFAULT = "";
@@ -80,14 +75,12 @@ export default function Playground() {
   let [shareUrl, setShareUrl] = useState<URL | null>(null);
   let [vConsole, setVConsole] = useState<VConsole[]>([]);
   let [state, setState] = useState(State.initial);
+  const [code, setCode] = useState<EditorContent>();
   let [codeSrc, setCodeSrc] = useState<string | undefined>();
-  let [iframeSrc, setIframeSrc] = useState("about:blank");
   const [isEmpty, setIsEmpty] = useState<boolean>(true);
-  const subdomain = useRef<string>(crypto.randomUUID());
   const [initialContent, setInitialContent] = useState<EditorContent | null>(
     null
   );
-  const [flipFlop, setFlipFlop] = useState(0);
   let { data: initialCode } = useSWRImmutable<EditorContent>(
     !stateParam && !shared && gistId
       ? `/api/v1/play/${encodeURIComponent(gistId)}`
@@ -119,34 +112,11 @@ export default function Playground() {
   const htmlRef = useRef<EditorHandle | null>(null);
   const cssRef = useRef<EditorHandle | null>(null);
   const jsRef = useRef<EditorHandle | null>(null);
-  const iframe = useRef<HTMLIFrameElement | null>(null);
   const diaRef = useRef<HTMLDialogElement | null>(null);
 
-  const updateWithCode = useCallback(
-    async (code: EditorContent) => {
-      const { state } = await compressAndBase64Encode(JSON.stringify(code));
-
-      // We're using a random subdomain for origin isolation.
-      const url = new URL(
-        window.location.hostname.endsWith("localhost")
-          ? window.location.origin
-          : `${window.location.protocol}//${
-              PLAYGROUND_BASE_HOST.startsWith("localhost")
-                ? ""
-                : `${subdomain.current}.`
-            }${PLAYGROUND_BASE_HOST}`
-      );
-      setVConsole([]);
-      url.searchParams.set("state", state);
-      // ensure iframe reloads even if code doesn't change
-      url.searchParams.set("f", flipFlop.toString());
-      url.pathname = `${codeSrc || code.src || ""}/runner.html`;
-      setIframeSrc(url.href);
-      // using an updater function causes the second "run" to not reload properly:
-      setFlipFlop((flipFlop + 1) % 2);
-    },
-    [codeSrc, setVConsole, setIframeSrc, flipFlop, setFlipFlop]
-  );
+  useEffect(() => {
+    setVConsole([]);
+  }, [code, setVConsole]);
 
   useEffect(() => {
     if (initialCode) {
@@ -168,26 +138,9 @@ export default function Playground() {
     return code;
   }, [initialContent?.src, initialCode?.src]);
 
-  let messageListener = useCallback(({ data: { typ, prop, message } }) => {
-    if (typ === "console") {
-      if (
-        (prop === "log" || prop === "error" || prop === "warn") &&
-        typeof message === "string"
-      ) {
-        setVConsole((vConsole) => [...vConsole, { prop, message }]);
-      } else {
-        const warning = "[Playground] Unsupported console message";
-        setVConsole((vConsole) => [
-          ...vConsole,
-          {
-            prop: "warn",
-            message: `${warning} (see browser console)`,
-          },
-        ]);
-        console.warn(warning, { prop, message });
-      }
-    }
-  }, []);
+  const onConsole = ({ detail }: CustomEvent<VConsole>) => {
+    setVConsole((vConsole) => [...vConsole, detail]);
+  };
 
   const setEditorContent = ({ html, css, js, src }: EditorContent) => {
     htmlRef.current?.setContent(html);
@@ -206,7 +159,7 @@ export default function Playground() {
           setEditorContent(initialCode);
           if (!gistId) {
             // don't auto run shared code
-            updateWithCode(initialCode);
+            setCode(initialCode);
           }
         } else if (stateParam) {
           try {
@@ -226,14 +179,7 @@ export default function Playground() {
         setState(State.ready);
       }
     })();
-  }, [initialCode, state, gistId, stateParam, updateWithCode]);
-
-  useEffect(() => {
-    window.addEventListener("message", messageListener);
-    return () => {
-      window.removeEventListener("message", messageListener);
-    };
-  }, [messageListener]);
+  }, [initialCode, state, gistId, stateParam, setCode]);
 
   const clear = async () => {
     setSearchParams([], { replace: true });
@@ -285,7 +231,7 @@ export default function Playground() {
       iterations: 1,
     };
     document.getElementById("run")?.firstElementChild?.animate(loading, timing);
-    updateWithCode({ html, css, js, src });
+    setCode({ html, css, js, src });
   };
 
   const format = async () => {
@@ -415,12 +361,11 @@ export default function Playground() {
               Seeing something inappropriate?
             </button>
           )}
-          <iframe
-            title="runner"
-            ref={iframe}
-            src={iframeSrc}
-            sandbox="allow-scripts allow-same-origin allow-forms"
-          ></iframe>
+          <ReactPlayRunner
+            code={code}
+            srcPrefix={codeSrc}
+            onConsole={onConsole}
+          />
           <ReactPlayConsole vConsole={vConsole} />
           <SidePlacement extraClasses={["horizontal"]} />
         </section>
