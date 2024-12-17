a50daa832d34a14b2cb2629896afad595b7d1345
Florian Dieminger <me@fiji-flo.de>
2024-12-17 16:24:49 +0100

    add build_out_root default

--- bins/build.mjs
+++ bins/build.mjs
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 import { rariBin } from "@mdn/rari";
 import { spawn } from "cross-spawn";
+import { config } from "dotenv";
+import path from "node:path";
+import { cwd } from "node:process";
+
+import { BUILD_OUT_ROOT } from "../libs/env/index.js";
+
+config({
+  path: path.join(cwd(), process.env.ENV_FILE || ".env"),
+});
+
+process.env.BUILD_OUT_ROOT = process.env.BUILD_OUT_ROOT || BUILD_OUT_ROOT;
 
 spawn(rariBin, ["build"], { stdio: "inherit" });
