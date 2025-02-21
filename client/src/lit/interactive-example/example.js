29c55f2fbea3437447ad306c8220346511aa4847
Leo McArdle <leo@mca.is>
2025-02-21 13:58:03 +0000

    fix(interactive-examples): open targetless `<area>`s in parent window (#12652)

--- client/src/lit/interactive-example/example.js
+++ client/src/lit/interactive-example/example.js
@@ -1,7 +1,10 @@
 window.addEventListener("click", (event) => {
   // open links in parent frame if they have no `_target` set
   const target = event.target;
-  if (target instanceof HTMLAnchorElement) {
+  if (
+    target instanceof HTMLAnchorElement ||
+    target instanceof HTMLAreaElement
+  ) {
     const hrefAttr = target.getAttribute("href");
     const targetAttr = target.getAttribute("target");
     if (hrefAttr && !hrefAttr.startsWith("#") && !targetAttr) {
