396ae5084e0cc0057459e1c6169b97430886f897
Andi Pieper <apieper@mozilla.com>
2025-02-24 17:18:18 +0100

    re-introduce extension

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -406,7 +406,12 @@ function config(webpackEnv) {
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
               // by webpacks internal loaders.
-              exclude: [/^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+              exclude: [
+                /^$/,
+                /\.(js|mjs|jsx|ts|tsx|wasm)$/,
+                /\.html$/,
+                /\.json$/,
+              ],
               type: "asset/resource",
             },
             // ** STOP ** Are you adding a new loader?
