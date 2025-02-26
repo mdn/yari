7ee1ba89b82b0cdb5721b6c62309aa68ad42b8d1
Claas Augner <caugner@mozilla.com>
2025-02-26 16:29:37 +0100

    chore(ix): remove unnecessary console.log

--- client/src/lit/interactive-example/choice.js
+++ client/src/lit/interactive-example/choice.js
@@ -1,5 +1,4 @@
 window.addEventListener("message", ({ data }) => {
-  console.log("DATA!", data);
   if (data.typ === "choice") {
     const code = data.code;
     const element = document.getElementById("example-element");
