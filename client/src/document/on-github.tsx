8f47b9b832b57b0b73eed5a1d9d3f2a5b13c5111
Claas Augner <caugner@mozilla.com>
2025-05-21 23:01:11 +0200

    fix(on-github): use allizom for Italian issue reports

--- client/src/document/on-github.tsx
+++ client/src/document/on-github.tsx
@@ -85,6 +85,9 @@ function NewIssueOnGitHubLink({
       : "page-report.yml"
   );
   sp.set("mdn-url", `https://developer.mozilla.org${doc.mdn_url}`);
+  if (doc.locale === "it") {
+    sp.set("mdn-url", `https://developer.allizom.org${doc.mdn_url}`);
+  }
   sp.set("metadata", fillMetadata(METADATA_TEMPLATE, doc));
 
   url.search = sp.toString();
