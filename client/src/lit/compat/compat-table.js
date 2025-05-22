c5bf81d30516a4ebc63e768b12648af74801b5a3
Leo McArdle <leo@mca.is>
2025-05-22 14:50:29 +0100

    fix(bcd): limit length of long bcd tables (#13061)

--- client/src/lit/compat/compat-table.js
+++ client/src/lit/compat/compat-table.js
@@ -266,7 +266,15 @@ class CompatTable extends GleanMixin(LitElement) {
   _renderTableBody() {
     // <FeatureListAccordion>
     const { data, _browsers: browsers, browserInfo, locale } = this;
-    const features = listFeatures(data, "", this._name);
+    let features = listFeatures(data, "", this._name);
+
+    // enormous BCD tables are unusable and have terrible performance: crashing in some browsers
+    if (features.length > 100) {
+      features = features.filter(({ depth }) => depth < 2);
+      if (features.length > 100) {
+        features = features.filter(({ depth }) => depth < 1);
+      }
+    }
 
     return html`<tbody>
       ${features.map((feature) => {
