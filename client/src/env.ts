mdn/yari

commit 4256f47795c309a5ed9ec9965aeefcf994391586

Update client/src/env.ts
Co-authored-by: Leo McArdle <leo@mca.is>

client/src/env.ts
@@ -64,7 +64,7 @@ export const PLACEMENT_ENABLED = Boolean(
 
 export const MDN_PLUS_SUBSCRIBE_URL_SP3_BASE =
   process.env.REACT_APP_MDN_PLUS_SUBSCRIBE_URL_SP3_BASE;
-export const MDN_PLUS_USE_SP3 = Boolean(MDN_PLUS_SUBSCRIBE_URL_SP3_BASE);
+export const MDN_PLUS_USE_SP3 = Boolean(JSON.parse(process.env.REACT_APP_MDN_PLUS_USE_SP3 || "false"));
 export const MDN_PLUS_SUBSCRIBE_5M_URL_SP3 = `${MDN_PLUS_SUBSCRIBE_URL_SP3_BASE}/${process.env.REACT_APP_MDN_PLUS_5M_SP3_ID}/monthly/landing`;
 export const MDN_PLUS_SUBSCRIBE_5Y_URL_SP3 = `${MDN_PLUS_SUBSCRIBE_URL_SP3_BASE}/${process.env.REACT_APP_MDN_PLUS_5Y_SP3_ID}/yearly/landing`;
 export const MDN_PLUS_SUBSCRIBE_10M_URL_SP3 = `${MDN_PLUS_SUBSCRIBE_URL_SP3_BASE}/${process.env.REACT_APP_MDN_PLUS_10M_SP3_ID}/monthly/landing`;

commit 7980a200e51ce02af2044974bbda5603dc00b562

fix(plus): fix use sp3
this got broken in https://github.com/mdn/yari/pull/13199/commits/4256f47795c309a5ed9ec9965aeefcf994391586

client/src/env.ts
@@ -65,7 +65,7 @@ export const PLACEMENT_ENABLED = Boolean(
 export const MDN_PLUS_SUBSCRIBE_URL_SP3_BASE =
   process.env.REACT_APP_MDN_PLUS_SUBSCRIBE_URL_SP3_BASE;
 export const MDN_PLUS_USE_SP3 = Boolean(
-  JSON.parse(process.env.REACT_APP_MDN_PLUS_USE_SP3 || "false")
+  JSON.parse(MDN_PLUS_SUBSCRIBE_URL_SP3_BASE || "false")
 );
 export const MDN_PLUS_SUBSCRIBE_5M_URL_SP3 = `${MDN_PLUS_SUBSCRIBE_URL_SP3_BASE}/${process.env.REACT_APP_MDN_PLUS_5M_SP3_ID}/monthly/landing`;
 export const MDN_PLUS_SUBSCRIBE_5Y_URL_SP3 = `${MDN_PLUS_SUBSCRIBE_URL_SP3_BASE}/${process.env.REACT_APP_MDN_PLUS_5Y_SP3_ID}/yearly/landing`;