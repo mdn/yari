a2db291ab5843be909463693b1b5da67923158d7
Andi Pieper <apieper@mozilla.com>
2025-06-24 18:00:16 +0200

    chore(plus): upgrade to SubPlat 3.0 (#13199)

--- client/src/env.ts
+++ client/src/env.ts
@@ -62,6 +62,16 @@ export const PLACEMENT_ENABLED = Boolean(
   JSON.parse(process.env.REACT_APP_PLACEMENT_ENABLED || "false")
 );
 
+export const MDN_PLUS_SUBSCRIBE_URL_SP3_BASE =
+  process.env.REACT_APP_MDN_PLUS_SUBSCRIBE_URL_SP3_BASE;
+export const MDN_PLUS_USE_SP3 = Boolean(
+  JSON.parse(process.env.REACT_APP_MDN_PLUS_USE_SP3 || "false")
+);
+export const MDN_PLUS_SUBSCRIBE_5M_URL_SP3 = `${MDN_PLUS_SUBSCRIBE_URL_SP3_BASE}/${process.env.REACT_APP_MDN_PLUS_5M_SP3_ID}/monthly/landing`;
+export const MDN_PLUS_SUBSCRIBE_5Y_URL_SP3 = `${MDN_PLUS_SUBSCRIBE_URL_SP3_BASE}/${process.env.REACT_APP_MDN_PLUS_5Y_SP3_ID}/yearly/landing`;
+export const MDN_PLUS_SUBSCRIBE_10M_URL_SP3 = `${MDN_PLUS_SUBSCRIBE_URL_SP3_BASE}/${process.env.REACT_APP_MDN_PLUS_10M_SP3_ID}/monthly/landing`;
+export const MDN_PLUS_SUBSCRIBE_10Y_URL_SP3 = `${MDN_PLUS_SUBSCRIBE_URL_SP3_BASE}/${process.env.REACT_APP_MDN_PLUS_10Y_SP3_ID}/yearly/landing`;
+
 export const MDN_PLUS_SUBSCRIBE_5M_URL = `${process.env.REACT_APP_MDN_PLUS_SUBSCRIBE_URL}?plan=${process.env.REACT_APP_MDN_PLUS_5M_PLAN}`;
 export const MDN_PLUS_SUBSCRIBE_5Y_URL = `${process.env.REACT_APP_MDN_PLUS_SUBSCRIBE_URL}?plan=${process.env.REACT_APP_MDN_PLUS_5Y_PLAN}`;
 export const MDN_PLUS_SUBSCRIBE_10M_URL = `${process.env.REACT_APP_MDN_PLUS_SUBSCRIBE_URL}?plan=${process.env.REACT_APP_MDN_PLUS_10M_PLAN}`;
