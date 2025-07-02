94b7ccbc7a31afaf9dae18ddf4b7c6ea181802df
Claas Augner <caugner@mozilla.com>
2025-07-02 14:23:31 +0200

    refactor(dotenv): use default export

--- cloud-function/src/build-canonicals.ts
+++ cloud-function/src/build-canonicals.ts
@@ -2,7 +2,7 @@ import { readFile, stat, writeFile } from "node:fs/promises";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
 
 import { normalizePath } from "./utils.js";
 
