c49aeb24ecec1e10f7e2b6c1ac76209af6ad6d9b
Claas Augner <caugner@mozilla.com>
2025-09-11 13:59:31 +0200

    chore(cloud-function): set "Referrer-Policy: no-referrer" on ad clicks

--- cloud-function/src/handlers/proxy-bsa.ts
+++ cloud-function/src/handlers/proxy-bsa.ts
@@ -65,6 +65,7 @@ export async function proxyBSA(req: Request, res: Response) {
         userAgent
       );
       if (location && (status === 301 || status === 302)) {
+        res.setHeader("Referrer-Policy", "no-referrer");
         return res.redirect(location);
       } else {
         return res.sendStatus(502).end();
